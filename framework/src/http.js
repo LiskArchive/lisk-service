e61df43b412f8b59f99f7f7916a3cee2d981f220
Michal Tuleja <michal@lightcurve.io>
2020-05-26 16:05:10 +0200

    Improve HTTP library implementation

--- framework/src/http.js
+++ framework/src/http.js
@@ -7,7 +7,9 @@ const CACHE_DEFAULT_TTL = 0;
 const CACHE_MAX_N_ITEMS = 4096;
 const CACHE_MAX_TTL = 12 * 60 * 60 * 1000; // 12 hrs
 
-const cache = require('./cacheLru')('_framework_http_cache', {
+const CacheLRU = require('./cacheLru');
+
+const cache = CacheLRU('_framework_http_cache', {
 	max: CACHE_MAX_N_ITEMS,
 	ttl: CACHE_MAX_TTL,
 });
@@ -19,10 +21,12 @@ const _validateHttpResponse = (response) => {
 
 const request = async (url, params = {}) => {
 	let response;
+	if (!params.method) params.method = 'get';
 	const key = url + JSON.stringify(params);
 
-	if (params.cacheTTL && params.cacheTTL > 0) {
-		response = await cache.get(key);
+	if (params.method.toLowerCase() === 'get'
+		&& params.cacheTTL && params.cacheTTL > 0) {
+			response = await cache.get(key);
 	}
 
 	if (!response) {
@@ -73,35 +77,6 @@ const performRequestUntilSuccess = async (url, params) => {
 	return response;
 }
 
-const requestWithPagination = async (fn, params, limit) => {
-	const defaultMaxAmount = limit || 1000;
-	const oneRequestLimit = params.limit || 100;
-	const firstRequest = await fn(Object.assign({}, params, {
-		limit: oneRequestLimit,
-		offset: 0,
-	}));
-	const { data } = firstRequest;
-	const maxAmount = Math.min(defaultMaxAmount, firstRequest.meta.count);
-
-	if (maxAmount > oneRequestLimit) {
-		const pages = [...Array(Math.ceil(maxAmount / oneRequestLimit)).keys()];
-		pages.shift();
-
-		const collection = await pages.reduce((promise, page) => promise.then(() => fn(
-			Object.assign({}, params, {
-				limit: oneRequestLimit,
-				offset: oneRequestLimit * page,
-			}))).then((result) => {
-			result.data.forEach((item) => { data.push(item); });
-			return data;
-		}).catch(err => {
-			logger.warn(`Failed to fetch data ${err}`);
-		}), Promise.resolve());
-		return collection;
-	}
-	return data;
-};
-
 module.exports = {
 	request,
 	get: (url, params) => request(url, { ...params, method: 'get' }),
@@ -113,4 +88,5 @@ module.exports = {
 	options: (url, params) => request(url, { ...params, method: 'options' }),
 	trace: (url, params) => request(url, { ...params, method: 'trace' }),
 	StatusCodes: HttpStatus,
+	_destroyCache: () => cache.destroy(),
 };
