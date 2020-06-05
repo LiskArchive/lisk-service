a8bed7ab6323f97476f284a46222b0ce43632765
Michal Tuleja <michal@lightcurve.io>
2020-06-05 23:10:10 +0200

    Add router config

--- services/gateway/routes.js
+++ services/gateway/routes.js
@@ -1,45 +1,54 @@
 const { mapper, Utils } = require('lisk-service-framework');
 const path = require('path');
 
-const transformPath = (url) => {
-	const dropSlash = (str) => str.replace(/^\//, '');
-	const curlyBracketsToColon = (str) => str.split('{').join(':').replace(/}/g, '');
-
-	return curlyBracketsToColon(dropSlash(url));
-};
-
-const allMethods = Utils.requireAllJs(path.resolve(__dirname, './apis/http-version1/methods'));
-
-const methods = Object.keys(allMethods).reduce((acc, key) => {
-	const method = allMethods[key];
-	if (method.version !== '2.0') return { ...acc };
-	if (!method.source) return { ...acc };
-	if (!method.source.method) return { ...acc };
-	if (!method.swaggerApiPath) return { ...acc };
-	return { ...acc, [key]: method };
-}, {});
-
-const methodPaths = Object.keys(methods).reduce((acc, key) => ({
-	...acc, [methods[key].swaggerApiPath]: methods[key]
-}), {});
-
-const whitelist = Object.keys(methods).reduce((acc, key) => [ ...acc, methods[key].source.method ], []);
-
-const aliases = Object.keys(methods).reduce((acc, key) => ({
-	...acc, [`GET ${transformPath(methods[key].swaggerApiPath)}`]: methods[key].source.method
-}), {});
-
-const transformResponse = async (path, data) => {
-	const transformedData = await mapper(data, methodPaths[path].source.definition);
-	return {
-		...methodPaths[path].envelope,
-		...transformedData,
+const configureApi = (apiName) => {
+	const transformPath = (url) => {
+		const dropSlash = (str) => str.replace(/^\//, '');
+		const curlyBracketsToColon = (str) => str.split('{').join(':').replace(/}/g, '');
+	
+		return curlyBracketsToColon(dropSlash(url));
 	};
+	
+	const allMethods = Utils.requireAllJs(path.resolve(__dirname, `./apis/${apiName}/methods`));
+	
+	const methods = Object.keys(allMethods).reduce((acc, key) => {
+		const method = allMethods[key];
+		if (method.version !== '2.0') return { ...acc };
+		if (!method.source) return { ...acc };
+		if (!method.source.method) return { ...acc };
+		if (!method.swaggerApiPath) return { ...acc };
+		return { ...acc, [key]: method };
+	}, {});
+	
+	const methodPaths = Object.keys(methods).reduce((acc, key) => ({
+		...acc, [methods[key].swaggerApiPath]: methods[key]
+	}), {});
+	
+	const whitelist = Object.keys(methods).reduce((acc, key) => [ ...acc, methods[key].source.method ], []);
+	
+	const aliases = Object.keys(methods).reduce((acc, key) => ({
+		...acc, [`GET ${transformPath(methods[key].swaggerApiPath)}`]: methods[key].source.method
+	}), {});
+	
+	return {
+		aliases, whitelist, methodPaths
+	}
 };
 
-module.exports = [
-	{
-		path: "/v1",
+const registerApi = (apiName, config) => {
+	const { path } = config;
+	const { aliases, whitelist, methodPaths } = configureApi(apiName);
+
+	const transformResponse = async (path, data) => {
+		const transformedData = await mapper(data, methodPaths[path].source.definition);
+		return {
+			...methodPaths[path].envelope,
+			...transformedData,
+		};
+	};
+
+	return {
+		path,
 
 		whitelist: [
 			...whitelist,
@@ -75,5 +84,11 @@ module.exports = [
 			res.setHeader("X-Custom-Header", "123456");
 			return transformResponse(req.url, data.data);
 		},
-	},
+	}
+};
+
+module.exports = [
+	registerApi('http-version1', {
+		path: '/v1',
+	}),
 ];
