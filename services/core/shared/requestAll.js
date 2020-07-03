d942fd8bdc40580645f298c8f06f4e7d0a403102
Michal Tuleja <michal@lightcurve.io>
2020-07-03 16:53:37 +0200

    Add function requestAll compatible with Lisk Core API

---
+++ services/core/shared/requestAll.js
@@ -0,0 +1,48 @@
+/*
+ * LiskHQ/lisk-service
+ * Copyright © 2019 Lisk Foundation
+ *
+ * See the LICENSE file at the top-level directory of this distribution
+ * for licensing information.
+ *
+ * Unless otherwise agreed in a custom licensing agreement with the Lisk Foundation,
+ * no part of this software, including this file, may be copied, modified,
+ * propagated, or distributed except according to the terms contained in the
+ * LICENSE file.
+ *
+ * Removal or modification of this copyright notice is prohibited.
+ *
+ */
+
+const { HTTP, Utils, Logger } = require('lisk-service-framework');
+
+const requestAll = async (fn, params, limit) => {
+	const defaultMaxAmount = limit || 10000;
+	const oneRequestLimit = params.limit || 100;
+	const firstRequest = await fn({ ...params,
+limit: oneRequestLimit,
+		offset: 0 });
+	const { data } = firstRequest;
+	const maxAmount = firstRequest.meta.count > defaultMaxAmount
+		? defaultMaxAmount
+		: firstRequest.meta.count;
+
+	if (maxAmount > oneRequestLimit) {
+		const pages = [...Array(Math.ceil(maxAmount / oneRequestLimit)).keys()];
+		pages.shift();
+
+		const collection = await pages.reduce((promise, page) => promise.then(() => fn(
+			{ ...params,
+limit: oneRequestLimit,
+				offset: oneRequestLimit * page })).then(result => {
+			result.data.forEach(item => { data.push(item); });
+			return data;
+		}).catch(err => {
+			logger.warn(`Failed to fetch data ${err}`);
+		}), Promise.resolve());
+		return collection;
+	}
+	return data;
+};
+
+module.exports = requestAll;
