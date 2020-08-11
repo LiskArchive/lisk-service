91845f8fc0de3776c95602305e8d1f5999cfb588
user <himanshu.nagda@lightcurve.io>
2020-08-11 11:05:07 +0200

    delegates tests

--- tests/integration_testnet/api/http/delegates.test.js
+++ tests/integration_testnet/api/http/delegates.test.js
@@ -13,7 +13,6 @@
  * Removal or modification of this copyright notice is prohibited.
  *
  */
-/* eslint-disable no-alert, no-console */
 import api from '../../helpers/api';
 import delegates from './constants/delegates';
 import accounts from './constants/accounts';
@@ -61,7 +60,6 @@ describe('Delegates API', () => {
 	describe('GET /delegates', () => {
 		it('known delegate by address -> ok', async () => {
 			const response = await api.get(`${endpoint}?address=${delegates.activeDelegate.address}`);
-			console.log('response', response);
 			expect(response.data[0]).toMapRequiredSchema({
 				...delegateSchema,
 				address: delegates.activeDelegate.address,
