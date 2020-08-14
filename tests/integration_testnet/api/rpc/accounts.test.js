7038004bebf2316a0e4e3ffe8e5c44e58e0e53bd
Michal Tuleja <michal@lightcurve.io>
2020-08-14 13:10:45 +0200

    Update account API tests

--- tests/integration_testnet/api/rpc/accounts.test.js
+++ tests/integration_testnet/api/rpc/accounts.test.js
@@ -20,7 +20,7 @@ import { JSON_RPC } from '../../helpers/errorCodes';
 import accounts from './constants/accounts';
 
 const baseUrl = config.SERVICE_ENDPOINT;
-const baseUrlRpcV1 = `${baseUrl}/rpc`;
+const baseUrlRpcV1 = `${baseUrl}/rpc-v1`;
 
 const accountSchema = Joi.object({
 	address: Joi.string().required(),
@@ -52,71 +52,83 @@ const invalidParamsSchema = Joi.object({
 const getAccounts = async params => request(baseUrlRpcV1, 'get.accounts', params);
 
 describe('Method get.accounts', () => {
-	it('returns account details on known address', async () => {
-		const { result } = await getAccounts({ address: accounts.genesis.address });
-		expect(result.data.length).toEqual(1);
-		expect(result.data[0]).toMap(accountSchema, { address: accounts.genesis.address });
+	describe('is able to retrieve account lists', () => {
+		it.todo('returns account details with no params');
 	});
 
-	it('returns account details when known address is written lowercase', async () => {
-		const { result } = await getAccounts({ address: accounts.genesis.address.toLowerCase() });
-		expect(result.data.length).toEqual(1);
-		expect(result.data[0]).toMap(accountSchema, { address: accounts.genesis.address });
-	});
-
-	it('returns empty response when unknown address', async () => {
-		const { result } = await getAccounts({ address: '99999L' });
-		expect(result).toEqual({});
-	});
-
-	it('returns INVALID_PARAMS error (-32602) on invalid address', async () => {
-		const { error } = await getAccounts({ address: 'L' }).catch(e => e);
-		expect(error).toMap(invalidParamsSchema, { code: JSON_RPC.INVALID_PARAMS[0] });
-	});
-
-	it('returns empty response when given empty address', async () => {
-		const { error } = await getAccounts({ address: '' }).catch(e => e);
-		expect(error).toMap(invalidParamsSchema, { code: JSON_RPC.INVALID_PARAMS[0] });
-	});
-
-	it('returns account details on known public key', async () => {
-		const { result } = await getAccounts({ publickey: accounts.genesis.publicKey });
-		expect(result.data.length).toEqual(1);
-		expect(result.data[0]).toMap(accountSchema, { address: accounts.genesis.address });
-	});
-
-	it('returns account details on known second public key', async () => {
-		const { result } = await getAccounts({
-			secpubkey: accounts['second passphrase account'].secondPublicKey,
+	describe('is able to retrieve account based on address', () => {
+		it('returns account details on known address', async () => {
+			const { result } = await getAccounts({ address: accounts.genesis.address });
+			expect(result.data.length).toEqual(1);
+			expect(result.data[0]).toMap(accountSchema, { address: accounts.genesis.address });
 		});
-		expect(result.data.length).toEqual(1);
-		expect(result.data[0]).toMap(accountSchema, {
-			address: accounts['second passphrase account'].address,
+
+		it('returns account details when known address is written lowercase', async () => {
+			const { result } = await getAccounts({ address: accounts.genesis.address.toLowerCase() });
+			expect(result.data.length).toEqual(1);
+			expect(result.data[0]).toMap(accountSchema, { address: accounts.genesis.address });
+		});
+
+		it('returns empty response when unknown address', async () => {
+			const { result } = await getAccounts({ address: '99999L' });
+			expect(result).toEqual({});
+		});
+
+		it('returns INVALID_PARAMS error (-32602) on invalid address', async () => {
+			const { error } = await getAccounts({ address: 'L' }).catch(e => e);
+			expect(error).toMap(invalidParamsSchema, { code: JSON_RPC.INVALID_PARAMS[0] });
+		});
+
+		it('returns empty response when given empty address', async () => {
+			const { error } = await getAccounts({ address: '' }).catch(e => e);
+			expect(error).toMap(invalidParamsSchema, { code: JSON_RPC.INVALID_PARAMS[0] });
 		});
 	});
 
-	it('returns empty response on invalid public key', async () => {
-		const { error } = await getAccounts({ address: '' }).catch(e => e);
-		expect(error).toMap(invalidParamsSchema, { code: JSON_RPC.INVALID_PARAMS[0] });
+	describe('is able to retrieve account based on public key', () => {
+		it('returns account details on known public key', async () => {
+			const { result } = await getAccounts({ publickey: accounts.genesis.publicKey });
+			expect(result.data.length).toEqual(1);
+			expect(result.data[0]).toMap(accountSchema, { address: accounts.genesis.address });
+		});
+
+		it('returns empty response on invalid public key', async () => {
+			const { error } = await getAccounts({ address: '' }).catch(e => e);
+			expect(error).toMap(invalidParamsSchema, { code: JSON_RPC.INVALID_PARAMS[0] });
+		});
+
+		it('returns empty response on unknown public key', async () => {
+			const { result } = await getAccounts({ publickey: 'ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff' });
+			expect(result).toEqual({});
+		});
+
+		it('throws INVALID_PARAMS error (-32602) on empty public key', async () => {
+			const { error } = await getAccounts({ publickey: '' }).catch(e => e);
+			expect(error).toMap(invalidParamsSchema, { code: JSON_RPC.INVALID_PARAMS[0] });
+		});
 	});
 
-	it('returns empty response on unknown public key', async () => {
-		const { result } = await getAccounts({ publickey: 'ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff' });
-		expect(result).toEqual({});
+	xdescribe('is able to retrieve account based on second public key', () => {
+		it('returns account details on known second public key', async () => {
+			const { result } = await getAccounts({
+				secpubkey: accounts['second passphrase account'].secondPublicKey,
+			});
+			expect(result.data.length).toEqual(1);
+			expect(result.data[0]).toMap(accountSchema, {
+				address: accounts['second passphrase account'].address,
+			});
+		});
 	});
 
-	it('throws INVALID_PARAMS error (-32602) on empty public key', async () => {
-		const { error } = await getAccounts({ publickey: '' }).catch(e => e);
-		expect(error).toMap(invalidParamsSchema, { code: JSON_RPC.INVALID_PARAMS[0] });
-	});
+	describe('is able to retrieve delegate account details', () => {
+		it('returns delegate data by address', async () => {
+			const { result } = await getAccounts({ address: accounts.delegate.address }).catch(e => e);
+			expect(result.data[0].delegate).toMap(delegateSchema);
+		});
 
-	it('returns delegate data by address', async () => {
-		const { result } = await getAccounts({ address: accounts.delegate.address }).catch(e => e);
-		expect(result.data[0].delegate).toMap(delegateSchema);
-	});
-
-	it('returns delegate data by delegate name', async () => {
-		const { result } = await getAccounts({ username: accounts.delegate.username });
-		expect(result.data[0].delegate).toMap(delegateSchema);
+		it('returns delegate data by delegate name', async () => {
+			const { result } = await getAccounts({ username: accounts.delegate.username });
+			expect(result.data[0].delegate).toMap(delegateSchema);
+		});
 	});
 });
