cab74a846ba2d3e08b5c8cfc5b29c000f685dd76
Sameer Kumar Subudhi <sameer.subudhi@lighcurve.io>
2020-08-26 16:35:20 +0200

    Update config to refer correct DB instance

--- services/core/config.js
+++ services/core/config.js
@@ -31,7 +31,7 @@ config.brokerTimeout = Number(process.env.SERVICE_BROKER_TIMEOUT) || 30 * 1000; 
  */
 config.endpoints.liskHttp = `${(process.env.LISK_CORE_HTTP || 'http://127.0.0.1:4000')}/api`;
 config.endpoints.liskWs = process.env.LISK_CORE_WS || config.endpoints.liskHttp.replace('http', 'ws').replace('/api', '');
-config.endpoints.redis = process.env.SERVICE_CORE_REDIS || 'redis://localhost:6379/0';
+config.endpoints.redis = process.env.SERVICE_CORE_REDIS || 'redis://localhost:6379/1';
 config.endpoints.postgres = process.env.SERVICE_CORE_POSTGRES || 'postgres://localhost:5432/lisk-service-core';
 config.endpoints.liskStatic = process.env.LISK_STATIC || 'https://static-data.lisk.io';
 config.endpoints.geoip = process.env.GEOIP_JSON || 'https://geoip.lisk.io/json';
