3364915dd61c966837115b355ef694404c63c175
Sameer Kumar Subudhi <sameer.subudhi@lightcurve.io>
2020-09-30 11:27:48 +0200

    Fix unit test cases for feeEstimation

--- services/core/tests/unit/feeEstimates.test.js
+++ services/core/tests/unit/feeEstimates.test.js
@@ -109,35 +109,55 @@ describe('Fee estimation tests', () => {
 		it('Batch of non-empty blocks', async () => {
 			const wavg = calculateWeightedAvg(highTrafficMockup.blocks);
 			expect(wavg).not.toBe(0);
+			expect(wavg).toBeCloseTo(134283.09572095712);
 		});
 	});
 
 	describe('calulateAvgFeePerByte for Transactions', () => {
+		let transactionDetails;
+		beforeAll(async () => {
+			const payload = nonEmptyBlock.transactions.data;
+			transactionDetails = payload.map(transaction => {
+				const tx = getTransactionInstanceByType(transaction);
+				const transactionSize = tx.getBytes().length;
+				const { minFee } = tx;
+				const feePriority = (Number(transaction.fee) - Number(minFee)) / transactionSize;
+				return {
+					id: transaction.id,
+					size: transactionSize,
+					feePriority,
+				};
+			});
+			transactionDetails.sort((t1, t2) => t1.feePriority - t2.feePriority);
+		});
+
 		it('Available computation modes', async () => {
 			expect(calcAvgFeeByteModes.MEDIUM).toBe('med');
 			expect(calcAvgFeeByteModes.HIGH).toBe('high');
 		});
 
-		xit('Mode: \'med\'', async () => {
+		it('Mode: \'med\'', async () => {
 			const avgFeeByte = calculateAvgFeePerByte(
 				calcAvgFeeByteModes.MEDIUM,
-				transactionType8,
+				transactionDetails,
 			);
 			expect(avgFeeByte).not.toBe(0);
+			expect(avgFeeByte).toBeCloseTo(36.501497200885304);
 		});
 
-		xit('Mode: \'high\'', async () => {
+		it('Mode: \'high\'', async () => {
 			const avgFeeByte = calculateAvgFeePerByte(
 				calcAvgFeeByteModes.HIGH,
-				transactionType8,
+				transactionDetails,
 			);
 			expect(avgFeeByte).not.toBe(0);
+			expect(avgFeeByte).toBeCloseTo(1727.6700292873413);
 		});
 	});
 
 	describe('calculateFeePerByte for Blocks', () => {
 		it('Empty block', async () => {
-			const feePerByte = calculateFeePerByte(noTrafficMockup.blocks[0]);
+			const feePerByte = calculateFeePerByte(emptyBlock);
 			expect(feePerByte.low).toBe(0);
 			expect(feePerByte.med).toBe(0);
 			expect(feePerByte.high).toBe(1);
@@ -149,7 +169,7 @@ describe('Fee estimation tests', () => {
 		});
 
 		it('Non-empty block', async () => {
-			const block = highTrafficMockup.blocks[0];
+			const block = nonEmptyBlock;
 			block.transactions.data = block.transactions.data.map(transaction => {
 				transaction.fee = BigInt(transaction.fee);
 				return transaction;
@@ -157,12 +177,12 @@ describe('Fee estimation tests', () => {
 
 			const feePerByte = calculateFeePerByte(block);
 			expect(feePerByte.low).toBeCloseTo(0);
-			expect(feePerByte.med).toBeCloseTo(0.13624);
-			expect(feePerByte.high).toBeCloseTo(0.1703);
+			expect(feePerByte.med).toBeCloseTo(36.501497200885304);
+			expect(feePerByte.high).toBeCloseTo(1727.6700292873413);
 			expect(feePerByte).toEqual({
 				low: 0,
-				med: 0.13624,
-				high: 0.1703,
+				med: 36.501497200885304,
+				high: 1727.6700292873413,
 			});
 		});
 	});
