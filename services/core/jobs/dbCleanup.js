e7b7ba8ef6929073ba7b69bb3db0830658ced188
Sameer Kumar Subudhi <sameer.subudhi@lightcurve.io>
2020-11-10 12:15:03 +0100

    Add jobs for DB cleanup

---
+++ services/core/jobs/dbCleanup.js
@@ -0,0 +1,50 @@
+/*
+ * LiskHQ/lisk-service
+ * Copyright © 2020 Lisk Foundation
+ *
+ * See the LICENSE file at the top-level directory of this distribution
+ * for licensing information.
+ *
+ * Unless otherwise agreed in a custom licensing agreement with the Lisk Foundation,
+ * no part of this software, including this file, may be copied, modified,
+ * propagated, or distributed except according to the terms contained in the
+ * LICENSE file.
+ *
+ * Removal or modification of this copyright notice is prohibited.
+ *
+ */
+const logger = require('lisk-service-framework').Logger();
+
+const config = require('../config');
+const purger = require('../shared/core/purge');
+
+module.exports = [
+	{
+		name: 'purge.blocks',
+		description: 'Remove old blocks',
+		schedule: '0 0 * * *', // Every day at mid-night
+		updateOnInit: true,
+		init: () => {
+			logger.debug('Scheduling delegate list init...');
+			purger.purgeBlocks(config.db.collections.blocks.purge_limit);
+		},
+		controller: async () => {
+			logger.debug('Scheduling delegate list reload...');
+			purger.purgeBlocks(config.db.collections.blocks.purge_limit);
+		},
+	},
+	{
+		name: 'purge.transactions',
+		description: 'Remove old transactions',
+		schedule: '0 0 * * *', // Every day at mid-night
+		updateOnInit: true,
+		init: () => {
+			logger.debug('Scheduling delegate list init...');
+			purger.purgeTransactions(config.db.collections.transactions.purge_limit);
+		},
+		controller: async () => {
+			logger.debug('Scheduling delegate list reload...');
+			purger.purgeTransactions(config.db.collections.transactions.purge_limit);
+		},
+	},
+];
