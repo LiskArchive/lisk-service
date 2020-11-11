4bbd12db37789951b2c841f4ac7cf3ce77ece65b
nagdahimanshu <himanshu.nagda@lightcurve.io>
2020-11-11 19:53:46 +0100

    add nonce to gateway transactions

--- services/core/shared/core/compat/sdk_v2/index.js
+++ services/core/shared/core/compat/sdk_v2/index.js
@@ -51,100 +51,86 @@ const peerStates = {
 };
 
 // Utils & helpers
-const parseAddress = (address) => {
+const parseAddress = address => {
 	if (typeof address !== 'string') return '';
 	return address.toUpperCase();
 };
 const validateAddress = address => (typeof address === 'string' && address.match(/^[0-9]{1,20}[L|l]$/g));
-const validatePublicKey = publicKey => (
-	typeof publicKey === 'string' && publicKey.match(/^([A-Fa-f0-9]{2}){32}$/g));
+const validatePublicKey = publicKey => (typeof publicKey === 'string' && publicKey.match(/^([A-Fa-f0-9]{2}){32}$/g));
 const { isProperObject } = ObjectUtilService;
 const { isEmptyArray } = ObjectUtilService;
 
 // Lisk Core API functions
-const confirmAddress = async (address) => {
+const confirmAddress = async address => {
 	if (!address || typeof address !== 'string') return false;
-	const account = await getCachedAccountByAddress(
-		parseAddress(address),
-	);
+	const account = await getCachedAccountByAddress(parseAddress(address));
 	return account && account.address === address;
 };
 
-const confirmPublicKey = async (publicKey) => {
+const confirmPublicKey = async publicKey => {
 	if (!publicKey || typeof publicKey !== 'string') return false;
 	const account = await getCachedAccountByPublicKey(publicKey);
 	return account && account.publicKey === publicKey;
 };
 
-const confirmSecondPublicKey = async (secondPublicKey) => {
+const confirmSecondPublicKey = async secondPublicKey => {
 	if (!secondPublicKey || typeof secondPublicKey !== 'string') return false;
-	const account = await getCachedAccountBySecondPublicKey(
-		secondPublicKey,
-	);
+	const account = await getCachedAccountBySecondPublicKey(secondPublicKey);
 	return account && account.secondPublicKey === secondPublicKey;
 };
 
-const confirmUsername = async (username) => {
+const confirmUsername = async username => {
 	if (!username || typeof username !== 'string') return false;
 	const result = await coreApi.getDelegates({ username });
 	if (!Array.isArray(result.data) || isEmptyArray(result.data)) return false;
 	return result.data[0].username === username;
 };
 
-const confirmAnyId = async (params) => {
+const confirmAnyId = async params => {
 	if (
-		(typeof params.username === 'string'
-			&& !(await confirmUsername(params.username)))
-		|| (typeof params.address === 'string'
-			&& !(await confirmAddress(parseAddress(params.address))))
-		|| (typeof params.publicKey === 'string'
-			&& !(await confirmPublicKey(params.publicKey)))
-		|| (typeof params.secondPublicKey === 'string'
-			&& !(await confirmSecondPublicKey(params.secondPublicKey)))
+		(typeof params.username === 'string' && !(await confirmUsername(params.username)))
+		|| (typeof params.address === 'string' && !(await confirmAddress(parseAddress(params.address))))
+		|| (typeof params.publicKey === 'string' && (!(await confirmPublicKey(params.publicKey))))
+		|| (typeof params.secondPublicKey === 'string' && (!(await confirmSecondPublicKey(params.secondPublicKey))))
 	) return false;
 
 	return true;
 };
 
-const getUsernameByAddress = async (address) => {
-	const account = await getCachedAccountByAddress(
-		parseAddress(address),
-	);
+const getUsernameByAddress = async address => {
+	const account = await getCachedAccountByAddress(parseAddress(address));
 	return account && account.username;
 };
 
-const getAddressByPublicKey = async (publicKey) => {
+const getAddressByPublicKey = async publicKey => {
 	if (!publicKey || typeof publicKey !== 'string') return '';
 	const account = await getCachedAccountByPublicKey(publicKey);
 	return account ? account.address : '';
 };
 
-const getAddressByUsername = async (username) => {
+const getAddressByUsername = async username => {
 	if (!username || typeof username !== 'string') return '';
 	const account = await getCachedAccountByUsername(username);
 	return account ? account.address : '';
 };
 
-const getAddressByAny = async (param) => {
+const getAddressByAny = async param => {
 	const paramNames = {
 		'username:': getAddressByUsername,
 		'address:': parseAddress,
 		'publickey:': getAddressByPublicKey,
 	};
 
-	const hasPrefix = (p) => !!Object.keys(paramNames).filter((item) => p.indexOf(item) === 0).length;
+	const hasPrefix = p => !!Object.keys(paramNames).filter(item => p.indexOf(item) === 0).length;
 
-	const separateParam = (p) => Object.keys(paramNames)
-			.filter((prefix) => p.indexOf(prefix) === 0)
-			.reduce(
-				(array, prefix) => [...array, prefix, p.slice(prefix.length)],
-				[],
-			);
+	const separateParam = p => Object.keys(paramNames)
+		.filter(prefix => p.indexOf(prefix) === 0)
+		.reduce((array, prefix) => [...array, prefix, p.slice(prefix.length)], []);
 
 	if (!hasPrefix(param)) {
 		const parsedAddress = parseAddress(param);
 		if (validateAddress(parsedAddress)
-		&& (await confirmAddress(parsedAddress))) return parsedAddress;
+			&& await confirmAddress(parsedAddress)) return parsedAddress;
 		if (validatePublicKey(param)) return getAddressByPublicKey(param);
 		return getAddressByUsername(param);
 	}
@@ -153,14 +139,14 @@ const getAddressByAny = async (param) => {
 	return paramNames[prefix](body);
 };
 
-const getPublicKeyByAddress = async (address) => {
+const getPublicKeyByAddress = async address => {
 	if (!address || typeof address !== 'string') return '';
 	const account = await getAccounts({ address });
 	if (!Array.isArray(account.data) || isEmptyArray(account.data)) return '';
 	return account.data[0].publicKey;
 };
 
-const getPublicKeyByUsername = async (username) => {
+const getPublicKeyByUsername = async username => {
 	if (!username || typeof username !== 'string') return '';
 	const account = await getAccounts({ username });
 	if (!Array.isArray(account.data) || isEmptyArray(account.data)) return '';
@@ -168,14 +154,14 @@ const getPublicKeyByUsername = async (username) => {
 	return publicKey;
 };
 
-const getPublicKeyByAny = async (param) => {
+const getPublicKeyByAny = async param => {
 	if (!param || typeof param !== 'string') return '';
 	if (validatePublicKey(param) && (await confirmPublicKey(param))) return param;
 	if (validateAddress(param)) return getPublicKeyByAddress(param);
 	return getPublicKeyByUsername(param);
 };
 
-const getIncomingTxsCount = async (address) => {
+const getIncomingTxsCount = async address => {
 	const result = await coreApi.getTransactions({
 		recipientId: parseAddress(address),
 		limit: 1,
@@ -190,7 +176,7 @@ const getIncomingTxsCount = async (address) => {
 	return result.meta.count;
 };
 
-const getOutgoingTxsCount = async (address) => {
+const getOutgoingTxsCount = async address => {
 	const result = await coreApi.getTransactions({
 		senderId: parseAddress(address),
 		limit: 1,
@@ -206,7 +192,7 @@ const getOutgoingTxsCount = async (address) => {
 	return result.meta.count;
 };
 
-const getForgingStats = async (address) => {
+const getForgingStats = async address => {
 	if (!validateAddress(address)) throw new Error('Missing/Invalid address');
 	try {
 		const result = await coreApi.getForgingStats(parseAddress(address));
--- services/core/shared/core/compat/sdk_v4/accounts.js
+++ services/core/shared/core/compat/sdk_v4/accounts.js
@@ -61,17 +61,13 @@ const getAccounts = async params => {
 		requestParams.address = parsedAddress;
 	}
 	if (params.publicKey && typeof params.publicKey === 'string') {
-		if (
-			!validatePublicKey(params.publicKey)
-			|| !(await confirmPublicKey(params.publicKey))
-		) {
+		if (!validatePublicKey(params.publicKey) || !(await confirmPublicKey(params.publicKey))) {
 			return {};
 		}
 		requestParams.publicKey = params.publicKey;
 	}
 	if (params.secondPublicKey && typeof params.secondPublicKey === 'string') {
-		if (
-			!validatePublicKey(params.secondPublicKey)
+		if (!validatePublicKey(params.secondPublicKey)
 			|| !(await confirmSecondPublicKey(params.secondPublicKey))
 		) {
 			return {};
