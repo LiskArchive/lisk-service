db3a1d5813681059a603850cf1917287fbb23322
Sameer Kumar Subudhi <sameer.subudhi@lightcurve.io>
2020-11-16 18:51:59 +0100

    Improve rpc tests for blocks

--- tests/integration_testnet/api/rpc/blocks.test.js
+++ tests/integration_testnet/api/rpc/blocks.test.js
@@ -14,31 +14,42 @@
  *
  */
 /* eslint-disable quotes, quote-props, comma-dangle */
-
-import config from '../../config';
-import request from '../../helpers/socketIoRpcRequest';
+const config = require('../../config');
+const { request } = require('../../helpers/socketIoRpcRequest');
 import block from './constants/blocks';
+
+const {
+	invalidParamsSchema,
+	emptyResultEnvelopeSchema,
+	emptyResponseSchema,
+	jsonRpcEnvelopeSchema,
+	metaSchema,
+} = require('../../schemas/rpcGenerics.schema');
 
 const {
 	blockSchema,
 } = require('../../schemas/block.schema');
 
-const {
-	invalidParamsSchema,
-	emptyResponseSchema,
-} = require('../../schemas/generics.schema');
-
 const wsRpcUrl = `${config.SERVICE_ENDPOINT}/rpc-v1`;
 const getBlocks = async params => request(wsRpcUrl, 'get.blocks', params);
 
 describe('Method get.blocks', () => {
+	let delegate;
+	beforeAll(async () => {
+		const response = await request(wsRpcUrl, 'get.delegates', { limit: 1 });
+		[delegate] = response.result.data;
+	});
+
 	describe('is able to retireve block lists', () => {
 		it.todo('no params -> ok');
 
 		it('limit=100 -> ok', async () => {
-			const { result } = await getBlocks({ limit: 100 });
+			const response = await getBlocks({ limit: 100 });
+			expect(response).toMap(jsonRpcEnvelopeSchema);
+			const { result } = response;
 			expect(result.data.length).toEqual(100);
-			expect(result.data[0]).toMap(blockSchema);
+			result.data.forEach(block => expect(block).toMap(blockSchema));
+			expect(result.meta).toMap(metaSchema);
 		});
 
 		it.todo('offset -> ok');
@@ -48,9 +59,12 @@ describe('Method get.blocks', () => {
 
 	describe('is able to retireve block details by block ID', () => {
 		it('known block by block ID -> ok', async () => {
-			const { result } = await getBlocks({ id: block.id });
+			const response = await getBlocks({ id: block.id });
+			expect(response).toMap(jsonRpcEnvelopeSchema);
+			const { result } = response;
 			expect(result.data.length).toEqual(1);
-			expect(result.data[0]).toMap(blockSchema, { id: block.id });
+			result.data.forEach(block => expect(block).toMap(blockSchema, { id: block.id }));
+			expect(result.meta).toMap(metaSchema);
 		});
 
 		// To Do : current response is server error, does not get correct invalid params
@@ -60,13 +74,17 @@ describe('Method get.blocks', () => {
 		});
 
 		it('too short block id -> -32602', async () => {
-			const result = await getBlocks({ id: '' }).catch(e => e);
-			expect(result).toMap(emptyResponseSchema);
+			const response = await getBlocks({ id: '' }).catch(e => e);
+			expect(response).toMap(emptyResponseSchema);
+			const { result } = response;
+			expect(result).toMap(emptyResultEnvelopeSchema);
 		});
 
 		it('invalid block id -> empty response', async () => {
-			const result = await getBlocks({ id: '12602944501676077162' }).catch(e => e);
-			expect(result).toMap(emptyResponseSchema);
+			const response = await getBlocks({ id: '12602944501676077162' }).catch(e => e);
+			expect(response).toMap(emptyResponseSchema);
+			const { result } = response;
+			expect(result).toMap(emptyResultEnvelopeSchema);
 		});
 
 		it('invalid query parameter -> -32602', async () => {
@@ -77,14 +95,18 @@ describe('Method get.blocks', () => {
 
 	describe('is able to retireve block details by height', () => {
 		it('known block by height -> ok', async () => {
-			const { result } = await getBlocks({ height: block.height });
+			const response = await getBlocks({ height: block.height });
+			expect(response).toMap(jsonRpcEnvelopeSchema);
+			const { result } = response;
 			expect(result.data.length).toEqual(1);
 			expect(result.data[0]).toMap(blockSchema, { height: block.height });
 		});
 
 		it('non-existent height -> empty response', async () => {
-			const result = await getBlocks({ height: 2000000000 });
-			expect(result).toMap(emptyResponseSchema);
+			const response = await getBlocks({ height: 2000000000 });
+			expect(response).toMap(emptyResponseSchema);
+			const { result } = response;
+			expect(result).toMap(emptyResultEnvelopeSchema);
 		});
 
 		it('height = 0 -> -32602', async () => {
@@ -93,28 +115,36 @@ describe('Method get.blocks', () => {
 		});
 
 		it('empty height -> -32602 ', async () => {
-			const result = await getBlocks({ limit: '' }).catch(e => e);
-			expect(result).toMap(emptyResponseSchema);
+			const response = await getBlocks({ limit: '' }).catch(e => e);
+			expect(response).toMap(emptyResponseSchema);
+			const { result } = response;
+			expect(result).toMap(emptyResultEnvelopeSchema);
 		});
 	});
 
 	describe('is able to retireve block lists by account address', () => {
 		it('block list by known account ID', async () => {
-			const { result } = await getBlocks({ address: '9528507096611161860L' });
+			const response = await getBlocks({ address: '9528507096611161860L' });
+			expect(response).toMap(jsonRpcEnvelopeSchema);
+			const { result } = response;
 			result.data.forEach((blockData) => {
 				expect(blockData).toMap(blockSchema, { generatorAddress: '9528507096611161860L' });
 			});
 		});
 
 		it('block list by invalid account ID returns empty list', async () => {
-			const result = await getBlocks({ address: '122233344455667L' });
-			expect(result).toMap(emptyResponseSchema);
+			const response = await getBlocks({ address: '122233344455667L' });
+			expect(response).toMap(emptyResponseSchema);
+			const { result } = response;
+			expect(result).toMap(emptyResultEnvelopeSchema);
 		});
 	});
 
 	describe('is able to retireve block lists by account public key', () => {
 		it('known block by publickey -> ok', async () => {
-			const { result } = await getBlocks({ address: 'fab7b58be4c1e9542c342023b52e9d359ea89a3af34440bdb97318273e8555f0' });
+			const response = await getBlocks({ address: 'fab7b58be4c1e9542c342023b52e9d359ea89a3af34440bdb97318273e8555f0' });
+			expect(response).toMap(jsonRpcEnvelopeSchema);
+			const { result } = response;
 			result.data.forEach((blockData) => {
 				expect(blockData)
 					.toMap(blockSchema, { generatorPublicKey: 'fab7b58be4c1e9542c342023b52e9d359ea89a3af34440bdb97318273e8555f0' });
@@ -124,7 +154,9 @@ describe('Method get.blocks', () => {
 
 	describe('is able to retireve block lists by account username', () => {
 		it('known block by username -> ok', async () => {
-			const { result } = await getBlocks({ address: 'genesis_71' });
+			const response = await getBlocks({ address: 'genesis_71' });
+			expect(response).toMap(jsonRpcEnvelopeSchema);
+			const { result } = response;
 			result.data.forEach((blockData) => {
 				expect(blockData).toMap(blockSchema, { generatorUsername: 'genesis_71' });
 			});
@@ -135,7 +167,9 @@ describe('Method get.blocks', () => {
 		it('Blocks with from...to timestamp -> ok', async () => {
 			const from = 1497855770;
 			const to = 1497855780;
-			const { result } = await getBlocks({ from, to });
+			const response = await getBlocks({ from, to });
+			expect(response).toMap(jsonRpcEnvelopeSchema);
+			const { result } = response;
 			expect(result.data.length).toEqual(2);
 			expect(result.data[0]).toMap(blockSchema);
 			result.data.forEach((blockItem) => {
