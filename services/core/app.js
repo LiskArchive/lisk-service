c637d3b8f8853f8c15187e520993ac6b42b9df48
nagdahimanshu <himanshu.nagda@lightcurve.io>
2020-11-20 17:49:22 +0100

    Revert

--- services/core/app.js
+++ services/core/app.js
@@ -46,8 +46,8 @@ nodeStatus.waitForNode().then(async () => {
 	logger.info('Found a node, initiating Lisk Core...');
 
 	app.addMethods(path.join(__dirname, 'methods'));
-	app.addJobs(path.join(__dirname, 'jobs'));
 	app.addEvents(path.join(__dirname, 'events'));
+	app.addJobs(path.join(__dirname, 'jobs'));
 
 	app.run().then(() => {
 		logger.info(`Service started ${packageJson.name}`);
