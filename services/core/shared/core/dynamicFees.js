8d4aebe19fd6dbd6617c0d188a3c713af1636389
Sameer Kumar Subudhi <sameer.subudhi@lightcurve.io>
2020-11-23 16:28:27 +0100

    Fix linting errors

--- services/core/shared/core/dynamicFees.js
+++ services/core/shared/core/dynamicFees.js
@@ -266,7 +266,6 @@ module.exports = {
 	EMAcalc,
 	getEstimateFeeByte,
 	getEstimateFeeByteCoreLogic,
-	calculateEstimateFeeByte,
 	getTransactionInstanceByType,
 	calculateBlockSize,
 	calculateFeePerByte,
