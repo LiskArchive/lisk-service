aae03b4f17d8361697a2a4e8a101a4eef3f44e0d
Sameer Kumar Subudhi <sameer.subudhi@lightcurve.io>
2020-11-25 20:24:36 +0100

    Better http tests for votes_sent (votes)

--- tests/integration_testnet/api/http/votesSent.test.js
+++ tests/integration_testnet/api/http/votesSent.test.js
@@ -19,57 +19,141 @@ const { api } = require('../../helpers/api');
 
 const baseUrl = config.SERVICE_ENDPOINT;
 const baseUrlV1 = `${baseUrl}/api/v1`;
-const accountEndpoint = `${baseUrlV1}/account`;
+// const endpoint = `${baseUrlV1}/votes_sent`;
 
 const {
 	goodRequestSchema,
 	badRequestSchema,
 	notFoundSchema,
 	wrongInputParamSchema,
-	metaSchema,
 } = require('../../schemas/httpGenerics.schema');
 
 const {
 	voteSchema,
+	metaSchema,
 } = require('../../schemas/vote.schema');
 
-describe('Votes API', () => {
-	xdescribe('GET /account/{address}/votes', () => {
-		it('existing account -> ok', async () => {
-			const response = await api.get(`${accountEndpoint}/16313739661670634666L/votes`);
-			expect(response.data[0]).toMap(voteSchema);
+[
+	`${baseUrlV1}/votes_sent`,
+	`${baseUrlV1}/votes`,
+].forEach(endpoint => {
+	describe('Votes Sent (Votes) API', () => {
+		let refDelegate;
+		beforeAll(async () => {
+			[refDelegate] = (await api.get(`${baseUrlV1}/delegates?limit=1`)).data;
 		});
 
-		it('existing user name -> ok', async () => {
-			const response = await api.get(`${accountEndpoint}/gottavoteemall/votes`);
-			expect(response.data[0]).toMap(voteSchema);
-		});
+		describe(`GET ${endpoint}`, () => {
+			it('Returns list of votes when requested for existing account by account ID', async () => {
+				const response = await api.get(`${endpoint}?address=${refDelegate.address}`);
+				expect(response).toMap(goodRequestSchema);
+				expect(response.data).toBeArrayOfSize(10);
+				response.data.forEach(vote => expect(vote).toMap(voteSchema));
+				expect(response.meta).toMap(metaSchema, {
+					address: refDelegate.address,
+					publicKey: refDelegate.publicKey,
+					username: refDelegate.username,
+				});
+			});
 
-		it('existing public key -> ok', async () => {
-			const response = await api.get(`${accountEndpoint}/d258627878a9b360fe4934218d2415d66b1ed2ef63ce097280bf02189a91468d/votes`);
-			expect(response.data[0]).toMap(voteSchema);
-		});
+			it('Returns list of votes when requested for existing account by username', async () => {
+				if (refDelegate.username) {
+					const response = await api.get(`${endpoint}?username=${refDelegate.username}`);
+					expect(response).toMap(goodRequestSchema);
+					expect(response.data).toBeArrayOfSize(10);
+					response.data.forEach(vote => expect(vote).toMap(voteSchema));
+					expect(response.meta).toMap(metaSchema, {
+						address: refDelegate.address,
+						publicKey: refDelegate.publicKey,
+						username: refDelegate.username,
+					});
+				}
+			});
 
-		it('existing account with address: -> ok', async () => {
-			const response = await api.get(`${accountEndpoint}/address:16313739661670634666L/votes`);
-			expect(response.data[0]).toMap(voteSchema);
-		});
+			it('Returns list of votes when requested for existing account by publickey', async () => {
+				if (refDelegate.publicKey) {
+					const response = await api.get(`${endpoint}?publickey=${refDelegate.publicKey}`);
+					expect(response).toMap(goodRequestSchema);
+					expect(response.data).toBeArrayOfSize(10);
+					response.data.forEach(vote => expect(vote).toMap(voteSchema));
+					expect(response.meta).toMap(metaSchema, {
+						address: refDelegate.address,
+						publicKey: refDelegate.publicKey,
+						username: refDelegate.username,
+					});
+				}
+			});
 
-		it('existing user name with username: -> ok', async () => {
-			const response = await api.get(`${accountEndpoint}/username:gottavoteemall/votes`);
-			expect(response.data[0]).toMap(voteSchema);
-		});
+			it('Returns list of votes when requested for existing account by secpubkey', async () => {
+				if (refDelegate.secondPublicKey) {
+					const response = await api.get(`${endpoint}?secpubkey=${refDelegate.secondPublicKey}`);
+					expect(response).toMap(goodRequestSchema);
+					expect(response.data).toBeArrayOfSize(10);
+					response.data.forEach(vote => expect(vote).toMap(voteSchema));
+					expect(response.meta).toMap(metaSchema, {
+						address: refDelegate.address,
+						publicKey: refDelegate.publicKey,
+						username: refDelegate.username,
+					});
+				}
+			});
 
-		it('existing public key with publickey: -> ok', async () => {
-			const response = await api.get(`${accountEndpoint}/publickey:d258627878a9b360fe4934218d2415d66b1ed2ef63ce097280bf02189a91468d/votes`);
-			expect(response.data[0]).toMap(voteSchema);
-		});
+			it('Returns list of votes when requested with offset', async () => {
+				const response = await api.get(`${endpoint}?address=${refDelegate.address}&offset=1`);
+				expect(response).toMap(goodRequestSchema);
+				expect(response.data).toBeArrayOfSize(10);
+				response.data.forEach(vote => expect(vote).toMap(voteSchema));
+				expect(response.meta).toMap(metaSchema, {
+					address: refDelegate.address,
+					publicKey: refDelegate.publicKey,
+					username: refDelegate.username,
+				});
+			});
 
-		it('wrong address -> 404', async () => {
-			const url = `${accountEndpoint}/999999999L/votes`;
-			const expectedStatus = 404;
-			const response = api.get(url, expectedStatus);
-			expect(response).resolves.toMap(notFoundSchema);
+			it('Returns list of votes when requested with limit', async () => {
+				const response = await api.get(`${endpoint}?address=${refDelegate.address}&limit=1`);
+				expect(response).toMap(goodRequestSchema);
+				expect(response.data).toBeArrayOfSize(1);
+				response.data.forEach(vote => expect(vote).toMap(voteSchema));
+				expect(response.meta).toMap(metaSchema, {
+					address: refDelegate.address,
+					publicKey: refDelegate.publicKey,
+					username: refDelegate.username,
+				});
+			});
+
+			it('Returns list of votes when requested with offset & limit', async () => {
+				const response = await api.get(`${endpoint}?address=${refDelegate.address}&offset=5&limit=1`);
+				expect(response).toMap(goodRequestSchema);
+				expect(response.data).toBeArrayOfSize(1);
+				response.data.forEach(vote => expect(vote).toMap(voteSchema));
+				expect(response.meta).toMap(metaSchema, {
+					address: refDelegate.address,
+					publicKey: refDelegate.publicKey,
+					username: refDelegate.username,
+				});
+			});
+
+			it('Returns BAD_REQUEST (400) when requested with limit = 0', async () => {
+				const response = await api.get(`${endpoint}?limit=0`, 400);
+				expect(response).toMap(badRequestSchema);
+			});
+
+			it('Returns NOT_FOUND (404) when requested with wrong address', async () => {
+				const response = await api.get(`${endpoint}?address=999999999L`, 404);
+				expect(response).toMap(notFoundSchema);
+			});
+
+			it('Returns BAD_REQUEST (400) when requested with unsupported param', async () => {
+				const response = await api.get(`${endpoint}?unsupported_param=999999999L`, 400);
+				expect(response).toMap(wrongInputParamSchema);
+			});
+
+			// TODO: Fails CI pipeline
+			xit('Returns BAD_REQUEST (400) when requested without required params', async () => {
+				const response = await api.get(`${endpoint}`, 400);
+				expect(response).toMap(badRequestSchema);
+			});
 		});
 	});
 });
