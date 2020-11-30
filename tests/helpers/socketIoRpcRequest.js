6bd3ba43a90663fa31d9be97ce4f27cd392ccabb
Sameer Kumar Subudhi <sameer.subudhi@lightcurve.io>
2020-11-30 13:01:38 +0100

    Fix linting errors

--- tests/helpers/socketIoRpcRequest.js
+++ tests/helpers/socketIoRpcRequest.js
@@ -19,7 +19,7 @@ import config from '../config';
 
 const wsRpcUrl = `${config.SERVICE_ENDPOINT}/rpc-v1`;
 
-const request = (endpoint, method, params) => new Promise((resolve, reject) => {
+const request = (endpoint, method, params) => new Promise(resolve => {
 	const socket = io(endpoint, { forceNew: true, transports: ['websocket'] });
 
 	socket.emit('request', { jsonrpc: '2.0', method, params }, answer => {
