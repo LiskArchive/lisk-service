8168a59386dc2e88258c237e0124c28fa28e4832
nagdahimanshu <himanshu.nagda@lightcurve.io>
2020-12-02 16:22:42 +0100

    Add secondary indexes in redis for supported query params

--- services/core/shared/database/redisdb.js
+++ services/core/shared/database/redisdb.js
@@ -33,11 +33,35 @@ const getDbInstance = async (collectionName) => {
     };
 
     const writeBatch = async (docs) => {
-        await Promise
-            .all(docs.map((doc) => db.hmset(collectionName, doc.id, JSON.stringify(doc))));
+        await Promise.all(docs.map(doc => {
+            db.hmset(collectionName, doc.id, JSON.stringify(doc));
+
+            // For accounts, delegates
+            if (doc.publicKey) db.zadd(collectionName, doc.publicKey, doc.id);
+            if (doc.secondPublicKey) db.zadd(collectionName, doc.secondPublicKey, doc.id);
+            if (doc.username) db.zadd(collectionName, doc.username, doc.id);
+
+            // For blocks, transactions
+            if (doc.generatorAddress) db.zadd(collectionName, doc.generatorAddress, doc.id);
+            if (doc.generatorPublicKey) db.zadd(collectionName, doc.generatorPublicKey, doc.id);
+            if (doc.generatorUsername) db.zadd(collectionName, doc.generatorUsername, doc.id);
+            if (doc.height) db.zadd(collectionName, doc.height, doc.id);
+            if (doc.timestamp) db.zadd(collectionName, doc.timestamp, doc.id);
+
+            // For transactions
+            if (doc.blockId) db.zadd(collectionName, Number(doc.blockId), doc.id);
+            if (doc.type) db.zadd(collectionName, doc.type, doc.id);
+            if (doc.senderId) db.zadd(collectionName, doc.senderId, doc.id);
+            if (doc.senderPublicKey) db.zadd(collectionName, doc.senderPublicKey, doc.id);
+            if (doc.senderSecondPublicKey) db.zadd(collectionName, doc.senderSecondPublicKey, doc.id);
+            if (doc.recipientId) db.zadd(collectionName, doc.recipientId, doc.id);
+            if (doc.recipientPublicKey) db.zadd(collectionName, doc.recipientPublicKey, doc.id);
+
+            // For peers
+            if (doc.ip) db.zadd(collectionName, doc.ip, doc.id);
+        }));
     };
 
-    // const findById = async (id) => await db.get(id);
     const findById = (id) => new Promise(resolve => {
         db.hgetall(collectionName, async (err, result) => {
             const res = {};
