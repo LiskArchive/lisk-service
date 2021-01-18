bda30519366fec3f8b97ecbaf493606f6dfafe47
nagdahimanshu <himanshu.nagda@lightcurve.io>
2021-01-18 13:33:44 +0100

    Logic to resolve current reward

--- services/core/shared/core/compat/sdk_v5/network.js
+++ services/core/shared/core/compat/sdk_v5/network.js
@@ -17,18 +17,25 @@ const coreApi = require('./coreApi');
 
 const getNetworkStatus = async () => {
 	const status = await coreApi.getNetworkStatus();
+	const { offset } = status.data.genesisConfig.rewards;
+	const { distance } = status.data.genesisConfig.rewards;
 	status.data.operations = [];
 	status.data.registeredModules.forEach(liskModule => {
 		if (liskModule.transactionAssets.length) {
 			status.data.operations = status.data.operations
-			.concat(
-				liskModule.transactionAssets.map(asset => {
-				const id = String(liskModule.id).concat(':').concat(asset.id);
-				const name = liskModule.name.concat(':').concat(asset.name);
-				return { id, name };
-			}));
+				.concat(
+					liskModule.transactionAssets.map(asset => {
+						const id = String(liskModule.id).concat(':').concat(asset.id);
+						const name = liskModule.name.concat(':').concat(asset.name);
+						return { id, name };
+					}));
 		}
 	});
+	const rewardIndex = Math.floor((status.data.height - offset) / distance);
+	const finalRewardIndex = rewardIndex >= status.data.genesisConfig.rewards.milestones.length
+		? status.data.genesisConfig.rewards.milestones.length - 1 : rewardIndex;
+	status.data.currentReward = finalRewardIndex >= 0
+		? status.data.genesisConfig.rewards.milestones[finalRewardIndex] : 0;
 	status.data.registeredModules = status.data.registeredModules.map(item => item.name);
 	status.data.lastUpdate = Math.floor(Date.now() / 1000);
 	return status;
