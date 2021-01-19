f772aa058fc6e6d12be7703217006629fe2c6318
Sameer Kumar Subudhi <sameer.subudhi@lightcurve.io>
2021-01-19 10:38:32 +0100

    Add gateway mapping for v2 account

---
+++ services/gateway/sources/version2/mappings/account.js
@@ -0,0 +1,81 @@
+/*
+ * LiskHQ/lisk-service
+ * Copyright © 2021 Lisk Foundation
+ *
+ * See the LICENSE file at the top-level directory of this distribution
+ * for licensing information.
+ *
+ * Unless otherwise agreed in a custom licensing agreement with the Lisk Foundation,
+ * no part of this software, including this file, may be copied, modified,
+ * propagated, or distributed except according to the terms contained in the
+ * LICENSE file.
+ *
+ * Removal or modification of this copyright notice is prohibited.
+ *
+ */
+module.exports = {
+	summary: {
+		address: '=,string',
+		balance: '=,string',
+		username: '=,string',
+		publicKey: '=,string',
+		isDelegate: '=,boolean',
+		isMultisignature: '=,boolean',
+	},
+	knowledge: {
+		owner: '=,string',
+		description: '=,string',
+	},
+	token: {
+		balance: '=,string',
+	},
+	sequence: {
+		nonce: '=,string',
+	},
+	keys: {
+		numberOfSignatures: '=,number',
+		mandatoryKeys: '=',
+		optionalKeys: '=',
+		members: [
+			{
+				address: '=,string',
+				publicKey: '=,string',
+				isMandatory: '=,boolean',
+			}
+		],
+		memberships: [
+			{
+				address: '=,string',
+				publicKey: '=,string',
+				username: '=,string',
+			}
+		],
+	},
+	dpos: {
+		delegate: {
+			username: '=,string',
+			pomHeights: ['pomHeights', {
+				start: '=,string',
+				end: '=,string',
+			}],
+			consecutiveMissedBlocks: '=,number',
+			lastForgedHeight: '=,number',
+			isBanned: '=,boolean',
+			totalVotesReceived: '=,string',
+			missedBlocks: '=,number',
+			producedBlocks: '=,number',
+			productivity: '=,string',
+			rank: '=,number',
+			rewards: '=,string',
+		},
+		sentVotes: ['sentVotes', {
+			delegateAddress: '=,string',
+			amount: '=,string',
+		}],
+		unlocking: ['unlocking', {
+			delegateAddress: '=,string',
+			amount: '=,string',
+			unvoteHeight: '=,number',
+		}],
+	}
+};
