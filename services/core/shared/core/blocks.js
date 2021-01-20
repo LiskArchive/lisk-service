9a15b30112face75ca9bdb3e5fb8acb3509884f0
Michal Tuleja <michal@lightcurve.io>
2021-01-20 16:18:55 +0100

    Wrap block update in try-catch

--- services/core/shared/core/blocks.js
+++ services/core/shared/core/blocks.js
@@ -125,9 +125,13 @@ const preloadBlocksByPage = async (n) => {
 const reloadBlocks = async (n) => preloadBlocksByPage(n);
 
 const performLastBlockUpdate = async () => {
-	const block = await getBlocks({ limit: 1, sort: 'height:desc' });
-	setLastBlock(block.data[0]);
-	logger.debug(`Current block height: ${block.data[0].height}  (id=${block.data[0].id})`);
+	try {
+		const block = await getBlocks({ limit: 1, sort: 'height:desc' });
+		setLastBlock(block.data[0]);
+		logger.debug(`Current block height: ${block.data[0].height}  (id=${block.data[0].id})`);
+	} catch (err) {
+		logger.error(err);
+	}
 };
 
 const initBlocks = (async () => {
