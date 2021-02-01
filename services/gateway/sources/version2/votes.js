aa6bc9743ae9ae4ec6b40003c6d07cd8a1819d49
Sameer Kumar Subudhi <sameer.subudhi@lightcurve.io>
2021-02-01 13:40:19 +0100

    Add API endpoints for v2 votes_sent/received

---
+++ services/gateway/sources/version2/votes.js
@@ -0,0 +1,44 @@
+/*
+ * LiskHQ/lisk-service
+ * Copyright © 2021 Lisk Foundation
+ *
+ * See the LICENSE file at the top-level directory of this distribution
+ * for licensing information.
+ *
+ * Unless otherwise agreed in a custom licensing agreement with the Lisk Foundation,
+ * no part of this software, including this file, may be copied, modified,
+ * propagated, or distributed except according to the terms contained in the
+ * LICENSE file.
+ *
+ * Removal or modification of this copyright notice is prohibited.
+ *
+ */
+const vote = require('./mappings/vote');
+
+module.exports = {
+	type: 'moleculer',
+	method: 'core.votes',
+	params: {
+		address: '=,string',
+		username: '=,string',
+		publicKey: 'publickey,string',
+		limit: '=,number',
+		offset: '=,number',
+	},
+	definition: {
+		data: {
+			account: {
+				address: '=,string',
+				username: '=,string',
+				votesUsed: '=,number',
+			},
+			votes: ['votes', vote]
+		},
+		meta: {
+			count: '=,number',
+			offset: '=,number',
+			total: '=,number',
+		},
+		links: {},
+	},
+};
