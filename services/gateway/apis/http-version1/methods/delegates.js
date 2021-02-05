01b562fa3e158580fcd55f0affe2095756c495ec
Sameer Kumar Subudhi <sameer.subudhi@lightcurve.io>
2021-02-05 11:26:57 +0100

    Allow delegate filtering based on multiple status values

--- services/gateway/apis/http-version1/methods/delegates.js
+++ services/gateway/apis/http-version1/methods/delegates.js
@@ -30,7 +30,7 @@ module.exports = {
 		status: {
 			optional: true,
 			type: 'string',
-			pattern: /\b(?:active|standby|banned|punished|non-eligible)\b/
+			pattern: /^(?:\b(?:active|standby|banned|punished|non-eligible)\b|\b(?:active|standby|banned|punished|non-eligible|,){3,}\b){1}$/,
 		},
 		search: { optional: true, type: 'string', min: 1 },
 		limit: { optional: true, type: 'number', min: 1, max: 101, default: 10 },
