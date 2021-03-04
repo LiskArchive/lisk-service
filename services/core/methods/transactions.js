0cf7a4c29cd7d6d2f211201622788ffc7cb60dda
Sameer Kumar Subudhi <sameer.subudhi@lightcurve.io>
2021-03-04 09:36:23 +0100

    Add param

--- services/core/methods/transactions.js
+++ services/core/methods/transactions.js
@@ -76,6 +76,8 @@ module.exports = [
 	{
 		name: 'transactions.post',
 		controller: postTransactions,
-		params: {},
+		params: {
+			transaction: { type: 'string', optional: false },
+		},
 	},
 ];
