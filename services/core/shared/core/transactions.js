3b22ddd1bdfbc4f662b0fab684ea4f7eb798cb5f
Sameer Kumar Subudhi <sameer.subudhi@lightcurve.io>
2021-03-05 19:17:18 +0100

    Separate 'includePending' from params

--- services/core/shared/core/transactions.js
+++ services/core/shared/core/transactions.js
@@ -38,7 +38,6 @@ const mergeTransactions = async (params) => {
 	const limit = params.limit || 10;
 
 	const pendingTxs = await getPendingTransactions(params);
-	delete params.includePending;
 	const transactions = await coreApi.getTransactions(params);
 	allTransactions.data = pendingTxs.data.concat(transactions.data).slice(offset, offset + limit);
 
@@ -54,7 +53,11 @@ const getTransactions = async params => {
 		data: [],
 		meta: {},
 	};
-	const response = params.includePending
+
+	const { includePending, ...remParams } = params;
+	params = remParams;
+
+	const response = includePending
 		? await mergeTransactions(params)
 		: await coreApi.getTransactions(params);
 	if (response.data) transactions.data = response.data;
