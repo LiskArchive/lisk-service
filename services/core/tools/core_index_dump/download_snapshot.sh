b70463a3edee8f6b71601e8a3119e81014b34039
Michal Tuleja <michal@lightcurve.io>
2021-03-10 20:26:44 +0100

    Update gzip check

--- services/core/tools/core_index_dump/download_snapshot.sh
+++ services/core/tools/core_index_dump/download_snapshot.sh
@@ -4,6 +4,7 @@ WGET_CMD=$(which wget)
 GZIP_CMD=$(which gzip)
 
 if [ -z $WGET_CMD ]; then echo 'The command wget is not installed in the system'; exit 1; fi
+if [ -z $GZIP_CMD ]; then echo 'The command gzip is not installed in the system'; exit 1; fi
 
 $WGET_CMD -O index_snapshot.sql.gz https://static-data.lisk.io/index_snapshot.sql.gz
 
