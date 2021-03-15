1d5b2e1ddea980e83c0482d7fd6ce5d9a1c04e7a
Sameer Kumar Subudhi <sameer.subudhi@lightcurve.io>
2021-03-15 12:18:55 +0100

    Add entrypoint script to import blockchain from snapshots

---
+++ docker/lisk-core/entrypoint.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+/home/lisk/node_modules/.bin/lisk-core blockchain:import /tmp/snapshots/blockchain.db.tar.gz
+/home/lisk/node_modules/.bin/lisk-core forger-info:import /tmp/snapshots/forger.db.tar.gz
+
+/home/lisk/node_modules/.bin/lisk-core start --network=devnet --api-ws --api-ws-port=5001 --enable-forger-plugin --enable-http-api-plugin --http-api-plugin-port=4000 --http-api-plugin-whitelist=172.16.0.0/12
