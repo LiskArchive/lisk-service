ae21e6540f1706263d5770a72520279db011198d
Sameer Kumar Subudhi <sameer.subudhi@lightcurve.io>
2021-03-17 15:43:08 +0100

    Enable accounts integration tests

--- tests/integration/api_v2/http/accounts.test.js
+++ tests/integration/api_v2/http/accounts.test.js
@@ -34,7 +34,7 @@ const baseUrlV2 = `${baseUrl}/api/v2`;
 const endpoint = `${baseUrlV2}/accounts`;
 const accountEndpoint = `${baseUrlV2}/account`;
 
-xdescribe('Accounts API', () => {
+describe('Accounts API', () => {
 	let delegate;
 	beforeAll(async () => {
 		const response = await api.get(`${baseUrlV2}/accounts?isDelegate=true&limit=1`);
