1619cad936b96902644d7913be446f40e10ab235
nagdahimanshu <himanshu.nagda@lightcurve.io>
2021-03-18 10:26:22 +0100

    Schema for forgers added to swagger doc

--- services/gateway/apis/http-version2/methods/forgers.js
+++ services/gateway/apis/http-version2/methods/forgers.js
@@ -15,6 +15,7 @@
  */
 const forgersSource = require('../../../sources/version2/forgers');
 const envelope = require('../../../sources/version2/mappings/stdEnvelope');
+const { transformParams, response } = require('../../swagger/utils');
 
 module.exports = {
     version: '2.0',
@@ -25,6 +26,26 @@ module.exports = {
         offset: { optional: true, min: 0, type: 'number', default: 0 },
     },
     tags: ['Forgers'],
+    get schema() {
+		const forgerSchema = {};
+		forgerSchema[this.swaggerApiPath] = { get: {} };
+		forgerSchema[this.swaggerApiPath].get.tags = this.tags;
+		forgerSchema[this.swaggerApiPath].get.summary = 'Requests next forgers list';
+		forgerSchema[this.swaggerApiPath].get.parameters = transformParams('forgers', this.params);
+		forgerSchema[this.swaggerApiPath].get.responses = {
+			200: {
+				description: 'list of next forgers',
+				schema: {
+					type: 'array',
+					items: {
+						$ref: '#/definitions/forgers',
+					},
+				},
+			},
+		};
+		Object.assign(forgerSchema[this.swaggerApiPath].get.responses, response);
+		return forgerSchema;
+	},
     source: forgersSource,
     envelope,
 };
