f7d8801bf1f3db0d68051a6f855ae7cabfd45f39
Sameer Kumar Subudhi <sameer.subudhi@lightcurve.io>
2021-03-23 18:40:25 +0100

    Update peer response mapper

--- services/gateway/sources/version2/mappings/peer.js
+++ services/gateway/sources/version2/mappings/peer.js
@@ -1,6 +1,6 @@
 /*
  * LiskHQ/lisk-service
- * Copyright © 2019 Lisk Foundation
+ * Copyright © 2021 Lisk Foundation
  *
  * See the LICENSE file at the top-level directory of this distribution
  * for licensing information.
@@ -16,22 +16,13 @@
 module.exports = {
 	ip: '=,string',
 	port: '=,number',
-	os: '=,string',
 	networkVersion: '=,string',
 	state: '=,string',
 	height: '=,number',
 	networkIdentifier: '=,string',
-	nonce: '=,string',
-	hostname: '=,string',
 	location: {
-		city: 'location.city,string',
 		countryCode: 'location.country_code,string',
-		ip: 'location.ip,string',
 		latitude: 'location.latitude,string',
 		longitude: 'location.longitude,string',
-		regionCode: 'location.region_code,string',
-		regionName: 'location.region_name,string',
-		timeZone: 'location.time_zone,string',
-		zipCode: 'location.zip_code',
 	},
 };
