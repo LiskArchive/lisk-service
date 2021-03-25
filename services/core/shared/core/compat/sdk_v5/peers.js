72a716a4b80325965125b90f3c022d4203fcc82c
nagdahimanshu <himanshu.nagda@lightcurve.io>
2021-03-25 11:16:05 +0100

    Update peers

--- services/core/shared/core/compat/sdk_v5/peers.js
+++ services/core/shared/core/compat/sdk_v5/peers.js
@@ -21,8 +21,8 @@ const peerStates = {
 };
 
 const getPeers = async () => {
-	const orgConnectedPeers = await coreApi.getPeers(peerStates.CONNECTED);
-	const connectedPeers = orgConnectedPeers.data.map(orgPeer => {
+	const connectedPeers = await coreApi.getPeers(peerStates.CONNECTED);
+	connectedPeers.data = connectedPeers.data.map(orgPeer => {
 		const { ipAddress, options: { height }, ...peer } = orgPeer;
 
 		peer.state = peerStates.CONNECTED;
