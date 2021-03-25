d76b4624fdd4bfadc220b061b02b9925e2c1886c
nagdahimanshu <himanshu.nagda@lightcurve.io>
2021-03-25 16:16:16 +0100

    Update gateway to match specs

--- services/gateway/apis/http-version2/methods/votesSent.js
+++ services/gateway/apis/http-version2/methods/votesSent.js
@@ -26,7 +26,7 @@ module.exports = {
 	params: {
 		address: { optional: true, type: 'string', min: 3, max: 41 },
 		username: { optional: true, type: 'string', min: 3, max: 20 },
-		publickey: { optional: true, type: 'string', min: 64, max: 64 },
+		publicKey: { optional: true, type: 'string', min: 64, max: 64 },
 		limit: { optional: true, min: 1, max: 100, type: 'number', default: 10 },
 		offset: { optional: true, min: 0, type: 'number', default: 0 },
 	},
@@ -34,7 +34,7 @@ module.exports = {
 	validParamPairings: [
 		['address'],
 		['username'],
-		['publickey'],
+		['publicKey'],
 	],
 	get schema() {
 		const votesSchema = {};
