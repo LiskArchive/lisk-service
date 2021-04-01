cd67490da3f5de63838e37ba3ef1b81de657889e
Sameer Kumar Subudhi <sameer.subudhi@lightcurve.io>
2021-04-01 18:42:53 +0200

    Wrap batchInsert within a transaction

--- services/core/shared/indexdb/mysql.js
+++ services/core/shared/indexdb/mysql.js
@@ -134,7 +134,7 @@ const getDbInstance = async (tableName, tableConfig, connEndpoint = config.endpo
 
 		try {
 			const chunkSize = 1000;
-			const ids = await knex.batchInsert(tableName, rows, chunkSize);
+			const ids = await knex.transaction(trx => knex.batchInsert(tableName, rows, chunkSize).transacting(trx));
 			logger.debug(`${rows.length} row(s) inserted/updated in '${tableName}' table`);
 			return ids;
 		} catch (error) {
