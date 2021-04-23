39293c42167d7f665a717f99db19bff082c4c1d4
nagdahimanshu <himanshu.nagda@lightcurve.io>
2021-04-23 18:05:16 +0200

    Update core features status: block index and stats

---
+++ services/core/coreReady.js
@@ -0,0 +1,40 @@
+/*
+ * LiskHQ/lisk-service
+ * Copyright © 2021 Lisk Foundation
+ *
+ * See the LICENSE file at the top-level directory of this distribution
+ * for licensing information.
+ *
+ * Unless otherwise agreed in a custom licensing agreement with the Lisk Foundation,
+ * no part of this software, including this file, may be copied, modified,
+ * propagated, or distributed except according to the terms contained in the
+ * LICENSE file.
+ *
+ * Removal or modification of this copyright notice is prohibited.
+ *
+ */
+const logger = require('lisk-service-framework').Logger();
+const signals = require('./shared/signals');
+
+const features = {
+    isIndexReady: false,
+    isTransactionStatsReady: false,
+};
+
+const coreReadinessStatus = async () => {
+    // signals.get('blockIndexReady').add(() => {
+    //     logger.debug('Indexing finished');
+    //     features.isIndexReady = true;
+    // });
+
+    signals.get('transactionStatsReady').add((days) => {
+        logger.debug('Transaction stats calculated for:', `${days}days`);
+        features.isTransactionStatsReady = true;
+        features.isIndexReady = true;
+    });
+    return features;
+};
+
+module.exports = {
+    coreReadinessStatus,
+};
