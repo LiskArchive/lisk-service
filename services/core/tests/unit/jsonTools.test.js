1f637458ea5e674ea5c86bff94b1375a4274472c
nagdahimanshu <himanshu.nagda@lightcurve.io>
2021-05-04 13:57:30 +0200

    Added test case for complex object parsing

--- services/core/tests/unit/jsonTools.test.js
+++ services/core/tests/unit/jsonTools.test.js
@@ -17,6 +17,19 @@ const {
     parseToJSONCompatObj,
 } = require('../../shared/jsonTools');
 
+const testObject = {
+    asset: {
+        amount: BigInt(150000000000),
+        recipientAddress: Buffer.from('b5a1052cd5bb1259202b291512387292e2d4a8e9', 'hex'),
+    },
+    amount: BigInt(150000000000),
+    recipientAddress: Buffer.from('b5a1052cd5bb1259202b291512387292e2d4a8e9', 'hex'),
+    assetID: 0,
+    fee: BigInt(1000000),
+    id: Buffer.from('b11e14378c8afdd1ba68a305d6168eb710a1c9ff20da1836ddf68f0139600bc9', 'hex'),
+    moduleID: 2,
+    nonce: BigInt(61),
+};
 
 describe('jsonTools tests', () => {
     it('Parse buffer to string', async () => {
@@ -24,5 +37,23 @@ describe('jsonTools tests', () => {
         const parsedResult = parseToJSONCompatObj(bufferData);
         expect(typeof parsedResult).toBe('string');
         expect(parsedResult).toBe('e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855');
+    });
+
+    it('Parse object', async () => {
+        const parsedResult = parseToJSONCompatObj(testObject);
+        expect(parsedResult).toBeInstanceOf(Object);
+        expect(parsedResult).toMatchObject({
+            asset: {
+                amount: '150000000000',
+                recipientAddress: 'b5a1052cd5bb1259202b291512387292e2d4a8e9',
+            },
+            amount: '150000000000',
+            recipientAddress: 'b5a1052cd5bb1259202b291512387292e2d4a8e9',
+            assetID: 0,
+            fee: '1000000',
+            id: 'b11e14378c8afdd1ba68a305d6168eb710a1c9ff20da1836ddf68f0139600bc9',
+            moduleID: 2,
+            nonce: '61',
+        });
     });
 });
