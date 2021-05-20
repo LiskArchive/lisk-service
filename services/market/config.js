aeafb6f0b2b3d65c30165aa35ca2cd06cbb11bfe
nagdahimanshu <himanshu.nagda@lightcurve.io>
2021-05-20 12:06:00 +0200

    Resolve conflicts

--- services/market/config.js
+++ services/market/config.js
@@ -38,12 +38,6 @@ config.log.level = process.env.SERVICE_LOG_LEVEL || 'info';
 config.log.console = process.env.SERVICE_LOG_CONSOLE || 'false';
 config.log.stdout = process.env.SERVICE_LOG_STDOUT || 'true';
 
-/**
- * External endpoints
- */
-config.endpoints.binance = 'https://api.binance.com/api/v3';
-config.endpoints.redis = process.env.SERVICE_MARKET_REDIS || 'redis://localhost:6379/2';
-
 /*
  * Configurable outputs
  * log.file   - outputs to a file (ie. ./logs/app.log)
@@ -60,5 +54,6 @@ config.log.docker_host = process.env.DOCKER_HOST || 'local';
  */
 config.endpoints.redis = process.env.SERVICE_MARKET_REDIS || 'redis://localhost:6379/2';
 config.endpoints.bittrex = 'https://api.bittrex.com/v3';
+config.endpoints.binance = 'https://api.binance.com/api/v3';
 
 module.exports = config;
