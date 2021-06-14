bf6ad53d1721420bd0ef964b9253e0bc1964d6aa
nagdahimanshu <himanshu.nagda@lightcurve.io>
2021-06-14 13:01:14 +0200

    Update transaction event test

--- tests/integration/api_v2/subscribeApi/transactionEvent.test.js
+++ tests/integration/api_v2/subscribeApi/transactionEvent.test.js
@@ -38,7 +38,7 @@ const endpoint = `${config.SERVICE_ENDPOINT_RPC}/blockchain`;
 
 describe('Test subscribe API transaction confirmed event', () => {
 	it('event update.transactions.confirmed', async () => {
-		const postTransactions = await api.post(`${baseUrlV2}/transactions?transaction=0802100018002080c2d72f2a200fe9a3f1a21b5530f27f87a414b549e79a940bf24fdf2b2f05e7f22aeeecc86a32270880c2d72f1214ab0041a7d3f7b2c290b5b834d46bdc7b7eb858151a0a73656e6420746f6b656e3a40ff340ed1eb838a4db3b9c225a39406b577ff91e0e5bb17dfd0a8b15e300aad5f153a0f4a03d093f24234682864dac7b0578c6dc4f96713ccba5f172da9243c07`);
+		const postTransactions = await api.post(`${baseUrlV2}/transactions?transaction=0802100018c5012080c2d72f2a200fe9a3f1a21b5530f27f87a414b549e79a940bf24fdf2b2f05e7f22aeeecc86a32270880c2d72f1214ab0041a7d3f7b2c290b5b834d46bdc7b7eb858151a0a73656e6420746f6b656e3a40e9e1af605cf2e562a525b9785d30d02e91be18d02f35050b1265156fb468ad2a0ad794b96feabae51222a50826b2eaf4d730e7cde4f94badc6b09f15a51a090d`);
 		expect(postTransactions).toMap(postTransactionSchema);
 		const response = await subscribeAndReturn(endpoint, 'update.transactions.confirmed');
 		expect(response).toMap(goodRequestSchema);
