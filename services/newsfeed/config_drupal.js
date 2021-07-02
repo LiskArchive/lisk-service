73774f64d03def02283742fe7d8eea9e404a43f1
Sameer Kumar Subudhi <sameer.subudhi@lightcurve.io>
2021-07-02 21:06:22 +0200

    Add missing interval config for Drupal

--- services/newsfeed/config_drupal.js
+++ services/newsfeed/config_drupal.js
@@ -17,6 +17,7 @@ const getDrupalConfig = ({ enabled, name, url, filter }) => ({
 	name,
 	enabled,
 	url,
+	interval: 5 * 60, // seconds
 	expiryInDays: Number(process.env.NEWSFEED_DB_EXPIRY_DAYS || 30),
 	table: 'newsfeed',
 	newsfeed: {
