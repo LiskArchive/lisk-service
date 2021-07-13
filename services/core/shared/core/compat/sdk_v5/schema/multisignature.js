c441d58a399d50a58ccd19b2a5b287ee6ab01c55
nagdahimanshu <himanshu.nagda@lightcurve.io>
2021-07-13 13:07:48 +0200

    Add schema for multisignature

---
+++ services/core/shared/core/compat/sdk_v5/schema/multisignature.js
@@ -0,0 +1,31 @@
+/*
+ * LiskHQ/lisk-service
+ * Copyright © 2021 Lisk Foundation
+ *
+ * See the LICENSE file at the top-level directory of this distribution
+ * for licensing information.
+ *
+ * Unless otherwise agreed in a custom licensing agreement with the Lisk Foundation,
+ * no part of this software, including this file, may be copied, modified,
+ * propagated, or distributed except according to the terms contained in the
+ * LICENSE file.
+ *
+ * Removal or modification of this copyright notice is prohibited.
+ *
+ */
+module.exports = {
+	primaryKey: 'id',
+	schema: {
+		id: { type: 'string' },
+		groupAddress: { type: 'string' },
+		memberAddress: { type: 'string' },
+		isMandatory: { type: 'boolean' },
+	},
+	indexes: {
+		id: { type: 'key' },
+		groupAddress: { type: 'key' },
+		memberAddress: { type: 'key' },
+		isMandatory: { type: 'key' },
+	},
+	purge: {},
+};
