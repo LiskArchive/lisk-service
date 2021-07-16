27f2e1b40a35dc4cfb5161383a16a6b40059fdc3
Sameer Kumar Subudhi <sameer.subudhi@lightcurve.io>
2021-07-16 10:07:07 +0200

    Ensure only one client is instantiated at a given time

--- services/core/shared/core/compat/common/wsRequest.js
+++ services/core/shared/core/compat/common/wsRequest.js
@@ -15,24 +15,33 @@
  */
 const { Logger, Signals } = require('lisk-service-framework');
 const { createWSClient } = require('@liskhq/lisk-api-client');
+
+const waitForIt = require('../../../waitForIt');
 const config = require('../../../../config');
 
 const logger = Logger();
 
 const liskAddress = config.endpoints.liskWs;
 let clientCache;
+let isInstantiating = false;
 
-const getApiClient = async () => {
+const instantiateClient = async () => {
 	try {
-		if (!clientCache || !clientCache._channel.isAlive) {
-			if (clientCache) await clientCache.disconnect();
-			clientCache = await createWSClient(`${liskAddress}/ws`);
+		if (!isInstantiating) {
+			if (!clientCache || !clientCache._channel.isAlive) {
+				if (clientCache) {
+					await clientCache.disconnect();
+					isInstantiating = true;
+				}
+				clientCache = await createWSClient(`${liskAddress}/ws`);
+				isInstantiating = false;
 
-			// Inform listeners about the newly created ApiClient
-			logger.debug(`============== 'newApiClient' signal: ${Signals.get('newApiClient')} ==============`);
-			Signals.get('newApiClient').dispatch();
+				// Inform listeners about the newly created ApiClient
+				logger.debug(`============== 'newApiClient' signal: ${Signals.get('newApiClient')} ==============`);
+				Signals.get('newApiClient').dispatch();
+			}
+			return clientCache;
 		}
-		return clientCache;
 	} catch (err) {
 		logger.error(`Error instantiating WS client to ${liskAddress}`);
 		logger.error(err.message);
@@ -44,6 +53,10 @@ const getApiClient = async () => {
 		};
 	}
 };
+
+const RETRY_INTERVAL = 50; // ms
+
+const getApiClient = () => waitForIt(instantiateClient, RETRY_INTERVAL);
 
 module.exports = {
 	getApiClient,
