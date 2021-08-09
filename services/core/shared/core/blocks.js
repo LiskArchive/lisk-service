42120e0c85fca331b7ac8cf58829ea12454a5242
Sameer Kumar Subudhi <sameer.subudhi@lightcurve.io>
2021-08-09 23:04:35 +0530

    Add error check for 'loadAllPendingTransactions'

--- services/core/shared/core/blocks.js
+++ services/core/shared/core/blocks.js
@@ -27,6 +27,7 @@ const setLastBlock = block => {
 	if (block && block.height && block.height > lastBlock.height) lastBlock = block;
 	else if (!lastBlock.height) lastBlock = block;
 };
+
 const getLastBlock = () => lastBlock;
 const waitForLastBlock = () => new Promise((resolve) => {
 	const checkLastBlock = (interval) => {
@@ -132,7 +133,7 @@ const performLastBlockUpdate = (newBlock) => {
 		logger.debug(`Setting last block to height: ${newBlock.height} (id: ${newBlock.id})`);
 		setLastBlock(newBlock);
 	} catch (err) {
-		logger.error(err);
+		logger.error(`Error occured when performing last block update:\n${err.stack}`);
 	}
 };
 
