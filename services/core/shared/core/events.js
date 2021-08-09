b407370a436637a2605766e9aeaf518e8976420f
Sameer Kumar Subudhi <sameer.subudhi@lightcurve.io>
2021-08-09 19:10:07 +0530

    Error handling for events

--- services/core/shared/core/events.js
+++ services/core/shared/core/events.js
@@ -40,41 +40,57 @@ const logger = Logger();
 
 const events = {
 	newBlock: async (newBlock) => {
-		logger.debug(`New block arrived: ${newBlock.id} at height ${newBlock.height}`);
-		performLastBlockUpdate(newBlock);
+		try {
+			logger.debug(`New block arrived: ${newBlock.id} at height ${newBlock.height}`);
+			performLastBlockUpdate(newBlock);
 
-		logger.debug(`============== Dispatching block to index: ${newBlock.id} at height ${newBlock.height} ==============`);
-		const response = await getBlocks({ limit: 1 });
-		logger.debug(`============== 'newBlock' signal: ${Signals.get('newBlock')} ==============`);
-		Signals.get('newBlock').dispatch(response);
+			logger.debug(`============== Dispatching block to index: ${newBlock.id} at height ${newBlock.height} ==============`);
+			const response = await getBlocks({ limit: 1 });
+			logger.debug(`============== 'newBlock' signal: ${Signals.get('newBlock')} ==============`);
+			Signals.get('newBlock').dispatch(response);
+		} catch (err) {
+			logger.error('Error occured when processing \'newBlock\' event:\n', err.stack);
+		}
 	},
 	deleteBlock: async (block) => {
-		await deleteBlock(block);
-		logger.debug(`============== 'deleteBlock' signal: ${Signals.get('deleteBlock')} ==============`);
-		Signals.get('deleteBlock').dispatch(block);
+		try {
+			await deleteBlock(block);
+			logger.debug(`============== 'deleteBlock' signal: ${Signals.get('deleteBlock')} ==============`);
+			Signals.get('deleteBlock').dispatch(block);
+		} catch (err) {
+			logger.error('Error occured when processing \'deleteBlock\' event:\n', err.stack);
+		}
 	},
 	newRound: async () => {
-		await reloadDelegateCache();
-		await reloadNextForgersCache();
-		const limit = core.getSDKVersion() >= 4 ? 103 : 101;
-		const nextForgers = await getNextForgers({ limit, offset: 0 });
-		const response = { nextForgers: nextForgers.data.map(forger => forger.address) };
-		logger.debug(`============== 'newRound' signal: ${Signals.get('newRound')} ==============`);
-		Signals.get('newRound').dispatch(response);
+		try {
+			await reloadDelegateCache();
+			await reloadNextForgersCache();
+			const limit = core.getSDKVersion() >= 4 ? 103 : 101;
+			const nextForgers = await getNextForgers({ limit, offset: 0 });
+			const response = { nextForgers: nextForgers.data.map(forger => forger.address) };
+			logger.debug(`============== 'newRound' signal: ${Signals.get('newRound')} ==============`);
+			Signals.get('newRound').dispatch(response);
+		} catch (err) {
+			logger.error('Error occured when processing \'newRound\' event:\n', err.stack);
+		}
 	},
 	calculateFeeEstimate: async () => {
 		if (core.getSDKVersion() >= 4) {
-			if (config.feeEstimates.fullAlgorithmEnabled) {
-				logger.debug('Initiate the dynamic fee estimates computation (full computation)');
-				calculateEstimateFeeByteNormal();
-			}
-			if (config.feeEstimates.quickAlgorithmEnabled) {
-				logger.debug('Initiate the dynamic fee estimates computation (quick algorithm)');
-				const feeEstimate = await calculateEstimateFeeByteQuick();
+			try {
+				if (config.feeEstimates.fullAlgorithmEnabled) {
+					logger.debug('Initiate the dynamic fee estimates computation (full computation)');
+					calculateEstimateFeeByteNormal();
+				}
+				if (config.feeEstimates.quickAlgorithmEnabled) {
+					logger.debug('Initiate the dynamic fee estimates computation (quick algorithm)');
+					const feeEstimate = await calculateEstimateFeeByteQuick();
 
-				// TODO: Make a better control over the estimate process
-				logger.debug(`============== 'newFeeEstimate' signal: ${Signals.get('newFeeEstimate')} ==============`);
-				Signals.get('newFeeEstimate').dispatch(feeEstimate);
+					// TODO: Make a better control over the estimate process
+					logger.debug(`============== 'newFeeEstimate' signal: ${Signals.get('newFeeEstimate')} ==============`);
+					Signals.get('newFeeEstimate').dispatch(feeEstimate);
+				}
+			} catch (err) {
+				logger.error('Error occured when processing \'calculateFeeEstimate\' event:\n', err.stack);
 			}
 		}
 	},
