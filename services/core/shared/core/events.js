d8c1bd5249b119f4c3cb9be5209552d57efb23dc
Sameer Kumar Subudhi <sameer.subudhi@lightcurve.io>
2021-08-10 14:36:44 +0530

    Revert "Error handling for events"
    This reverts commit c3dabb8cabe001b725f9c82f17ddd4069dba6f3a.

--- services/core/shared/core/events.js
+++ services/core/shared/core/events.js
@@ -40,57 +40,41 @@ const logger = Logger();
 
 const events = {
 	newBlock: async (newBlock) => {
-		try {
-			logger.debug(`New block arrived: ${newBlock.id} at height ${newBlock.height}`);
-			performLastBlockUpdate(newBlock);
+		logger.debug(`New block arrived: ${newBlock.id} at height ${newBlock.height}`);
+		performLastBlockUpdate(newBlock);
 
-			logger.debug(`============== Dispatching block to index: ${newBlock.id} at height ${newBlock.height} ==============`);
-			const response = await getBlocks({ limit: 1 });
-			logger.debug(`============== 'newBlock' signal: ${Signals.get('newBlock')} ==============`);
-			Signals.get('newBlock').dispatch(response);
-		} catch (err) {
-			logger.error('Error occured when processing \'newBlock\' event:\n', err.stack);
-		}
+		logger.debug(`============== Dispatching block to index: ${newBlock.id} at height ${newBlock.height} ==============`);
+		const response = await getBlocks({ limit: 1 });
+		logger.debug(`============== 'newBlock' signal: ${Signals.get('newBlock')} ==============`);
+		Signals.get('newBlock').dispatch(response);
 	},
 	deleteBlock: async (block) => {
-		try {
-			await deleteBlock(block);
-			logger.debug(`============== 'deleteBlock' signal: ${Signals.get('deleteBlock')} ==============`);
-			Signals.get('deleteBlock').dispatch(block);
-		} catch (err) {
-			logger.error('Error occured when processing \'deleteBlock\' event:\n', err.stack);
-		}
+		await deleteBlock(block);
+		logger.debug(`============== 'deleteBlock' signal: ${Signals.get('deleteBlock')} ==============`);
+		Signals.get('deleteBlock').dispatch(block);
 	},
 	newRound: async () => {
-		try {
-			await reloadDelegateCache();
-			await reloadNextForgersCache();
-			const limit = core.getSDKVersion() >= 4 ? 103 : 101;
-			const nextForgers = await getNextForgers({ limit, offset: 0 });
-			const response = { nextForgers: nextForgers.data.map(forger => forger.address) };
-			logger.debug(`============== 'newRound' signal: ${Signals.get('newRound')} ==============`);
-			Signals.get('newRound').dispatch(response);
-		} catch (err) {
-			logger.error('Error occured when processing \'newRound\' event:\n', err.stack);
-		}
+		await reloadDelegateCache();
+		await reloadNextForgersCache();
+		const limit = core.getSDKVersion() >= 4 ? 103 : 101;
+		const nextForgers = await getNextForgers({ limit, offset: 0 });
+		const response = { nextForgers: nextForgers.data.map(forger => forger.address) };
+		logger.debug(`============== 'newRound' signal: ${Signals.get('newRound')} ==============`);
+		Signals.get('newRound').dispatch(response);
 	},
 	calculateFeeEstimate: async () => {
 		if (core.getSDKVersion() >= 4) {
-			try {
-				if (config.feeEstimates.fullAlgorithmEnabled) {
-					logger.debug('Initiate the dynamic fee estimates computation (full computation)');
-					calculateEstimateFeeByteNormal();
-				}
-				if (config.feeEstimates.quickAlgorithmEnabled) {
-					logger.debug('Initiate the dynamic fee estimates computation (quick algorithm)');
-					const feeEstimate = await calculateEstimateFeeByteQuick();
+			if (config.feeEstimates.fullAlgorithmEnabled) {
+				logger.debug('Initiate the dynamic fee estimates computation (full computation)');
+				calculateEstimateFeeByteNormal();
+			}
+			if (config.feeEstimates.quickAlgorithmEnabled) {
+				logger.debug('Initiate the dynamic fee estimates computation (quick algorithm)');
+				const feeEstimate = await calculateEstimateFeeByteQuick();
 
-					// TODO: Make a better control over the estimate process
-					logger.debug(`============== 'newFeeEstimate' signal: ${Signals.get('newFeeEstimate')} ==============`);
-					Signals.get('newFeeEstimate').dispatch(feeEstimate);
-				}
-			} catch (err) {
-				logger.error('Error occured when processing \'calculateFeeEstimate\' event:\n', err.stack);
+				// TODO: Make a better control over the estimate process
+				logger.debug(`============== 'newFeeEstimate' signal: ${Signals.get('newFeeEstimate')} ==============`);
+				Signals.get('newFeeEstimate').dispatch(feeEstimate);
 			}
 		}
 	},
