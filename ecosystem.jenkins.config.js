afbf3e899bf1824de1828076073d550596082fed
nagdahimanshu <himanshu.nagda@lightcurve.io>
2021-09-01 10:38:34 +0200

    Jenkins ecosystem file cleanup

--- ecosystem.jenkins.config.js
+++ ecosystem.jenkins.config.js
@@ -34,8 +34,6 @@ module.exports = {
 				STRICT_READINESS_CHECK: true,
 				ENABLE_WS_RATELIMIT: false,
 				RATE_LIMIT_CONNECTIONS:'0',
-				WS_RATELIMIT_POINTS: '10',
-				WS_RATELIMIT_DURATION:'1',
 			},
 		},
 		{
