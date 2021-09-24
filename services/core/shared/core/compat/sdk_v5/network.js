a3d50dc1a2fa9cca09dd780c57b2ff4fdc4feecd
Sameer Kumar Subudhi <sameer.subudhi@lightcurve.io>
2021-09-24 18:51:03 +0530

    Update default reward calculation logic

--- services/core/shared/core/compat/sdk_v5/network.js
+++ services/core/shared/core/compat/sdk_v5/network.js
@@ -18,20 +18,39 @@ const { resolvemoduleAssets } = require('../common/constants');
 
 const requestApi = coreApi.requestRetry;
 
+// From https://github.com/LiskHQ/lisk-sdk/blob/v5.1.4/elements/lisk-chain/src/block_reward.ts
+const calculateMilestone = (height, blockRewardArgs) => {
+	const distance = Math.floor(blockRewardArgs.distance);
+	const location = Math.trunc((height - blockRewardArgs.offset) / distance);
+	const lastMile = blockRewardArgs.milestones[blockRewardArgs.milestones.length - 1];
+
+	if (location > blockRewardArgs.milestones.length - 1) {
+		return blockRewardArgs.milestones.lastIndexOf(lastMile);
+	}
+
+	return location;
+};
+
+const calculateDefaultReward = (height, blockRewardArgs) => {
+	if (height < blockRewardArgs.offset) {
+		return BigInt(0);
+	}
+
+	return blockRewardArgs.milestones[calculateMilestone(height, blockRewardArgs)];
+};
+
 const getNetworkStatus = async () => {
 	const status = await requestApi(coreApi.getNetworkStatus);
 
-	const { offset, distance, milestones } = status.data.genesisConfig.rewards;
-	const rewardIndex = Math.floor((status.data.height - offset) / distance);
-	const finalRewardIndex = rewardIndex >= milestones.length
-		? milestones.length - 1
-		: rewardIndex;
-	status.data.currentReward = finalRewardIndex >= 0
-		? milestones[finalRewardIndex]
-		: 0;
-	status.data.milestone = status.data.currentReward;
-
-	status.data.moduleAssets = await resolvemoduleAssets(status.data.registeredModules);
+	status.data.milestone = calculateDefaultReward(
+		status.data.height,
+		status.data.genesisConfig.rewards,
+	);
+	status.data.currentReward = calculateDefaultReward(
+		status.data.height,
+		status.data.genesisConfig.rewards,
+	);
+	status.data.moduleAssets = resolvemoduleAssets(status.data.registeredModules);
 	status.data.registeredModules = status.data.registeredModules.map(item => item.name);
 
 	status.data.lastUpdate = Math.floor(Date.now() / 1000);
