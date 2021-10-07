2d4947a566e2e4640b800a2c1569e4e4313c98ce
Sameer Kumar Subudhi <sameer.subudhi@lightcurve.io>
2021-10-07 16:50:05 +0530

    Update regression tests for accounts

--- tests/regression/api_v2/testnet/http/accounts.test.js
+++ tests/regression/api_v2/testnet/http/accounts.test.js
@@ -40,11 +40,145 @@ describe('Accounts API', () => {
 
 	it(`Retrieve a delegate account with address: ${delegate.data[0].summary.address}`, async () => {
 		const response = await api.get(`${endpoint}?address=${delegate.data[0].summary.address}`);
-		expect(response).toStrictEqual(delegate);
+
+		expect(response).toEqual(expect.objectContaining({
+			data: expect.any(Array),
+			meta: expect.any(Object),
+		}));
+
+		// response.data
+		const [account] = response.data;
+		const { token, summary, sequence, dpos, ...remAccount } = account;
+
+		Object.getOwnPropertyNames(remAccount).forEach((prop) => {
+			expect(remAccount[prop])
+				.toStrictEqual(delegate.data[0][prop]);
+		});
+
+		// account.token
+		const { balance: tokenBal, ...remToken } = token;
+		expect(tokenBal).toBeDefined();
+		expect(typeof tokenBal).toBe('string');
+		Object.getOwnPropertyNames(remToken).forEach((prop) => {
+			expect(remToken[prop])
+				.toStrictEqual(delegate.data[0].token[prop]);
+		});
+
+		// account.summary
+		const { balance: summaryBal, ...remSummary } = summary;
+		expect(summaryBal).toBeDefined();
+		expect(typeof summaryBal).toBe('string');
+		expect(summaryBal).toBe(tokenBal);
+		Object.getOwnPropertyNames(remSummary).forEach((prop) => {
+			expect(remSummary[prop])
+				.toStrictEqual(delegate.data[0].summary[prop]);
+		});
+
+		// account.sequence
+		const { nonce, ...remSequence } = sequence;
+		expect(nonce).toBeDefined();
+		expect(typeof nonce).toBe('string');
+		expect(BigInt(nonce))
+			.toBeGreaterThanOrEqual(BigInt(delegate.data[0].sequence.nonce));
+		Object.getOwnPropertyNames(remSequence).forEach((prop) => {
+			expect(remSequence[prop])
+				.toStrictEqual(delegate.data[0].sequence[prop]);
+		});
+
+		// account.dpos
+		const { delegate: accDelegate, ...remDpos } = dpos;
+		expect(accDelegate).toBeDefined();
+		expect(accDelegate).toBeInstanceOf(Object);
+
+		const { rank, status, ...remDelegate } = accDelegate;
+		expect(rank).toBeDefined();
+		expect(typeof rank).toBe('number');
+
+		expect(status).toBeDefined();
+		expect(typeof status).toBe('string');
+
+		Object.getOwnPropertyNames(remDelegate).forEach((prop) => {
+			expect(remDelegate[prop])
+				.toStrictEqual(delegate.data[0].dpos.delegate[prop]);
+		});
+		Object.getOwnPropertyNames(remDpos).forEach((prop) => {
+			expect(remDpos[prop])
+				.toStrictEqual(delegate.data[0].dpos[prop]);
+		});
+
+		// response.meta
+		expect(response.meta).toStrictEqual(delegate.meta);
 	});
 
 	it(`Retrieve a delegate account with 'isDelegate' request param and address: ${delegateWithIsDelegate.data[0].summary.address}`, async () => {
 		const response = await api.get(`${endpoint}?address=${delegateWithIsDelegate.data[0].summary.address}&isDelegate=true`);
-		expect(response).toStrictEqual(delegateWithIsDelegate);
+
+		expect(response).toEqual(expect.objectContaining({
+			data: expect.any(Array),
+			meta: expect.any(Object),
+		}));
+
+		// response.data
+		const [account] = response.data;
+		const { token, summary, sequence, dpos, ...remAccount } = account;
+
+		Object.getOwnPropertyNames(remAccount).forEach((prop) => {
+			expect(remAccount[prop])
+				.toStrictEqual(delegateWithIsDelegate.data[0][prop]);
+		});
+
+		// account.token
+		const { balance: tokenBal, ...remToken } = token;
+		expect(tokenBal).toBeDefined();
+		expect(typeof tokenBal).toBe('string');
+		Object.getOwnPropertyNames(remToken).forEach((prop) => {
+			expect(remToken[prop])
+				.toStrictEqual(delegateWithIsDelegate.data[0].token[prop]);
+		});
+
+		// account.summary
+		const { balance: summaryBal, ...remSummary } = summary;
+		expect(summaryBal).toBeDefined();
+		expect(typeof summaryBal).toBe('string');
+		expect(summaryBal).toBe(tokenBal);
+		Object.getOwnPropertyNames(remSummary).forEach((prop) => {
+			expect(remSummary[prop])
+				.toStrictEqual(delegateWithIsDelegate.data[0].summary[prop]);
+		});
+
+		// account.sequence
+		const { nonce, ...remSequence } = sequence;
+		expect(nonce).toBeDefined();
+		expect(typeof nonce).toBe('string');
+		expect(BigInt(nonce))
+			.toBeGreaterThanOrEqual(BigInt(delegateWithIsDelegate.data[0].sequence.nonce));
+		Object.getOwnPropertyNames(remSequence).forEach((prop) => {
+			expect(remSequence[prop])
+				.toStrictEqual(delegateWithIsDelegate.data[0].sequence[prop]);
+		});
+
+		// account.dpos
+		const { delegate: accDelegate, ...remDpos } = dpos;
+		expect(accDelegate).toBeDefined();
+		expect(accDelegate).toBeInstanceOf(Object);
+
+		const { rank, status, ...remDelegate } = accDelegate;
+		expect(rank).toBeDefined();
+		expect(typeof rank).toBe('number');
+
+		expect(status).toBeDefined();
+		expect(typeof status).toBe('string');
+
+		Object.getOwnPropertyNames(remDelegate).forEach((prop) => {
+			expect(remDelegate[prop])
+				.toStrictEqual(delegateWithIsDelegate.data[0].dpos.delegate[prop]);
+		});
+		Object.getOwnPropertyNames(remDpos).forEach((prop) => {
+			expect(remDpos[prop])
+				.toStrictEqual(delegateWithIsDelegate.data[0].dpos[prop]);
+		});
+
+		// response.meta
+		expect(response.meta).toStrictEqual(delegateWithIsDelegate.meta);
 	});
 });
--- tests/regression/api_v2/testnet/rpc/accounts.test.js
+++ tests/regression/api_v2/testnet/rpc/accounts.test.js
@@ -39,7 +39,74 @@ describe('Accounts API', () => {
 
 	it(`Retrieve a delegate account with address: ${delegate.result.data[0].summary.address}`, async () => {
 		const response = await getAccounts({ address: delegate.result.data[0].summary.address });
-		expect(response).toStrictEqual(delegate);
+
+		expect(response.result).toEqual(expect.objectContaining({
+			data: expect.any(Array),
+			meta: expect.any(Object),
+		}));
+
+		// response.result.data
+		const [account] = response.result.data;
+		const { token, summary, sequence, dpos, ...remAccount } = account;
+
+		Object.getOwnPropertyNames(remAccount).forEach((prop) => {
+			expect(remAccount[prop])
+				.toStrictEqual(delegate.result.data[0][prop]);
+		});
+
+		// account.token
+		const { balance: tokenBal, ...remToken } = token;
+		expect(tokenBal).toBeDefined();
+		expect(typeof tokenBal).toBe('string');
+		Object.getOwnPropertyNames(remToken).forEach((prop) => {
+			expect(remToken[prop])
+				.toStrictEqual(delegate.result.data[0].token[prop]);
+		});
+
+		// account.summary
+		const { balance: summaryBal, ...remSummary } = summary;
+		expect(summaryBal).toBeDefined();
+		expect(typeof summaryBal).toBe('string');
+		expect(summaryBal).toBe(tokenBal);
+		Object.getOwnPropertyNames(remSummary).forEach((prop) => {
+			expect(remSummary[prop])
+				.toStrictEqual(delegate.result.data[0].summary[prop]);
+		});
+
+		// account.sequence
+		const { nonce, ...remSequence } = sequence;
+		expect(nonce).toBeDefined();
+		expect(typeof nonce).toBe('string');
+		expect(BigInt(nonce))
+			.toBeGreaterThanOrEqual(BigInt(delegate.result.data[0].sequence.nonce));
+		Object.getOwnPropertyNames(remSequence).forEach((prop) => {
+			expect(remSequence[prop])
+				.toStrictEqual(delegate.result.data[0].sequence[prop]);
+		});
+
+		// account.dpos
+		const { delegate: accDelegate, ...remDpos } = dpos;
+		expect(accDelegate).toBeDefined();
+		expect(accDelegate).toBeInstanceOf(Object);
+
+		const { rank, status, ...remDelegate } = accDelegate;
+		expect(rank).toBeDefined();
+		expect(typeof rank).toBe('number');
+
+		expect(status).toBeDefined();
+		expect(typeof status).toBe('string');
+
+		Object.getOwnPropertyNames(remDelegate).forEach((prop) => {
+			expect(remDelegate[prop])
+				.toStrictEqual(delegate.result.data[0].dpos.delegate[prop]);
+		});
+		Object.getOwnPropertyNames(remDpos).forEach((prop) => {
+			expect(remDpos[prop])
+				.toStrictEqual(delegate.result.data[0].dpos[prop]);
+		});
+
+		// response.result.meta
+		expect(response.result.meta).toStrictEqual(delegate.result.meta);
 	});
 
 	it(`Retrieve a delegate account with 'isDelegate' request param and address: ${delegateWithIsDelegate.result.data[0].summary.address}`, async () => {
@@ -47,6 +114,73 @@ describe('Accounts API', () => {
 			address: delegateWithIsDelegate.result.data[0].summary.address,
 			isDelegate: true,
 		});
-		expect(response).toStrictEqual(delegateWithIsDelegate);
+
+		expect(response.result).toEqual(expect.objectContaining({
+			data: expect.any(Array),
+			meta: expect.any(Object),
+		}));
+
+		// response.result.data
+		const [account] = response.result.data;
+		const { token, summary, sequence, dpos, ...remAccount } = account;
+
+		Object.getOwnPropertyNames(remAccount).forEach((prop) => {
+			expect(remAccount[prop])
+				.toStrictEqual(delegateWithIsDelegate.result.data[0][prop]);
+		});
+
+		// account.token
+		const { balance: tokenBal, ...remToken } = token;
+		expect(tokenBal).toBeDefined();
+		expect(typeof tokenBal).toBe('string');
+		Object.getOwnPropertyNames(remToken).forEach((prop) => {
+			expect(remToken[prop])
+				.toStrictEqual(delegateWithIsDelegate.result.data[0].token[prop]);
+		});
+
+		// account.summary
+		const { balance: summaryBal, ...remSummary } = summary;
+		expect(summaryBal).toBeDefined();
+		expect(typeof summaryBal).toBe('string');
+		expect(summaryBal).toBe(tokenBal);
+		Object.getOwnPropertyNames(remSummary).forEach((prop) => {
+			expect(remSummary[prop])
+				.toStrictEqual(delegateWithIsDelegate.result.data[0].summary[prop]);
+		});
+
+		// account.sequence
+		const { nonce, ...remSequence } = sequence;
+		expect(nonce).toBeDefined();
+		expect(typeof nonce).toBe('string');
+		expect(BigInt(nonce))
+			.toBeGreaterThanOrEqual(BigInt(delegateWithIsDelegate.result.data[0].sequence.nonce));
+		Object.getOwnPropertyNames(remSequence).forEach((prop) => {
+			expect(remSequence[prop])
+				.toStrictEqual(delegateWithIsDelegate.result.data[0].sequence[prop]);
+		});
+
+		// account.dpos
+		const { delegate: accDelegate, ...remDpos } = dpos;
+		expect(accDelegate).toBeDefined();
+		expect(accDelegate).toBeInstanceOf(Object);
+
+		const { rank, status, ...remDelegate } = accDelegate;
+		expect(rank).toBeDefined();
+		expect(typeof rank).toBe('number');
+
+		expect(status).toBeDefined();
+		expect(typeof status).toBe('string');
+
+		Object.getOwnPropertyNames(remDelegate).forEach((prop) => {
+			expect(remDelegate[prop])
+				.toStrictEqual(delegateWithIsDelegate.result.data[0].dpos.delegate[prop]);
+		});
+		Object.getOwnPropertyNames(remDpos).forEach((prop) => {
+			expect(remDpos[prop])
+				.toStrictEqual(delegateWithIsDelegate.result.data[0].dpos[prop]);
+		});
+
+		// response.result.meta
+		expect(response.result.meta).toStrictEqual(delegateWithIsDelegate.result.meta);
 	});
 });
