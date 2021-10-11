7c3480f7ba984fb257857869162765f569b8180d
Michal Tuleja <michal@lightcurve.io>
2021-10-11 17:49:22 +0200

    Add the npm script for regression tests

---
+++ tests/jest.config.regression.js
@@ -0,0 +1,34 @@
+/*
+ * LiskHQ/lisk-service
+ * Copyright © 2021 Lisk Foundation
+ *
+ * See the LICENSE file at the top-level directory of this distribution
+ * for licensing information.
+ *
+ * Unless otherwise agreed in a custom licensing agreement with the Lisk Foundation,
+ * no part of this software, including this file, may be copied, modified,
+ * propagated, or distributed except according to the terms contained in the
+ * LICENSE file.
+ *
+ * Removal or modification of this copyright notice is prohibited.
+ *
+ */
+module.exports = {
+	verbose: true,
+	testMatch: [
+		'<rootDir>/regression/**/*.test.js',
+	],
+	testEnvironment: 'node',
+	testTimeout: 50000,
+	maxWorkers: 1,
+	setupFilesAfterEnv: [
+		'jest-extended',
+		'<rootDir>/helpers/setupCustomMatchers.js',
+	],
+	watchPlugins: [
+		['jest-watch-toggle-config', { setting: 'verbose' }],
+		['jest-watch-toggle-config', { setting: 'bail' }],
+		['jest-watch-toggle-config', { setting: 'notify' }],
+		'jest-watch-typeahead/filename',
+	],
+};
