8bc671b242b4b1c6b1fd8730fc3c07cbfd6ea58f
Sameer Kumar Subudhi <sameer.subudhi@lightcurve.io>
2021-10-18 19:10:59 +0530

    Update swagger specs for multisig create endpoint

--- services/gateway/apis/http-version2/methods/postMultisigTransactions.js
+++ services/gateway/apis/http-version2/methods/postMultisigTransactions.js
@@ -15,6 +15,7 @@
  */
 const postMultisigTransactionsSource = require('../../../sources/version2/postMultisigTransactionsSource');
 const envelope = require('../../../sources/version2/mappings/stdEnvelope');
+const { transformParams, response, getSwaggerDescription } = require('../../../shared/utils');
 
 module.exports = {
 	version: '2.0',
@@ -23,6 +24,27 @@ module.exports = {
 	tags: ['Multisignature Transactions'],
 	params: {
 		transaction: { optional: true, type: 'string' },
+	},
+	get schema() {
+		const multisigTransactionSchema = {};
+		multisigTransactionSchema[this.swaggerApiPath] = { post: {} };
+		multisigTransactionSchema[this.swaggerApiPath].post.tags = this.tags;
+		multisigTransactionSchema[this.swaggerApiPath].post.summary = 'Requests multisignature transactions';
+		multisigTransactionSchema[this.swaggerApiPath].post.description = getSwaggerDescription({
+			rpcMethod: this.rpcMethod,
+			description: 'Returns multisignature transactions',
+		});
+		multisigTransactionSchema[this.swaggerApiPath].post.parameters = transformParams('multisignature', this.params);
+		multisigTransactionSchema[this.swaggerApiPath].post.responses = {
+			200: {
+				description: 'Updated multisignature transaction',
+				schema: {
+					$ref: '#/definitions/MultisigTransactionsWithEnvelope',
+				},
+			},
+		};
+		Object.assign(multisigTransactionSchema[this.swaggerApiPath].post.responses, response);
+		return multisigTransactionSchema;
 	},
 	source: postMultisigTransactionsSource,
 	envelope,
