9b4fe79df8613a1e64b44a44d54507f760bfc989
Michal Tuleja <michal@lightcurve.io>
2021-11-09 17:06:20 +0100

    Update unit tests

--- services/transaction/tests/unit/validators.spec.js
+++ services/transaction/tests/unit/validators.spec.js
@@ -24,74 +24,133 @@ const {
 	isWithinPoolLimit,
 	hasValidServiceId,
 	isValidCoreTransaction,
-	hasValidSignatures,
+	isValidSignature,
 	hasValidNonce,
 } = require('../../shared/validators');
 
+const {
+	transactions,
+	inputTransaction,
+	// inputTransactionServiceId,
+} = require('../constants/multisignature');
+
+const { convertToCoreTransaction } = require('../../shared/transactionUtils');
+
+const genericTransaction = inputTransaction;
+
+const registrationTransaction = transactions[1];
+
 const genericAccount = {
-	accountId: '',
-	// balance: '',
+	address: 'lskved5acuxcjo6bsvj7rggxkw9dnzca9tpf2h93d',
+	token: {
+		balance: 10000000000,
+	},
+	sequence: {
+		nonce: 0,
+	},
+	keys: {
+		numberOfSignatures: 0,
+		mandatoryKeys: [],
+		optionalKeys: [],
+	},
+	dpos: {
+		delegate: {
+			username: '',
+			pomHeights: [],
+			consecutiveMissedBlocks: 0,
+			lastForgedHeight: 0,
+			isBanned: false,
+			totalVotesReceived: '0',
+		},
+		sentVotes: [],
+		unlocking: [],
+	},
+	isDelegate: false,
+	isMultisignature: false,
+	isGenesisAccount: 0,
+	publicKey: null,
+	multisignatureGroups: {
+		isMultisignature: false,
+	},
+	multisignatureMemberships: {
+		memberships: [],
+	},
+	knowledge: {},
 };
 
-const accountWithEnoughBalance = { ...genericAccount, balance: 7500000 };
+const genericSignature = {
+	publicKey: genericAccount.publicKey,
+	address: genericAccount.address,
+	signature: '123456',
+};
 
-const accountWithLittleBalance = { ...genericAccount, balance: 150 };
+const coreTransaction = convertToCoreTransaction(genericTransaction);
 
-const genericTransaction = {};
-
-const transactionWithinExpiryTime = {};
-
-const expiredTransaction = {};
-
-const transactionInvalidModuleAssetId = {};
+const getCurrentTimestamp = (new Date()).getTime();
 
 describe('Test validator', () => {
 	beforeAll(async () => {});
 
 	afterAll(async () => {});
 
+	// This can be tested only in conjunction with Core Microservice
+	// TODO: Mock Core Microservice
 	it.todo('for minimal balance', async () => {
-		expect(hasMinimalBalance(accountWithEnoughBalance)).toBe(true);
-		expect(hasMinimalBalance(accountWithLittleBalance)).toBe(false);
+		expect(await hasMinimalBalance({ ...genericAccount, balance: 7500000 })).toBe(true);
+		expect(await hasMinimalBalance({ ...genericAccount, balance: 150 })).toBe(false);
 	});
 
-	it.todo('for account being a multisignature account', () => {
-		expect(isMultisigAccount(genericTransaction)).toBe(true);
-		expect(isMultisigAccount(transactionInvalidModuleAssetId)).toBe(false);
+	it('for account being a multisignature account', async () => {
+		expect(isMultisigAccount({ ...genericAccount, isMultisignature: true })).toBe(true);
+		expect(isMultisigAccount({ ...genericAccount, isMultisignature: false })).toBe(false);
 	});
 
-	it.todo('for expiration time', async () => {
-		expect(isWithinExpirationTime(transactionWithinExpiryTime)).toBe(true);
-		expect(isWithinExpirationTime(expiredTransaction)).toBe(false);
+	it('for expiration time', async () => {
+		expect(isWithinExpirationTime(
+			{ ...genericTransaction, expires: getCurrentTimestamp() + 5000 }))
+			.toBe(true);
+		expect(isWithinExpirationTime(
+			{ ...genericTransaction, expires: getCurrentTimestamp() - 100 }))
+			.toBe(false);
 	});
 
-	it.todo('for moduleAssetId matching multisig registration type', () => {
-		expect(isMultisigRegistration(genericTransaction)).toBe(true);
-		expect(isMultisigRegistration(transactionInvalidModuleAssetId)).toBe(false);
+	it('for moduleAssetId matching multisig registration type', async () => {
+		expect(isMultisigRegistration({ ...registrationTransaction, moduleAssetId: '4:0' })).toBe(true);
+		expect(isMultisigRegistration({ ...registrationTransaction, moduleAssetId: '2:0' })).toBe(false);
 	});
 
+	// TODO: Mock MySQL database
 	it.todo('for number of transactions per account within the limit of 64', () => {
-		expect(isWithinPoolLimit(genericTransaction)).toBe(true);
-		expect(isWithinPoolLimit(transactionInvalidModuleAssetId)).toBe(false);
+		expect(isWithinPoolLimit(genericAccount.address)).toBe(true);
+		expect(isWithinPoolLimit('lskved5acuxcjo6bsvj7rggxkw9dnzca9tpf2h92d')).toBe(false);
 	});
 
-	it.todo('for transaction containing a valid serviceId', () => {
-		expect(hasValidServiceId(genericTransaction)).toBe(true);
-		expect(hasValidServiceId(transactionInvalidModuleAssetId)).toBe(false);
+	it('for transaction containing a valid serviceId', async () => {
+		expect(hasValidServiceId({ ...genericTransaction, serviceId: '7328a536b56942ae5005c31fe0cba6e46dd98ab79e7511eefc3740cead0f37fb' })).toBe(true);
+		expect(hasValidServiceId({ ...genericTransaction, serviceId: 'abcdef' })).toBe(false);
 	});
 
+	// TODO: Mock getAssetSchema()
 	it.todo('for transaction validation using lisk-elements', () => {
-		expect(isValidCoreTransaction(genericTransaction)).toBe(true);
-		expect(isValidCoreTransaction(transactionInvalidModuleAssetId)).toBe(false);
+		expect(isValidCoreTransaction(convertToCoreTransaction(genericTransaction))).toBe(true);
+		expect(isValidCoreTransaction(convertToCoreTransaction(genericTransaction))).toBe(false);
 	});
 
-	it.todo('for valid signatures', () => {
-		expect(hasValidSignatures(genericTransaction)).toBe(true);
-		expect(hasValidSignatures(transactionInvalidModuleAssetId)).toBe(false);
+	it('for signature validation', async () => {
+		expect(isValidSignature(coreTransaction, genericSignature)).toBe(true);
+		expect(isValidSignature(coreTransaction,
+			{ ...genericSignature, signature: 'invalid_signature' }))
+			.toBe(false);
 	});
 
-	it.todo('for having a valid nonce account', () => {
-		expect(hasValidNonce(genericTransaction)).toBe(true);
-		expect(hasValidNonce(transactionInvalidModuleAssetId)).toBe(false);
+	it('for having a valid nonce account', async () => {
+		expect(hasValidNonce(
+			{ ...genericTransaction, nonce: 2 },
+			{ ...genericAccount, nonce: 2 }))
+			.toBe(true);
+		expect(hasValidNonce(
+			{ ...genericTransaction, nonce: 12 },
+			{ ...genericAccount, nonce: 3 }))
+			.toBe(false);
 	});
 });
