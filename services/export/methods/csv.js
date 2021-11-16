6c5adf257f70e411608627221e60bbc0d48293c7
Michal Tuleja <michal@lightcurve.io>
2021-11-16 10:32:44 +0100

    Add csv export controller
    Co-authored-by: Sameer Kumar Subudhi <sameersubudhi@users.noreply.github.com>
    Co-authored-by: Himanshu Nagda <nagdahimanshu@users.noreply.github.com>

---
+++ services/export/methods/csv.js
@@ -0,0 +1,28 @@
+/*
+ * LiskHQ/lisk-service
+ * Copyright © 2021 Lisk Foundation
+ *
+ * See the LICENSE file at the top-level directory of this distribution
+ * for licensing information.
+ *
+ * Unless otherwise agreed in a custom licensing agreement with the Lisk Foundation,
+ * no part of this software, including this file, may be copied, modified,
+ * propagated, or distributed except according to the terms contained in the
+ * LICENSE file.
+ *
+ * Removal or modification of this copyright notice is prohibited.
+ *
+ */
+module.exports = [
+	{
+		name: 'csv',
+		description: 'Exports CSV',
+		params: {
+			address: { type: 'string', optional: false },
+		},
+		controller: async param => {
+			// TODO: write content
+			return param.address;
+		},
+	},
+];
