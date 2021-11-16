9bb8b24f72e335f5239423305ef8b18244e95689
Sameer Kumar Subudhi <sameer.subudhi@lightcurve.io>
2021-11-16 16:52:44 +0100

    Add parser field mappings

---
+++ services/export/shared/csvFieldMappings.js
@@ -0,0 +1,69 @@
+/*
+ * LiskHQ/lisk-service
+ * Copyright © 2021 Lisk Foundation
+ *
+ * See the LICENSE file at the top-level directory of this distribution
+ * for licensing information.
+ *
+ * Unless otherwise agreed in a custom licensing agreement with the Lisk Foundation,
+ * no part of this software, including this file, may be copied, modified,
+ * propagated, or distributed except according to the terms contained in the
+ * LICENSE file.
+ *
+ * Removal or modification of this copyright notice is prohibited.
+ *
+ */
+module.exports = [
+	{
+		label: 'Date',
+		value: 'date',
+	},
+	{
+		label: 'Time',
+		value: 'time',
+	},
+	{
+		label: 'Amount LSK',
+		value: 'amount',
+	},
+	{
+		label: 'Fee LSK',
+		value: 'fee',
+	},
+	{
+		label: 'Module:Asset',
+		value: 'moduleAssetId',
+	},
+	{
+		label: 'Module:Asset Name',
+		value: 'moduleAssetName',
+	},
+	{
+		label: 'Sender',
+		value: 'sender',
+	},
+	{
+		label: 'Recipient',
+		value: 'recipient',
+	},
+	{
+		label: 'Sender Public Key',
+		value: 'senderPublicKey',
+	},
+	{
+		label: 'Recipient Public Key',
+		value: 'recipientPublicKey',
+	},
+	{
+		label: 'Block Height',
+		value: 'blockHeight',
+	},
+	{
+		label: 'Note',
+		value: 'note',
+	},
+	{
+		label: 'Transaction ID',
+		value: 'transactionId',
+	},
+];
