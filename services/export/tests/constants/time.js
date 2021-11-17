d8e8166d8fcdc71bb0160ce0a773060e174e31e5
Sameer Kumar Subudhi <sameer.subudhi@lightcurve.io>
2021-11-17 15:10:27 +0100

    Add unit tests for time utils

---
+++ services/export/tests/constants/time.js
@@ -0,0 +1,24 @@
+/*
+ * LiskHQ/lisk-service
+ * Copyright © 2021 Lisk Foundation
+ *
+ * See the LICENSE file at the top-level directory of this distribution
+ * for licensing information.
+ *
+ * Unless otherwise agreed in a custom licensing agreement with the Lisk Foundation,
+ * no part of this software, including this file, may be copied, modified,
+ * propagated, or distributed except according to the terms contained in the
+ * LICENSE file.
+ *
+ * Removal or modification of this copyright notice is prohibited.
+ *
+ */
+const timestamp = 1637156065;
+const expectedDate = '2021-11-17';
+const expectedTime = '02:34:25';
+
+module.exports = {
+	timestamp,
+	expectedDate,
+	expectedTime,
+};
