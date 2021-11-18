50b937b49b95f6975c41fdb22e7c8f61aec727f6
nagdahimanshu <himanshu.nagda@lightcurve.io>
2021-11-18 17:23:21 +0530

    Refactor purge implementation

--- services/export/shared/helpers/file.js
+++ services/export/shared/helpers/file.js
@@ -13,6 +13,7 @@
  * Removal or modification of this copyright notice is prohibited.
  *
  */
+const BluebirdPromise = require('bluebird');
 const path = require('path');
 const fs = require('fs');
 const { Logger } = require('lisk-service-framework');
@@ -21,92 +22,105 @@ const logger = Logger();
 
 const getDaysinMilliseconds = days => days * 86400 * 1000;
 
+const getStats = filePath => new Promise((resolve, reject) => {
+    fs.stat(filePath, (err, stat) => {
+        if (err) {
+            logger.error(err);
+            return reject(err);
+        }
+        return resolve(stat);
+    });
+});
+
 const createDir = (dirPath, options = { recursive: true }) => new Promise((resolve, reject) => {
-	logger.debug(`Creating directory: ${dirPath}`);
-	return fs.mkdir(
-		dirPath,
-		options,
-		(err) => {
-			if (err) {
-				logger.error(`Error when creating directory: ${dirPath}\n`, err.message);
-				return reject(err);
-			}
-			logger.debug(`Successfully created directory: ${dirPath}`);
-			return resolve();
-		},
-	);
+    logger.debug(`Creating directory: ${dirPath}`);
+    return fs.mkdir(
+        dirPath,
+        options,
+        (err) => {
+            if (err) {
+                logger.error(`Error when creating directory: ${dirPath}\n`, err.message);
+                return reject(err);
+            }
+            logger.debug(`Successfully created directory: ${dirPath}`);
+            return resolve();
+        },
+    );
 });
 
 const init = async (params) => createDir(params.dirPath);
 
 const write = (filePath, content) => new Promise((resolve, reject) => {
-	fs.writeFile(filePath, JSON.stringify(content), (err) => {
-		if (err) {
-			logger.error(err);
-			return reject(err);
-		}
-		return resolve();
-	});
+    fs.writeFile(filePath, JSON.stringify(content), (err) => {
+        if (err) {
+            logger.error(err);
+            return reject(err);
+        }
+        return resolve();
+    });
 });
 
 const read = (filePath) => new Promise((resolve, reject) => {
-	fs.readFile(filePath, (err, data) => {
-		if (err) {
-			logger.error(err);
-			return reject(err);
-		}
-		return resolve(JSON.parse(data));
-	});
+    fs.readFile(filePath, (err, data) => {
+        if (err) {
+            logger.error(err);
+            return reject(err);
+        }
+        return resolve(JSON.parse(data));
+    });
 });
 
 const remove = (filePath) => new Promise((resolve, reject) => {
-	fs.unlink(
-		filePath,
-		(err) => {
-			if (err) {
-				logger.error(err);
-				return reject(err);
-			}
-			return resolve();
-		},
-	);
+    fs.unlink(
+        filePath,
+        (err) => {
+            if (err) {
+                logger.error(err);
+                return reject(err);
+            }
+            return resolve();
+        },
+    );
 });
 
 const list = (dirPath, n = 100, page = 0) => new Promise((resolve, reject) => {
-	fs.readdir(
-		dirPath,
-		(err, files) => {
-			if (err) {
-				logger.error(err);
-				return reject(err);
-			}
-			return resolve(files.slice(page, page + n));
-		},
-	);
+    fs.readdir(
+        dirPath,
+        (err, files) => {
+            if (err) {
+                logger.error(err);
+                return reject(err);
+            }
+            return resolve(files.slice(page, page + n));
+        },
+    );
 });
 
-const purge = (dirPath, days = 1) => {
-	fs.readdir(dirPath, (_, files) => {
-		files.forEach((file) => {
-			fs.stat(path.join(dirPath, file), (err, stat) => {
-				if (err) logger.error(err);
-				const now = new Date().getTime();
-				const endTime = new Date(stat.ctime).getTime() + getDaysinMilliseconds(days);
-				if (now > endTime) {
-					fs.unlink(path.join(dirPath, file), (error) => {
-						if (error) logger.error(error);
-					});
-				}
-			});
-		});
-	});
-};
+const purge = (dirPath, days = 1) => new Promise((resolve, reject) => {
+    fs.readdir(dirPath, async (err, files) => {
+        if (err) {
+            logger.error(err);
+            return reject(err);
+        }
+        await BluebirdPromise.map(
+            files,
+            async (file) => {
+                const stat = await getStats(path.join(dirPath, file));
+                const currentTime = new Date().getTime();
+                const expirationTime = new Date(stat.ctime).getTime() + getDaysinMilliseconds(days);
+                if (currentTime > expirationTime) await remove(path.join(dirPath, file));
+            },
+            { concurrency: files.length },
+        );
+        return resolve();
+    });
+});
 
 module.exports = {
-	init,
-	write,
-	read,
-	remove,
-	list,
-	purge,
+    init,
+    write,
+    read,
+    remove,
+    list,
+    purge,
 };
