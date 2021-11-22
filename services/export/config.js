30f85414d46b463b12b582b7232402d257b4d9c9
Michal Tuleja <michal@lightcurve.io>
2021-11-22 17:21:18 +0000

    Update config comment

--- services/export/config.js
+++ services/export/config.js
@@ -51,7 +51,7 @@ config.log.file = process.env.SERVICE_LOG_FILE || 'false';
 // Set docker host if running inside the container
 config.log.docker_host = process.env.DOCKER_HOST || 'local';
 
-// CSV related config
+// CSV output
 config.csv = {};
 config.csv.delimiter = ';';
 config.csv.dateFormat = 'YYYY-MM-DD';
