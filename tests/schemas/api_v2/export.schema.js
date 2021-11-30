52bc3e3d98860d321bbfdc86de7eb61a6696118e
nagdahimanshu <himanshu.nagda@lightcurve.io>
2021-11-30 17:51:49 +0530

    Add export schema for tests

---
+++ tests/schemas/api_v2/export.schema.js
@@ -0,0 +1,39 @@
+/*
+ * LiskHQ/lisk-service
+ * Copyright © 2021 Lisk Foundation
+ *
+ * See the LICENSE file at the top-level directory of this distribution
+ * for licensing information.
+ *
+ * Unless otherwise agreed in a custom licensing agreement with the Lisk Foundation,
+ * no part of this software, including this file, may be copied, modified,
+ * propagated, or distributed except according to the terms contained in the
+ * LICENSE file.
+ *
+ * Removal or modification of this copyright notice is prohibited.
+ *
+ */
+import Joi from 'joi';
+
+const exportSchema = {
+	address: Joi.string().required(),
+	interval: Joi.string().required(),
+	fileName: Joi.string().optional(),
+	fileUrl: Joi.string().optional(),
+};
+
+const metaSchema = {
+	ready: Joi.boolean().required(),
+};
+
+const goodRequestSchema = {
+	data: Joi.object().required(),
+	meta: Joi.object().required(),
+	links: Joi.object().optional(),
+};
+
+module.exports = {
+	metaSchema: Joi.object(metaSchema).required(),
+	exportSchema: Joi.object(exportSchema).required(),
+	goodRequestSchema: Joi.object(goodRequestSchema).required(),
+};
