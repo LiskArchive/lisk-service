d3f4e3985d3c08cd928a2cf46814bd01aa2defbd
nagdahimanshu <himanshu.nagda@lightcurve.io>
2022-04-25 08:57:10 +0200

    Update copyright year

--- services/fee-estimator/tests/blockGenerator/createBlocks.js
+++ services/fee-estimator/tests/blockGenerator/createBlocks.js
@@ -1,6 +1,6 @@
 /*
  * LiskHQ/lisk-service
- * Copyright © 2020 Lisk Foundation
+ * Copyright © 2022 Lisk Foundation
  *
  * See the LICENSE file at the top-level directory of this distribution
  * for licensing information.
