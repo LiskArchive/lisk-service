97e3a14f9ccb522c78a7a33de718b3c3fd15b40f
nagdahimanshu <himanshu.nagda@lightcurve.io>
2022-05-18 12:06:31 +0200

    Update test data

--- services/blockchain-connector/tests/constants/transactions.js
+++ services/blockchain-connector/tests/constants/transactions.js
@@ -30,9 +30,27 @@ const transaction = {
 	id: 'a6ba9d89d8e57bc921dd56417313a01fa7834f12cdbcebdfda58c7b385397d96',
 };
 
+const invalidTransaction = {
+	moduleID: 2,
+	commandID: 0,
+	nonce: '4',
+	fee: '200000',
+	senderPublicKey: '41e9303d6e871e066f58f4900422b802cf98ee9035a070f92baa1aa7b55728a8',
+	asset: {
+		amount: '752291501',
+		recipientAddress: '60db70ee3f962d518096127ed73b8b8ac9441a28',
+		data: '',
+	},
+	signatures: [
+		'fb02e7a125d20909a449be226cb8f7cc17085b98414584e3422d3405bd83d0dc0aa108ecaf1af01825dbbb632d85bf6bd0f7bbbc9fd81519485679dda78f7006',
+	],
+	id: 'a6ba9d89d8e57bc921dd56417313a01fa7834f12cdbcebdfda58c7b385397d96',
+};
+
 const encodedTransaction = '08021000180420c09a0c2a2041e9303d6e871e066f58f4900422b802cf98ee9035a070f92baa1aa7b55728a8321e08ad9ddce602121460db70ee3f962d518096127ed73b8b8ac9441a281a003a40fb02e7a125d20909a449be226cb8f7cc17085b98414584e3422d3405bd83d0dc0aa108ecaf1af01825dbbb632d85bf6bd0f7bbbc9fd81519485679dda78f7006';
 
 module.exports = {
 	transaction,
+	invalidTransaction,
 	encodedTransaction,
 };
