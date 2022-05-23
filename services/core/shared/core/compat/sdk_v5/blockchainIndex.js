b04dead8fb1814afb620cd1df9b2bac372db4e34
Sameer Kumar Subudhi <sameer.subudhi@lightcurve.io>
2022-05-24 00:48:01 +0200

    Add log

--- services/core/shared/core/compat/sdk_v5/blockchainIndex.js
+++ services/core/shared/core/compat/sdk_v5/blockchainIndex.js
@@ -237,7 +237,9 @@ const indexBlock = async job => {
 		logger.debug(`Rolled back MySQL transaction to index block at height ${height}`);
 
 		if (error.message.includes('ER_LOCK_DEADLOCK')) {
-			throw new Error(`Deadlock encountered while indexing block at height ${height}. Will retry later.`);
+			const errMessage = `Deadlock encountered while indexing block at height ${height}. Will retry later.`;
+			logger.warn(errMessage);
+			throw new Error(errMessage);
 		}
 		throw error;
 	}
