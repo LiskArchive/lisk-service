4f18abb34fcd095e0a2289b3327ea0cf87958b11
nagdahimanshu <himanshu.nagda@lightcurve.io>
2022-06-07 18:44:26 +0200

    Add network endpoint in gateway

---
+++ services/gateway/sources/version3/networkStatus.js
@@ -0,0 +1,42 @@
+/*
+ * LiskHQ/lisk-service
+ * Copyright © 2022 Lisk Foundation
+ *
+ * See the LICENSE file at the top-level directory of this distribution
+ * for licensing information.
+ *
+ * Unless otherwise agreed in a custom licensing agreement with the Lisk Foundation,
+ * no part of this software, including this file, may be copied, modified,
+ * propagated, or distributed except according to the terms contained in the
+ * LICENSE file.
+ *
+ * Removal or modification of this copyright notice is prohibited.
+ *
+ */
+module.exports = {
+	type: 'moleculer',
+	params: {},
+	method: 'indexer.network.status',
+	definition: {
+		data: {
+			version: '=,string',
+			networkVersion: '=,string',
+			networkIdentifier: '=,string',
+			lastBlockID: '=,string',
+			height: '=,number',
+			finalizedHeight: '=,number',
+			syncing: '=,boolean',
+			unconfirmedTransactions: '=,number',
+			genesis: '=',
+			registeredModules: '=',
+			moduleCommands: '=',
+			network: '=',
+		},
+		meta: {
+			lastUpdate: 'data.lastUpdate,number',
+			lastBlockHeight: 'data.height,number',
+			lastBlockId: 'data.lastBlockID,string',
+		},
+		links: {},
+	},
+};
