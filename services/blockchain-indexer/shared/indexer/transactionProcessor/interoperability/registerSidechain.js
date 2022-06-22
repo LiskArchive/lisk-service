21b9d26feb67f7e74cbb5a626fcd33e4c039ea9f
nagdahimanshu <himanshu.nagda@lightcurve.io>
2022-06-22 11:12:32 +0200

    Index sidechain registration transaction info

--- services/blockchain-indexer/shared/indexer/transactionProcessor/interoperability/registerSidechain.js
+++ services/blockchain-indexer/shared/indexer/transactionProcessor/interoperability/registerSidechain.js
@@ -23,8 +23,12 @@ const logger = Logger();
 
 const MYSQL_ENDPOINT = config.endpoints.mysql;
 const transactionsIndexSchema = require('../../../database/schema/transactions');
+const crossChainMessagesIndexSchema = require('../../../database/schema/crossChainMessages');
+const blockchainAppsIndexSchema = require('../../../database/schema/blockchainApps');
 
 const getTransactionsIndex = () => getTableInstance('transactions', transactionsIndexSchema, MYSQL_ENDPOINT);
+const getCrossChainMessagesIndex = () => getTableInstance('ccm', crossChainMessagesIndexSchema, MYSQL_ENDPOINT);
+const getBlockchainAppsIndex = () => getTableInstance('blockchain_apps', blockchainAppsIndexSchema, MYSQL_ENDPOINT);
 
 // command specific constants
 const commandID = 0;
@@ -33,9 +37,22 @@ const commandName = 'sidechainRegistration';
 // eslint-disable-next-line no-unused-vars
 const processTransaction = async (blockHeader, tx, dbTrx) => {
 	const transactionsDB = await getTransactionsIndex();
+	const crossChainMessagesDB = await getCrossChainMessagesIndex();
+	const blockchainAppsDB = await getBlockchainAppsIndex();
 
 	logger.trace(`Indexing transaction ${tx.id} contained in block at height ${tx.height}`);
 	await transactionsDB.upsert(tx, dbTrx);
+
+	tx.name = tx.params.name;
+	await crossChainMessagesDB.upsert(tx, dbTrx);
+
+	const appInfo = {
+		name: tx.params.name,
+		chainID: tx.params.chainID,
+		state: tx.status,
+	};
+	await blockchainAppsDB.upsert(appInfo, dbTrx);
+
 	logger.debug(`Indexed transaction ${tx.id} contained in block at height ${tx.height}`);
 };
 
