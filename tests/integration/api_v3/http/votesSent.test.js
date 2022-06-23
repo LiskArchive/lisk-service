a93eb789d817817c0c18c20c87710102ec4b1fce
nagdahimanshu <himanshu.nagda@lightcurve.io>
2022-06-23 11:03:41 +0200

    Add integration tests for votes sent

---
+++ tests/integration/api_v3/http/votesSent.test.js
@@ -0,0 +1,101 @@
+/*
+ * LiskHQ/lisk-service
+ * Copyright © 2022 Lisk Foundation
+ *
+ * See the LICENSE file at the top-level directory of this distribution
+ * for licensing information.
+ *
+ * Unless otherwise agreed in a custom licensing agreement with the Lisk Foundation,
+ * no part of this software, including this file, may be copied, modified,
+ * propagated, or distributed except according to the terms contained in the
+ * LICENSE file.
+ *
+ * Removal or modification of this copyright notice is prohibited.
+ *
+ */
+const config = require('../../../config');
+const { api } = require('../../../helpers/api');
+
+const baseUrl = config.SERVICE_ENDPOINT;
+const baseUrlV3 = `${baseUrl}/api/v3`;
+
+const {
+	badRequestSchema,
+	metaSchema,
+} = require('../../../schemas/httpGenerics.schema');
+
+const {
+	voteSchema,
+} = require('../../../schemas/api_v3/vote.schema');
+
+[
+	`${baseUrlV3}/votes/sent`,
+].forEach(endpoint => {
+	// TODO: Enable when test blockchain is updated
+	xdescribe('Votes Sent (Votes) API', () => {
+		let refDelegate;
+		beforeAll(async () => {
+			let response;
+			do {
+				// eslint-disable-next-line no-await-in-loop
+				response = await api.get(`${baseUrlV3}/dpos/delegates?limit=1`);
+			} while (!response.data);
+			[refDelegate] = response.data;
+		});
+
+		describe(`GET ${endpoint}`, () => {
+			it('Returns list of votes when requested for existing account by address', async () => {
+				const response = await api.get(`${endpoint}?address=${refDelegate.address}`);
+				expect(response.data.votes).toBeInstanceOf(Array);
+				expect(response.data.votes.length).toBeGreaterThanOrEqual(1);
+				expect(response.data.votes.length).toBeLessThanOrEqual(10);
+				expect(response.data).toMap(voteSchema);
+				expect(response.meta).toMap(metaSchema);
+			});
+
+			it('Returns list of votes when requested for existing account by name', async () => {
+				if (refDelegate.name) {
+					const response = await api.get(`${endpoint}?name=${refDelegate.name}`);
+					expect(response.data.votes).toBeInstanceOf(Array);
+					expect(response.data.votes.length).toBeGreaterThanOrEqual(1);
+					expect(response.data.votes.length).toBeLessThanOrEqual(10);
+					expect(response.data).toMap(voteSchema);
+					expect(response.meta).toMap(metaSchema);
+				}
+			});
+
+			it('Returns list of votes when requested for existing account by address and limit=10', async () => {
+				const response = await api.get(`${endpoint}?address=${refDelegate.address}&limit=10`);
+				expect(response.data.votes).toBeInstanceOf(Array);
+				expect(response.data.votes.length).toBeGreaterThanOrEqual(1);
+				expect(response.data.votes.length).toBeLessThanOrEqual(10);
+				expect(response.data).toMap(voteSchema);
+				expect(response.meta).toMap(metaSchema);
+			});
+
+			it('Returns list of votes when requested for existing account by address, limit=10 and offset=1', async () => {
+				const response = await api.get(`${endpoint}?address=${refDelegate.address}&limit=10&offset=1`);
+				expect(response.data.votes).toBeInstanceOf(Array);
+				expect(response.data.votes.length).toBeGreaterThanOrEqual(1);
+				expect(response.data.votes.length).toBeLessThanOrEqual(10);
+				expect(response.data).toMap(voteSchema);
+				expect(response.meta).toMap(metaSchema);
+			});
+
+			it('No address -> bad request', async () => {
+				const response = await api.get(endpoint, 400);
+				expect(response).toMap(badRequestSchema);
+			});
+
+			it('invalid address -> bad request', async () => {
+				const response = await api.get(`${endpoint}?address=address=L`, 400);
+				expect(response).toMap(badRequestSchema);
+			});
+
+			it('invalid request param -> bad request', async () => {
+				const response = await api.get(`${endpoint}?invalidParam=invalid`, 400);
+				expect(response).toMap(badRequestSchema);
+			});
+		});
+	});
+});
---
+++ tests/integration/api_v3/rpc/votesSent.test.js
@@ -0,0 +1,96 @@
+/*
+ * LiskHQ/lisk-service
+ * Copyright © 2022 Lisk Foundation
+ *
+ * See the LICENSE file at the top-level directory of this distribution
+ * for licensing information.
+ *
+ * Unless otherwise agreed in a custom licensing agreement with the Lisk Foundation,
+ * no part of this software, including this file, may be copied, modified,
+ * propagated, or distributed except according to the terms contained in the
+ * LICENSE file.
+ *
+ * Removal or modification of this copyright notice is prohibited.
+ *
+ */
+const config = require('../../../config');
+const { request } = require('../../../helpers/socketIoRpcRequest');
+
+const {
+	invalidParamsSchema,
+	jsonRpcEnvelopeSchema,
+	metaSchema,
+} = require('../../../schemas/rpcGenerics.schema');
+
+const {
+	voteSchema,
+} = require('../../../schemas/api_v3/vote.schema');
+
+const wsRpcUrl = `${config.SERVICE_ENDPOINT}/rpc-v3`;
+
+[
+	'get.votes.sent',
+].forEach(methodName => {
+	const getVotes = async params => request(wsRpcUrl, methodName, params);
+	// TODO: Enable when test blockchain is updated
+	xdescribe(`Method ${methodName}`, () => {
+		let refDelegate;
+		beforeAll(async () => {
+			let response;
+			do {
+				// eslint-disable-next-line no-await-in-loop
+				response = await request(wsRpcUrl, 'get.accounts', { isDelegate: true, limit: 1 });
+			} while (!response.result);
+			[refDelegate] = response.result.data;
+		});
+
+		it('Returns list of votes when requested for existing account by address', async () => {
+			const response = await getVotes({ address: refDelegate.address });
+			expect(response).toMap(jsonRpcEnvelopeSchema);
+			const { result } = response;
+			expect(result.data).toMap(voteSchema);
+			expect(result.meta).toMap(metaSchema);
+		});
+
+		it('Returns list of votes when requested for existing account by name', async () => {
+			if (refDelegate.name) {
+				const response = await getVotes({ name: refDelegate.name });
+				expect(response).toMap(jsonRpcEnvelopeSchema);
+				const { result } = response;
+				expect(result.data).toMap(voteSchema);
+				expect(result.meta).toMap(metaSchema);
+			}
+		});
+
+		it('Returns list of votes when requested for existing account by address and limit=10', async () => {
+			const response = await getVotes({ address: refDelegate.address, limit: 10 });
+			expect(response).toMap(jsonRpcEnvelopeSchema);
+			const { result } = response;
+			expect(result.data).toMap(voteSchema);
+			expect(result.meta).toMap(metaSchema);
+		});
+
+		it('Returns list of votes when requested for existing account by address, limit=10 and offset=1', async () => {
+			const response = await getVotes({ address: refDelegate.address, limit: 10, offset: 1 });
+			expect(response).toMap(jsonRpcEnvelopeSchema);
+			const { result } = response;
+			expect(result.data).toMap(voteSchema);
+			expect(result.meta).toMap(metaSchema);
+		});
+
+		it('No address -> invalid param', async () => {
+			const response = await getVotes();
+			expect(response).toMap(invalidParamsSchema);
+		});
+
+		it('invalid request param -> invalid param', async () => {
+			const response = await getVotes({ invalidParam: 'invalid' });
+			expect(response).toMap(invalidParamsSchema);
+		});
+
+		it('invalid address -> invalid param', async () => {
+			const response = await getVotes({ address: 'L' });
+			expect(response).toMap(invalidParamsSchema);
+		});
+	});
+});
