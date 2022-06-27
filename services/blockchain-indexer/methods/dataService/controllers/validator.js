558234530e612560c36cf5aba398458eb943c935
nagdahimanshu <himanshu.nagda@lightcurve.io>
2022-06-27 12:07:24 +0200

    Add controller for validator endpoint

---
+++ services/blockchain-indexer/methods/dataService/controllers/validator.js
@@ -0,0 +1,32 @@
+/*
+ * LiskHQ/lisk-service
+ * Copyright © 2022 Lisk Foundation
+ *
+ * See the LICENSE file at the top-level directory of this distribution
+ * for licensing information.
+ *
+ * Unless otherwise agreed in a custom licensing agreement with the Lisk Foundation,
+ * no part of this software, including this file, may be copied, modified,
+ * propagated, or distributed except according to the terms contained in the
+ * LICENSE file.
+ *
+ * Removal or modification of this copyright notice is prohibited.
+ *
+ */
+const dataService = require('../../../shared/dataService');
+
+const getValidator = async params => {
+	const validator = {
+		data: {},
+		meta: {},
+	};
+	const response = await dataService.getValidator(params);
+	if (response.data) validator.data = response.data;
+	if (response.meta) validator.meta = response.meta;
+
+	return validator;
+};
+
+module.exports = {
+	getValidator,
+};
