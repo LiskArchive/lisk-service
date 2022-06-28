4ceb3fd655feadc919945b1380809e57196785e0
nagdahimanshu <himanshu.nagda@lightcurve.io>
2022-06-28 15:56:40 +0200

    Fix lint error

--- tests/schemas/api_v3/crossChainMessages.schema.js
+++ tests/schemas/api_v3/crossChainMessages.schema.js
@@ -18,50 +18,50 @@ import Joi from 'joi';
 const regex = require('./regex');
 
 const sender = {
-    address: Joi.string().pattern(regex.ADDRESS_BASE32).required(),
-    publicKey: Joi.string().pattern(regex.PUBLIC_KEY).optional(),
-    name: Joi.string().pattern(regex.NAME).optional(),
+	address: Joi.string().pattern(regex.ADDRESS_BASE32).required(),
+	publicKey: Joi.string().pattern(regex.PUBLIC_KEY).optional(),
+	name: Joi.string().pattern(regex.NAME).optional(),
 };
 
 const getCurrentTime = () => Math.floor(Date.now() / 1000);
 
 const block = {
-    id: Joi.string().min(1).max(64).pattern(regex.HASH_SHA256)
-        .required(),
-    height: Joi.number().integer().min(1).required(),
-    timestamp: Joi.number().integer().positive().max(getCurrentTime())
-        .required(),
-    transactionID: Joi.string().min(1).max(64).pattern(regex.HASH_SHA256)
-        .required(),
+	id: Joi.string().min(1).max(64).pattern(regex.HASH_SHA256)
+		.required(),
+	height: Joi.number().integer().min(1).required(),
+	timestamp: Joi.number().integer().positive().max(getCurrentTime())
+		.required(),
+	transactionID: Joi.string().min(1).max(64).pattern(regex.HASH_SHA256)
+		.required(),
 
 };
 
 const TRANSACTION_EXECUTION_STATUSES = [
-    'pending',
-    'succeeded',
-    'failed',
+	'pending',
+	'succeeded',
+	'failed',
 ];
 
 const CCM_STATUS = [
-    'ok',
-    'module_not_supported',
-    'ccm_not_supported',
-    'channel_unavailable',
-    'recovered',
+	'ok',
+	'module_not_supported',
+	'ccm_not_supported',
+	'channel_unavailable',
+	'recovered',
 ];
 
 const crossChainMessageSchema = {
-    moduleCommandID: Joi.string().required(),
-    moduleCommandName: Joi.string().required(),
-    nonce: Joi.string().pattern(regex.NONCE).required(),
-    fee: Joi.string().required(),
-    status: Joi.string().valid(...CCM_STATUS).required(),
-    sender: Joi.object(sender).required(),
-    params: Joi.object().required(),
-    block: Joi.array.items(block).required(),
-    executionStatus: Joi.string().valid(...TRANSACTION_EXECUTION_STATUSES).required(),
+	moduleCommandID: Joi.string().required(),
+	moduleCommandName: Joi.string().required(),
+	nonce: Joi.string().pattern(regex.NONCE).required(),
+	fee: Joi.string().required(),
+	status: Joi.string().valid(...CCM_STATUS).required(),
+	sender: Joi.object(sender).required(),
+	params: Joi.object().required(),
+	block: Joi.array.items(block).required(),
+	executionStatus: Joi.string().valid(...TRANSACTION_EXECUTION_STATUSES).required(),
 };
 
 module.exports = {
-    crossChainMessageSchema: Joi.object(crossChainMessageSchema),
+	crossChainMessageSchema: Joi.object(crossChainMessageSchema),
 };
