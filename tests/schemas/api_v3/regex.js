2647b07d7321b5783721cedcf7976965d511c43b
nagdahimanshu <himanshu.nagda@lightcurve.io>
2022-06-28 15:37:56 +0200

    Update ccm test schema

--- tests/schemas/api_v3/regex.js
+++ tests/schemas/api_v3/regex.js
@@ -22,6 +22,7 @@ const SEMVER = /^([0-9]+)\.([0-9]+)\.([0-9]+)(?:-([0-9A-Za-z-]+(?:\.[0-9A-Za-z-]
 const PUBLIC_KEY = /^([A-Fa-f0-9]{2}){32}$/;
 const NAME = /^[a-z0-9!@$&_.]{1,20}$/;
 const NEWSFEED_SOURCE = /^\b(?:(?:drupal_lisk(?:_general|_announcements)|twitter_lisk),?)+\b$/;
+const NONCE = /^[0-9]+$/;
 
 module.exports = {
 	ADDRESS_BASE32,
@@ -33,4 +34,5 @@ module.exports = {
 	SEMVER,
 	NAME,
 	NEWSFEED_SOURCE,
+	NONCE,
 };
