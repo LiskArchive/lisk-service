f247ae24aa323a6ebde56543b0cb216dcf5627ad
nagdahimanshu <himanshu.nagda@lightcurve.io>
2022-06-30 13:17:57 +0200

    Add implementation for votes/votesAggregates
    Signed-off-by: nagdahimanshu <himanshu.nagda@lightcurve.io>

--- services/blockchain-indexer/shared/indexer/transactionProcessor/dpos/voteDelegate.js
+++ services/blockchain-indexer/shared/indexer/transactionProcessor/dpos/voteDelegate.js
@@ -13,30 +13,104 @@
  * Removal or modification of this copyright notice is prohibited.
  *
  */
+const BluebirdPromise = require('bluebird');
+
 const {
 	Logger,
 	MySQL: { getTableInstance },
 } = require('lisk-service-framework');
+
+const {
+	getBase32AddressFromPublicKey,
+	getBase32AddressFromHex,
+} = require('../../../utils/accountUtils');
 const config = require('../../../../config');
 
 const logger = Logger();
 
 const MYSQL_ENDPOINT = config.endpoints.mysql;
 const transactionsIndexSchema = require('../../../database/schema/transactions');
+const votesIndexSchema = require('../../../database/schema/votes');
+const votesAggregateIndexSchema = require('../../../database/schema/votesAggregate');
 
 const getTransactionsIndex = () => getTableInstance('transactions', transactionsIndexSchema, MYSQL_ENDPOINT);
+const getVotesIndex = () => getTableInstance('votes', votesIndexSchema);
+const getVotesAggregateIndex = () => getTableInstance('votes_aggregate', votesAggregateIndexSchema);
 
 // Command specific constants
 const commandID = 1;
 const commandName = 'voteDelegate';
 
+const getVoteIndexingInfo = async (tx) => {
+	const votesDB = await getVotesIndex();
+	const votesToAggregateArray = [];
+	const votesMultiArray = tx.params.votes.map(async vote => {
+		const voteEntry = {};
+
+		voteEntry.sentAddress = getBase32AddressFromPublicKey(tx.senderPublicKey);
+		voteEntry.receivedAddress = getBase32AddressFromHex(vote.delegateAddress);
+		voteEntry.amount = vote.amount;
+
+		const [row] = await votesDB.find({
+			id: tx.id,
+			receivedAddress: voteEntry.receivedAddress,
+			limit: 1,
+		}, ['isAggregated']);
+		if (!row || !row.isAggregated) {
+			votesToAggregateArray.push({
+				amount: BigInt(vote.amount),
+				id: voteEntry.receivedAddress.concat(voteEntry.sentAddress),
+				voteObject: {
+					...voteEntry,
+					id: voteEntry.receivedAddress.concat(voteEntry.sentAddress),
+				},
+			});
+		}
+		voteEntry.id = tx.id;
+		return voteEntry;
+	});
+	let allVotePromises = [];
+	votesMultiArray.forEach(votes => allVotePromises = allVotePromises.concat(votes));
+	const votes = await BluebirdPromise.all(allVotePromises);
+	return { votes, votesToAggregateArray };
+};
+
+const updateVoteAggregatesTrx = async (voteToAggregate, trx) => {
+	const votesAggregateDB = await getVotesAggregateIndex();
+
+	const incrementParam = {
+		increment: {
+			amount: BigInt(voteToAggregate.amount),
+		},
+		where: {
+			property: 'id',
+			value: voteToAggregate.id,
+		},
+	};
+
+	const numRowsAffected = await votesAggregateDB.increment(incrementParam, trx);
+	if (numRowsAffected === 0) {
+		await votesAggregateDB.upsert(voteToAggregate.voteObject, trx);
+	}
+};
+
 // eslint-disable-next-line no-unused-vars
 const processTransaction = async (blockHeader, tx, dbTrx) => {
 	const transactionsDB = await getTransactionsIndex();
+	const votesDB = await getVotesIndex();
 
-	// TODO: Implement logic
+	const { votes, votesToAggregateArray } = await getVoteIndexingInfo(tx);
 
 	logger.trace(`Indexing transaction ${tx.id} contained in block at height ${tx.height}`);
+
+	await votesDB.upsert(votes, dbTrx);
+
+	await BluebirdPromise.map(
+		votesToAggregateArray,
+		async (voteToAggregate) => updateVoteAggregatesTrx(voteToAggregate, dbTrx),
+		{ concurrency: 1 },
+	);
+
 	await transactionsDB.upsert(tx, dbTrx);
 	logger.debug(`Indexed transaction ${tx.id} contained in block at height ${tx.height}`);
 };
