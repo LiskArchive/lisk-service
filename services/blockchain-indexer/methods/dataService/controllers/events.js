7546160f11a9a62fd2514e281000360bc9eb1635
nagdahimanshu <himanshu.nagda@lightcurve.io>
2022-07-06 17:29:58 +0200

    Add events to business layer

--- services/blockchain-indexer/methods/dataService/controllers/events.js
+++ services/blockchain-indexer/methods/dataService/controllers/events.js
@@ -13,17 +13,44 @@
  * Removal or modification of this copyright notice is prohibited.
  *
  */
-const dataService = require('../../../shared/dataService');
+// const dataService = require('../../../shared/dataService');
 
 const getEvents = async (params) => {
-	const events = {
-		data: {},
-		meta: {},
-	};
+	// const events = {
+	// 	data: {},
+	// 	meta: {},
+	// };
 
-	const response = await dataService.getEvents(params);
-	if (response.data) events.data = response.data;
-	if (response.meta) events.meta = response.meta;
+	// const response = await dataService.getEvents(params);
+	// if (response.data) events.data = response.data;
+	// if (response.meta) events.meta = response.meta;
+
+	const events = {
+		data: [
+			{
+				moduleID: '2',
+				moduleName: 'token',
+				typeID: '2',
+				data: {
+					amount: '150000000',
+					recipientAddress: 'lsk24cd35u4jdq8szo3pnsqe5dsxwrnazyqqqg5eu',
+					data: 'message',
+				},
+				topics: [],
+				block: {
+					id: '6258354802676165798',
+					height: 8350681,
+					timestamp: 28227090,
+				},
+			},
+		],
+		meta: {
+			count: 1,
+			offset: params.offset,
+			total: 100,
+		},
+		links: {},
+	};
 
 	return events;
 };
--- services/blockchain-indexer/shared/dataService/events.js
+++ services/blockchain-indexer/shared/dataService/events.js
@@ -13,35 +13,10 @@
  * Removal or modification of this copyright notice is prohibited.
  *
  */
-const getEvents = async (params) => {
-	// TODO: Replace with real implementation with the issue https://github.com/liskhq/lisk-service/issues/1122
-	const response = {
-		data: [
-			{
-				moduleID: '2',
-				moduleName: 'token',
-				typeID: '2',
-				data: {
-					amount: '150000000',
-					recipientAddress: 'lsk24cd35u4jdq8szo3pnsqe5dsxwrnazyqqqg5eu',
-					data: 'message',
-				},
-				topics: [],
-				block: {
-					id: '6258354802676165798',
-					height: 8350681,
-					timestamp: 28227090,
-				},
-			},
-		],
-		meta: {
-			count: 1,
-			offset: params.offset,
-			total: 100,
-		},
-		links: {},
-	};
+const dataService = require('../business');
 
+const getEvents = async (params) => {
+	const response = await dataService.getEvents(params);
 	return response;
 };
 
