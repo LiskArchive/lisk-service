51317b576ef01ce2414ae1ebab7727c51166ce29
nagdahimanshu <himanshu.nagda@lightcurve.io>
2022-07-07 15:04:36 +0200

    Fix eslint error

--- services/blockchain-indexer/shared/indexer/eventsIndex.js
+++ services/blockchain-indexer/shared/indexer/eventsIndex.js
@@ -21,54 +21,54 @@ const { requestConnector } = require('../utils/request');
 const { parseToJSONCompatObj } = require('../utils/parser');
 
 const encodeEvent = async (event) => {
-    const schemas = await requestConnector('getSchema');
-    const eventID = codec.encode(schemas.event, event);
-    return eventID;
+	const schemas = await requestConnector('getSchema');
+	const eventID = codec.encode(schemas.event, event);
+	return eventID;
 };
 
 const getEventsByHeight = async (height) => {
-    const events = await requestConnector('chain_getEvents', { height });
-    return parseToJSONCompatObj(events);
+	const events = await requestConnector('chain_getEvents', { height });
+	return parseToJSONCompatObj(events);
 };
 
 const getEventsInfoToIndex = async (blockHeader, events) => {
-    const eventsInfoToIndex = {
-        eventsInfo: [],
-        eventTopicsInfo: [],
-    };
+	const eventsInfoToIndex = {
+		eventsInfo: [],
+		eventTopicsInfo: [],
+	};
 
-    await BluebirdPromise.map(
-        events,
-        async (event) => {
-            const id = hash(event);
-            const encodedEvent = await encodeEvent(event);
+	await BluebirdPromise.map(
+		events,
+		async (event) => {
+			const id = hash(event);
+			const encodedEvent = await encodeEvent(event);
 
-            const eventInfo = {
-                id,
-                typeID: event.typeID,
-                moduleID: event.moduleID,
-                height: blockHeader.height,
-                index: event.index,
-                event: encodedEvent,
-            };
+			const eventInfo = {
+				id,
+				typeID: event.typeID,
+				moduleID: event.moduleID,
+				height: blockHeader.height,
+				index: event.index,
+				event: encodedEvent,
+			};
 
-            eventsInfoToIndex.eventsInfo.push(eventInfo);
+			eventsInfoToIndex.eventsInfo.push(eventInfo);
 
-            event.topics.forEach(topic => {
-                eventsInfoToIndex.eventTopicsInfo.push({
-                    ...eventInfo,
-                    timestamp: blockHeader.timestamp,
-                    topic,
-                });
-            });
-        },
-        { concurrency: events.length },
-    );
+			event.topics.forEach(topic => {
+				eventsInfoToIndex.eventTopicsInfo.push({
+					...eventInfo,
+					timestamp: blockHeader.timestamp,
+					topic,
+				});
+			});
+		},
+		{ concurrency: events.length },
+	);
 
-    return eventsInfoToIndex;
+	return eventsInfoToIndex;
 };
 
 module.exports = {
-    getEventsByHeight,
-    getEventsInfoToIndex,
+	getEventsByHeight,
+	getEventsInfoToIndex,
 };
