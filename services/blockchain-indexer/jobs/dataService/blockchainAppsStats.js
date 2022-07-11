6809ef5581020c9da3e9589e759904065f49796a
nagdahimanshu <himanshu.nagda@lightcurve.io>
2022-07-11 09:40:33 +0200

    Add job to calculate app stats every 15 mins

---
+++ services/blockchain-indexer/jobs/dataService/blockchainAppsStats.js
@@ -0,0 +1,45 @@
+/*
+ * LiskHQ/lisk-service
+ * Copyright © 2022 Lisk Foundation
+ *
+ * See the LICENSE file at the top-level directory of this distribution
+ * for licensing information.
+ *
+ * Unless otherwise agreed in a custom licensing agreement with the Lisk Foundation,
+ * no part of this software, including this file, may be copied, modified,
+ * propagated, or distributed except according to the terms contained in the
+ * LICENSE file.
+ *
+ * Removal or modification of this copyright notice is prohibited.
+ *
+ */
+const logger = require('lisk-service-framework').Logger();
+
+const {
+	reloadBlockchainAppsStats,
+} = require('../../shared/dataService');
+
+module.exports = [
+	{
+		name: 'reload.blockchain.apps.statistics',
+		description: 'Keep the blockchain apps statistics up-to-date',
+		schedule: '*/15 * * * *', // Every 15 min
+		init: async () => {
+			logger.debug('Initializing blockchain apps statistics cache...');
+			try {
+				await reloadBlockchainAppsStats();
+				logger.info('Successfully initialized blockchain apps statistics cache');
+			} catch (err) {
+				logger.warn(`Initializing blockchain apps statistics cache failed due to: ${err.message}`);
+			}
+		},
+		controller: async () => {
+			logger.debug('Reloading blockchain apps statistics cache...');
+			try {
+				await reloadBlockchainAppsStats();
+			} catch (err) {
+				logger.warn(`Reloading blockchain apps statistics cachee failed due to: ${err.message}`);
+			}
+		},
+	},
+];
