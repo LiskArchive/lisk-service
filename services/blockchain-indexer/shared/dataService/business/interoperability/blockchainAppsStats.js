c0494abe317e4b6f8098a459b4b5f89be7f1fce0
nagdahimanshu <himanshu.nagda@lightcurve.io>
2022-07-11 09:41:45 +0200

    Add implementation to cache apps stats in-memory

--- services/blockchain-indexer/shared/dataService/business/interoperability/blockchainAppsStats.js
+++ services/blockchain-indexer/shared/dataService/business/interoperability/blockchainAppsStats.js
@@ -13,7 +13,12 @@
 * Removal or modification of this copyright notice is prohibited.
 *
 */
-const { MySQL: { getTableInstance } } = require('lisk-service-framework');
+const {
+	Logger,
+	MySQL: { getTableInstance },
+} = require('lisk-service-framework');
+
+const logger = Logger();
 
 const { APP_STATE } = require('./constants');
 const config = require('../../../../config');
@@ -24,30 +29,46 @@ const blockchainAppsIndexSchema = require('../../../database/schema/blockchainAp
 
 const getBlockchainAppsIndex = () => getTableInstance('blockchain_apps', blockchainAppsIndexSchema, MYSQL_ENDPOINT);
 
+let blockchainAppsStatsCache = {};
+
 const getBlockchainAppsStatistics = async () => {
-	// TODO: Update implementation once interoperability_getOwnChainAccount is available
-	const blockchainAppsDB = await getBlockchainAppsIndex();
-
-	const numActiveChains = await blockchainAppsDB.count({ state: APP_STATE.ACTIVE });
-	const numRegisteredChains = await blockchainAppsDB.count({ state: APP_STATE.REGISTERED });
-	const numTerminatedChains = await blockchainAppsDB.count({ state: APP_STATE.TERMINATED });
-
-	const response = {
-		registered: numRegisteredChains,
-		active: numActiveChains,
-		terminated: numTerminatedChains,
-		// TODO: Get these information directly from SDK once issue https://github.com/LiskHQ/lisk-sdk/issues/7225 is closed
-		totalSupplyLSK: '',
-		stakedLSK: '',
-		inflationRate: '',
-	};
-
-	return {
-		data: response,
+	const blockchainAppsStats = {
+		data: {},
 		meta: {},
 	};
+
+	blockchainAppsStats.data = blockchainAppsStatsCache;
+	return blockchainAppsStats;
+};
+
+const reloadBlockchainAppsStats = async () => {
+	try {
+		// TODO: Update implementation once interoperability_getOwnChainAccount is available
+		const blockchainAppsDB = await getBlockchainAppsIndex();
+
+		const numActiveChains = await blockchainAppsDB.count({ state: APP_STATE.ACTIVE });
+		const numRegisteredChains = await blockchainAppsDB.count({ state: APP_STATE.REGISTERED });
+		const numTerminatedChains = await blockchainAppsDB.count({ state: APP_STATE.TERMINATED });
+
+		logger.debug('Updating blockchain apps statistics cache');
+
+		blockchainAppsStatsCache = {
+			registered: numRegisteredChains,
+			active: numActiveChains,
+			terminated: numTerminatedChains,
+			// TODO: Get these information directly from SDK once issue https://github.com/LiskHQ/lisk-sdk/issues/7225 is closed
+			totalSupplyLSK: '',
+			stakedLSK: '',
+			inflationRate: '',
+		};
+
+		logger.info('Updated blockchain apps statistics cache');
+	} catch (err) {
+		logger.warn(`Failed to update blockchain apps statistics cache due to: ${err.message}`);
+	}
 };
 
 module.exports = {
 	getBlockchainAppsStatistics,
+	reloadBlockchainAppsStats,
 };
