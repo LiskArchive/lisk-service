652a9333930d24bef8cbf3e71bd7d2573279596d
nagdahimanshu <himanshu.nagda@lightcurve.io>
2022-07-26 09:13:49 +0200

    Update event dummy response

--- services/blockchain-indexer/methods/dataService/controllers/events.js
+++ services/blockchain-indexer/methods/dataService/controllers/events.js
@@ -40,11 +40,7 @@ const getEvents = async (params) => {
 				moduleID: '2',
 				moduleName: 'token',
 				typeID: '2',
-				data: {
-					amount: '150000000',
-					recipientAddress: 'lsk24cd35u4jdq8szo3pnsqe5dsxwrnazyqqqg5eu',
-					data: 'message',
-				},
+				data: '0a14e135813f51103e7645ed87a0562a823d2fd48bc612207eef331c6d58f3962f5fb35b13f780f0ee7d93fbc37a3e9f4ccbdc6d1551db801a303629827aaa0836111137215708fd2007e9221ca1d56b29b98d8e9747ec3243c0549dc2091515d2bdd72fb28acef50160',
 				topics: [],
 				block: {
 					id: '6258354802676165798',
