3df76204a981f3556ee8b3e5bbd813b5ca9e59fa
nagdahimanshu <himanshu.nagda@lightcurve.io>
2022-08-04 18:31:17 +0200

    Update query for distinct count

--- services/blockchain-app-registry/shared/metadata.js
+++ services/blockchain-app-registry/shared/metadata.js
@@ -50,7 +50,7 @@ const getBlockchainAppsMetaList = async (params) => {
 		};
 	}
 
-	const [{ 'COUNT(DISTINCT(name))': total }] = await applicationsDB.rawQuery('SELECT COUNT(DISTINCT(name)) from applications');
+	const [{ count: total }] = await applicationsDB.rawQuery('SELECT COUNT(DISTINCT(name)) as count from applications');
 
 	const defaultApps = await applicationsDB.find(
 		{ ...params, isDefault: true },
