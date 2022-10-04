f3695a5e20515e6775c2dfce58c56af1275dbffc
Priojeet Das Priyom <priojeetpriyom@gmail.com>
2022-10-04 20:18:13 +0200

    :art: Add todo to comment

--- tests/integration/api_v3/http/fees.test.js
+++ tests/integration/api_v3/http/fees.test.js
@@ -30,7 +30,7 @@ const baseUrl = config.SERVICE_ENDPOINT;
 const baseUrlV2 = `${baseUrl}/api/v3`;
 const endpoint = `${baseUrlV2}/fees`;
 
-// Enable test once response structure is refactored for minFeePerByte
+// TODO: Enable test once response structure is refactored for minFeePerByte
 xdescribe('Fee estimates API', () => {
 	it('estimate fees true -> 200 OK', async () => {
 		const response = await api.get(`${endpoint}`);
--- tests/integration/api_v3/rpc/fees.test.js
+++ tests/integration/api_v3/rpc/fees.test.js
@@ -30,7 +30,7 @@ const {
 const wsRpcUrl = `${config.SERVICE_ENDPOINT}/rpc-v3`;
 const requestFeeEstimate = async () => request(wsRpcUrl, 'get.fees');
 
-// Enable test once response structure is refactored for minFeePerByte
+// TODO: Enable test once response structure is refactored for minFeePerByte
 xdescribe('get.fees', () => {
 	it('returns estimated fees, when supported', async () => {
 		const response = await requestFeeEstimate();
