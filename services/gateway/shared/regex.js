410e957e577c65249b9c1c14ca9d7657fc459271
Himanshu Nagda <69150921+nagdahimanshu@users.noreply.github.com>
2022-10-20 19:02:38 +0530

    Update services/gateway/shared/regex.js
    Co-authored-by: Sameer Kumar Subudhi <sameer.subudhi@lightcurve.io>

--- services/gateway/shared/regex.js
+++ services/gateway/shared/regex.js
@@ -34,7 +34,7 @@ const TOKEN_ID = /^\b[a-fA-F0-9]{16}\b$/;
 const BLS_KEY = /^\b[a-fA-F0-9]{96}\b$/;
 const PROOF_OF_POSSESSION = /^\b[a-fA-F0-9]{192}\b$/;
 const TRANSACTION = /^\b[0-9a-fA-F]+\b$/;
-const MODULE = /^(?:[a-z ]+,)*[a-z ]+$/;
+const MODULE = /^\b(?:[\w!@$&.]{1,32}|,)+\b$/;
 
 module.exports = {
 	PUBLIC_KEY,
