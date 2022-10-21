a44f2f74c863d1f429b26191e198edad1a5b5249
Priojeet Das Priyom <priojeetpriyom@gmail.com>
2022-10-21 11:31:32 +0200

    Add coverage

--- tests/integration/api_v3/http/dryRunTransactions.test.js
+++ tests/integration/api_v3/http/dryRunTransactions.test.js
@@ -15,6 +15,7 @@
  */
 const config = require('../../../config');
 const { api } = require('../../../helpers/api');
+const { VALID_TRANSACTION, INVALID_TRANSACTION } = require('../constants/dryRunTransactions');
 
 const {
 	badRequestSchema,
@@ -34,7 +35,8 @@ const endpoint = `${baseUrlV3}/transactions/dryrun`;
 describe('Post dryrun transactions API', () => {
 	it('Post dryrun transaction succesfully', async () => {
 		const response = await api.post(
-			`${endpoint}?transaction=0a05746f6b656e12087472616e7366657218042080c2d72f2a20a3f96c50d0446220ef2f98240898515cbba8155730679ca35326d98dcfb680f032270a0800000000000000001080c2d72f1a1474e3ba5ade3e94451bd4de9d19917c8e6eff624d22003a401a78d6a2d2c5d980bbbe4f12f2d59b937b6dbd7fa01e3644ce3e3d06bf3f5aff2aff728cb902031f93039dd80ba91fabfa4ea4dca813d281ec957789727baa0f`,
+			endpoint,
+			{ transaction: VALID_TRANSACTION },
 		);
 
 		expect(response).toMap(goodRequestSchema);
@@ -42,19 +44,27 @@ describe('Post dryrun transactions API', () => {
 		expect(response.meta).toMap(metaSchema);
 	});
 
-	it('throws error when posting invalid binary transaction', async () => {
+	it('Throws error when posting invalid binary transaction', async () => {
 		const dryrunTransaction = await api.post(
 			endpoint,
-			{ transaction: '0802100018002080c2d72f2a2027f87a414b549e79a940bf24fdf2b2f05e7f22aeeecc86a322e0880a0cebdf3ef171214aebd99f07218109162a905d0e0c91e58bedc83c51a0e746f6b656e207472616e736665723a40a30998db4e96911a3d784b0a01b817baf64ec9745d7c0407255967506cb01764220e2e6ce66183d07' },
+			{ transaction: INVALID_TRANSACTION },
 			500,
 		);
 		expect(dryrunTransaction).toMap(badRequestSchema);
 	});
 
-	it('throws error in case of invalid query params', async () => {
+	it('No transaction -> bad request', async () => {
+		const response = await api.post(endpoint, {}, 400);
+		expect(response).toMap(wrongInputParamSchema);
+	});
+
+	it('Throws error in case of invalid query params', async () => {
 		const dryrunTransaction = await api.post(
 			endpoint,
-			{ transactions: '0802100018002080c2d72f2a2027f87a414b549e79a940bf24fdf2b2f05e7f22aeeecc86a322e0880a0cebdf3ef171214aebd99f07218109162a905d0e0c91e58bedc83c51a0e746f6b656e207472616e736665723a40a30998db4e96911a3d784b0a01b817baf64ec9745d7c0407255967506cb01764220e2e6ce66183d07' },
+			{
+				transaction: VALID_TRANSACTION,
+				transactions: INVALID_TRANSACTION,
+			},
 			400,
 		);
 		expect(dryrunTransaction).toMap(wrongInputParamSchema);
--- tests/integration/api_v3/rpc/dryRunTransactions.test.js
+++ tests/integration/api_v3/rpc/dryRunTransactions.test.js
@@ -14,6 +14,7 @@
  *
  */
 const config = require('../../../config');
+const { VALID_TRANSACTION, INVALID_TRANSACTION } = require('../constants/dryRunTransactions');
 
 const {
 	request,
@@ -37,7 +38,7 @@ describe('Method post.transactions', () => {
 	it('Post transaction succesfully', async () => {
 		const response = await postDryrunTransaction(
 			{
-				transaction: '0a05746f6b656e12087472616e7366657218042080c2d72f2a20a3f96c50d0446220ef2f98240898515cbba8155730679ca35326d98dcfb680f032270a0800000000000000001080c2d72f1a1474e3ba5ade3e94451bd4de9d19917c8e6eff624d22003a401a78d6a2d2c5d980bbbe4f12f2d59b937b6dbd7fa01e3644ce3e3d06bf3f5aff2aff728cb902031f93039dd80ba91fabfa4ea4dca813d281ec957789727baa0f',
+				transaction: VALID_TRANSACTION,
 			},
 		);
 		expect(response).toMap(jsonRpcEnvelopeSchema);
@@ -49,12 +50,22 @@ describe('Method post.transactions', () => {
 	});
 
 	it('invalid binary transaction -> empty response', async () => {
-		const response = await postDryrunTransaction({ transaction: 'a200fe9a3f1a21b5530f27f87a414b549e79a940bf24fdf2b2f05e7f22aeeecc86a32250880c2d72f1214b49074a2eb04e7611908985f02c12fb7cd488d451a0874657374207478733a4065faed7b49d1ee63730cbb545ea25e5036' }).catch(e => e);
+		const response = await postDryrunTransaction({
+			transaction: INVALID_TRANSACTION,
+		}).catch(e => e);
 		expect(response).toMap(serverErrorSchema);
 	});
 
+	it('No transaction -> invalid param', async () => {
+		const response = await postDryrunTransaction();
+		expect(response).toMap(invalidParamsSchema);
+	});
+
 	it('invalid query parameter -> -32602', async () => {
-		const response = await postDryrunTransaction({ transactions: '0802100018002080c2d72f2a200fe9a3f1a21b5530f27f87a414b549e79a940bf24fdf2b2f05e7f22aeeecc86a32250880c2d72f1214b49074a2eb04e7611908985f02c12fb7cd488d451a0874657374207478733a4065faed7b49d1ee63730cbb545ea25e50361581e35412eeffac3b35746afd1176d2ee1e270e8b072b1ccfad2d64f72918063c383003971600d56b168d6e429f05' }).catch(e => e);
+		const response = await postDryrunTransaction({
+			transaction: VALID_TRANSACTION,
+			transactions: INVALID_TRANSACTION,
+		}).catch(e => e);
 		expect(response).toMap(invalidParamsSchema);
 	});
 });
