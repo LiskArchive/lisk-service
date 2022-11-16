b9224c576bf2e08497a123dddfe208e13271e5c9
nagdahimanshu <himanshu.nagda@lightcurve.io>
2022-11-16 21:06:47 +0530

    Update PR

--- services/blockchain-indexer/shared/indexer/tokenIndex.js
+++ services/blockchain-indexer/shared/indexer/tokenIndex.js
@@ -28,7 +28,7 @@ const topLSKAddressesIndexSchema = require('../database/schema/topLSKAddresses')
 const MYSQL_ENDPOINT = config.endpoints.mysql;
 
 let liskTokenID;
-const TOKEN_ID_LSK_MAINCHAIN = '0000000000000000';
+const TOKEN_ID_LSK_MAINNET = '0000000000000000';
 
 const getTopLSKAddressesIndex = () => getTableInstance(
 	topLSKAddressesIndexSchema.tableName,
@@ -40,7 +40,7 @@ const getLiskBalanceByAddress = async (address) => {
 	if (!liskTokenID) {
 		const { chainID } = (await dataService.getNetworkStatus()).data;
 		const chainIDPrefix = chainID.substring(0, 2); // Determine network
-		liskTokenID = chainIDPrefix.concat(TOKEN_ID_LSK_MAINCHAIN.slice(chainIDPrefix.length));
+		liskTokenID = chainIDPrefix.concat(TOKEN_ID_LSK_MAINNET.slice(chainIDPrefix.length));
 	}
 
 	const response = await requestConnector(
