92ece441dd4ab76beac3a867751075f05147b667
nagdahimanshu <himanshu.nagda@lightcurve.io>
2022-12-06 18:30:57 +0530

    Add implementation to retrieve claimable rewards

---
+++ services/blockchain-indexer/shared/dataService/business/pos/claimableRewards.js
@@ -0,0 +1,49 @@
+/*
+ * LiskHQ/lisk-service
+ * Copyright © 2022 Lisk Foundation
+ *
+ * See the LICENSE file at the top-level directory of this distribution
+ * for licensing information.
+ *
+ * Unless otherwise agreed in a custom licensing agreement with the Lisk Foundation,
+ * no part of this software, including this file, may be copied, modified,
+ * propagated, or distributed except according to the terms contained in the
+ * LICENSE file.
+ *
+ * Removal or modification of this copyright notice is prohibited.
+ *
+ */
+const { getLisk32AddressFromPublicKey } = require('../../../utils/accountUtils');
+const { requestConnector } = require('../../../utils/request');
+const { getAddressByName } = require('../../../utils/validatorUtils');
+
+const getClaimableRewards = async params => {
+	const claimableRewards = {
+		data: [],
+		meta: {},
+	};
+
+	if (!params.address && params.name) {
+		params.address = await getAddressByName(params.name);
+	}
+
+	if (!params.address && params.publicKey) {
+		params.address = getLisk32AddressFromPublicKey(params.publicKey);
+	}
+
+	const response = await requestConnector('getClaimableRewards', { address: params.address });
+	claimableRewards.data = response.rewards;
+	claimableRewards.data = claimableRewards.data.slice(params.offset, params.offset + params.limit);
+
+	claimableRewards.meta = {
+		count: claimableRewards.data.length,
+		offset: params.offset,
+		total: response.rewards.length,
+	};
+
+	return claimableRewards;
+};
+
+module.exports = {
+	getClaimableRewards,
+};
