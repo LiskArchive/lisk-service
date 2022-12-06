9f1e354cfb6228da86fc6e5b079f8383d020200e
nagdahimanshu <himanshu.nagda@lightcurve.io>
2022-12-06 18:17:25 +0530

    :sparkles: Add gateway endpoint for claimable rewards

---
+++ services/gateway/sources/version3/posClaimableRewards.js
@@ -0,0 +1,36 @@
+/*
+ * LiskHQ/lisk-service
+ * Copyright © 2022 Lisk Foundation
+ *
+ * See the LICENSE file at the top-level directory of this distribution
+ * for licensing information.
+ *
+ * Unless otherwise agreed in a custom licensing agreement with the Lisk Foundation,
+ * no part of this software, including this file, may be copied, modified,
+ * propagated, or distributed except according to the terms contained in the
+ * LICENSE file.
+ *
+ * Removal or modification of this copyright notice is prohibited.
+ *
+ */
+const posClaimableReward = require('./mappings/posClaimableReward');
+
+module.exports = {
+	type: 'moleculer',
+	method: 'indexer.pos.rewards.claimable',
+	params: {
+		address: '=,string',
+		publicKey: '=,string',
+		name: '=,string',
+		limit: '=,number',
+		offset: '=,number',
+	},
+	definition: {
+		data: ['data', posClaimableReward],
+		meta: {
+			count: '=,number',
+			offset: '=,number',
+			total: '=,number',
+		},
+	},
+};
