1d3fb1a491e5212d75fad8b05067f54f8938b952
Priojeet Das Priyom <priojeetpriyom@gmail.com>
2022-12-08 12:23:43 +0100

    Update test

---
+++ tests/schemas/api_v3/rewardConstants.schema.js
@@ -0,0 +1,32 @@
+/*
+ * LiskHQ/lisk-service
+ * Copyright © 2022 Lisk Foundation
+ *
+ * See the LICENSE file at the top-level directory of this distribution
+ * for licensing information.
+ *
+ * Unless otherwise agreed in a custom licensing agreement with the Lisk Foundation,
+ * no part of this software, including this file, may be copied, modified,
+ * propagated, or distributed except according to the terms contained in the
+ * LICENSE file.
+ *
+ * Removal or modification of this copyright notice is prohibited.
+ *
+ */
+import Joi from 'joi';
+import regex from './regex';
+
+const data = {
+	rewardTokenID: Joi.string().pattern(regex.DIGITS).required(),
+};
+
+const meta = {};
+
+const rewardConstantsResponseSchema = {
+	data: Joi.object(data).required(),
+	meta: Joi.object(meta).required(),
+};
+
+module.exports = {
+	rewardConstantsResponseSchema: Joi.object(rewardConstantsResponseSchema).required(),
+};
