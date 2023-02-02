ddf6ac19490d4153d96308ab4b69e0a18ce75b7f
nagdahimanshu <himanshu.nagda@lightcurve.io>
2023-02-02 18:59:11 +0100

    :white_check_mark: Add unit test for verifyFileChecksum method

---
+++ services/blockchain-connector/tests/unit/download.test.js
@@ -0,0 +1,33 @@
+/*
+ * LiskHQ/lisk-service
+ * Copyright © 2023 Lisk Foundation
+ *
+ * See the LICENSE file at the top-level directory of this distribution
+ * for licensing information.
+ *
+ * Unless otherwise agreed in a custom licensing agreement with the Lisk Foundation,
+ * no part of this software, including this file, may be copied, modified,
+ * propagated, or distributed except according to the terms contained in the
+ * LICENSE file.
+ *
+ * Removal or modification of this copyright notice is prohibited.
+ *
+ */
+const { resolve } = require('path');
+const { verifyFileChecksum } = require('../../shared/utils/download');
+
+const genesisBlockFilePath = resolve(`${process.cwd()}/tests/fixtures/genesis_block.json`);
+const checksumFilePath = resolve(`${process.cwd()}/tests/fixtures/genesis_block.json.SHA256`);
+const invalidChecksumFilePath = resolve(`${process.cwd()}/tests/fixtures/invalid_genesis_block.json.SHA256`);
+
+describe('Download utility -> verifyFileChecksum method', () => {
+	it('Return true in case of valid checksum', async () => {
+		const result = await verifyFileChecksum(genesisBlockFilePath, checksumFilePath);
+		expect(result).toEqual(true);
+	});
+
+	it('Return false in case of invalid checksum', async () => {
+		const result = await verifyFileChecksum(genesisBlockFilePath, invalidChecksumFilePath);
+		expect(result).toEqual(false);
+	});
+});
