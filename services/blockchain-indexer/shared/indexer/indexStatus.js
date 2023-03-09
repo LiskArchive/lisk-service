5a98cc3eade31f4b897190104c3cbe03b2c0e741
nagdahimanshu <himanshu.nagda@lightcurve.io>
2023-03-09 14:56:31 +0100

    Fix deepscan issue

--- services/blockchain-indexer/shared/indexer/indexStatus.js
+++ services/blockchain-indexer/shared/indexer/indexStatus.js
@@ -139,7 +139,7 @@ const init = async () => {
 
 	// Index stakers and commission information available in genesis block
 	await indexValidatorCommissionInfo(genesisBlock);
-	await indexStakersInfo(genesisBlock);
+	await indexStakersInfo();
 };
 
 module.exports = {
