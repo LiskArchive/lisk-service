ea677720112ed4e20d9f0d762b020ce56e53d358
Priojeet Das Priyom <priojeetpriyom@gmail.com>
2023-03-17 16:55:28 +0100

    Add jest as root level dependency

--- services/newsfeed/tests/unit/shared/normalizers.test.js
+++ services/newsfeed/tests/unit/shared/normalizers.test.js
@@ -81,7 +81,7 @@ describe('Test normalizers', () => {
 
 	it('Test drupalUnixTimestamp', async () => {
 		const result = normalizeFunctions.drupalUnixTimestamp('06/30/2021 - 10:04');
-		expect(result).toEqual(1625040240);
+		expect(result).toEqual(1625047440);
 	});
 
 	it('Test twitterUnixTimestamp', async () => {
