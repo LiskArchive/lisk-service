e0a0778f83578ee9a483a9f84da9b3c55830fd08
Vardan Nadkarni <vardan.nadkarni@gmail.com>
2023-04-27 13:22:34 +0530

    Lint fixes

--- services/gateway/tests/constants/generateDocs.js
+++ services/gateway/tests/constants/generateDocs.js
@@ -775,141 +775,141 @@ const createApiDocsExpectedResponse = {
 	},
 	'/token/account/exists': {
 		get: {
-			description: "Returns existence of an account for the specified token.\n RPC => get.token.account.exists",
+			description: 'Returns existence of an account for the specified token.\n RPC => get.token.account.exists',
 			parameters: [
 				{
-					$ref: "#/parameters/address",
+					$ref: '#/parameters/address',
 				},
 				{
-					$ref: "#/parameters/publicKey",
+					$ref: '#/parameters/publicKey',
 				},
 				{
-					$ref: "#/parameters/accountName",
+					$ref: '#/parameters/accountName',
 				},
 				{
-					$ref: "#/parameters/tokenID",
+					$ref: '#/parameters/tokenID',
 				},
 			],
 			responses: {
 				200: {
-					description: "Returns existence of an account for the specified token.",
-					"schema": {
-						$ref: "#/definitions/tokenAccountExistsWithEnvelope",
+					description: 'Returns existence of an account for the specified token.',
+					schema: {
+						$ref: '#/definitions/tokenAccountExistsWithEnvelope',
 					},
 				},
 				400: {
-					description: "Bad request",
-					"schema": {
-						$ref: "#/definitions/badRequest",
+					description: 'Bad request',
+					schema: {
+						$ref: '#/definitions/badRequest',
 					},
 				},
 				404: {
-					description: "Not found",
-					"schema": {
-						$ref: "#/definitions/notFound",
+					description: 'Not found',
+					schema: {
+						$ref: '#/definitions/notFound',
 					},
 				},
 			},
-			summary: "Requests to check existence of an account for the specified token.",
+			summary: 'Requests to check existence of an account for the specified token.',
 			tags: [
-				"Token",
+				'Token',
 			],
 		},
 	},
 	'/token/balances': {
 		get: {
-			description: "Returns tokens information\n RPC => get.token.balances",
+			description: 'Returns tokens information\n RPC => get.token.balances',
 			parameters: [
 				{
-					$ref: "#/parameters/address",
+					$ref: '#/parameters/address',
 				},
 				{
-					$ref: "#/parameters/tokenID",
+					$ref: '#/parameters/tokenID',
 				},
 				{
-					$ref: "#/parameters/limit",
+					$ref: '#/parameters/limit',
 				},
 				{
-					$ref: "#/parameters/offset",
+					$ref: '#/parameters/offset',
 				},
 			],
 			responses: {
 				200: {
-					description: "Returns a list of supported tokens by the blockchain application",
-					"schema": {
-						$ref: "#/definitions/tokenWithEnvelope",
+					description: 'Returns a list of supported tokens by the blockchain application',
+					schema: {
+						$ref: '#/definitions/tokenWithEnvelope',
 					},
 				},
 				400: {
-					description: "Bad request",
+					description: 'Bad request',
 					schema: {
-						$ref: "#/definitions/badRequest",
+						$ref: '#/definitions/badRequest',
 					},
 				},
 				404: {
-					description: "Not found",
+					description: 'Not found',
 					schema: {
-						$ref: "#/definitions/notFound",
+						$ref: '#/definitions/notFound',
 					},
 				},
 			},
-			summary: "Requests tokens information",
+			summary: 'Requests tokens information',
 			tags: [
-				"Token",
+				'Token',
 			],
 		},
 	},
 	'/token/constants': {
 		get: {
-			description: "Requests all the configured constants for the Token module.\n RPC => get.token.constants",
+			description: 'Requests all the configured constants for the Token module.\n RPC => get.token.constants',
 			responses: {
 				200: {
-					description: "Returns all the configured constants for the Token module.",
+					description: 'Returns all the configured constants for the Token module.',
 					schema: {
-						$ref: "#/definitions/tokenConstantsWithEnvelope",
+						$ref: '#/definitions/tokenConstantsWithEnvelope',
 					},
 				},
 				400: {
-					description: "Bad request",
+					description: 'Bad request',
 					schema: {
-						$ref: "#/definitions/badRequest",
+						$ref: '#/definitions/badRequest',
 					},
 				},
 				404: {
-					description: "Not found",
+					description: 'Not found',
 					schema: {
-						$ref: "#/definitions/notFound",
+						$ref: '#/definitions/notFound',
 					},
 				},
 			},
-			summary: "Requests Token module constants.",
+			summary: 'Requests Token module constants.',
 			tags: [
-				"Token",
+				'Token',
 			],
 		},
 	},
 	'/token/ids': {
 		get: {
-			description: "Returns all the available token identifiers.\n RPC => get.token.ids",
+			description: 'Returns all the available token identifiers.\n RPC => get.token.ids',
 			parameters: [
 				{
-					$ref: "#/parameters/limit",
+					$ref: '#/parameters/limit',
 				},
 				{
-					$ref: "#/parameters/offset",
+					$ref: '#/parameters/offset',
 				},
 			],
 			responses: {
 				200: {
-					description: "Returns all the available token identifiers.",
+					description: 'Returns all the available token identifiers.',
 					schema: {
-						$ref: "#/definitions/tokenIDsWithEnvelope",
+						$ref: '#/definitions/tokenIDsWithEnvelope',
 					},
 				},
 			},
-			summary: "Requests the list of available tokens identifiers.",
+			summary: 'Requests the list of available tokens identifiers.',
 			tags: [
-				"Token",
+				'Token',
 			],
 		},
 	},
@@ -921,25 +921,25 @@ const createApiDocsExpectedResponse = {
 				200: {
 					description: "Returns the token summary. The 'supportedTokens' is an empty list when all the tokens are supported on the blockchain application.",
 					schema: {
-						$ref: "#/definitions/tokenSummaryWithEnvelope",
+						$ref: '#/definitions/tokenSummaryWithEnvelope',
 					},
 				},
 				400: {
-					description: "Bad request",
+					description: 'Bad request',
 					schema: {
-						$ref: "#/definitions/badRequest",
+						$ref: '#/definitions/badRequest',
 					},
 				},
 				404: {
-					description: "Not found",
+					description: 'Not found',
 					schema: {
-						$ref: "#/definitions/notFound",
+						$ref: '#/definitions/notFound',
 					},
 				},
 			},
-			summary: "Requests the tokens summary for the current blockchain application.",
+			summary: 'Requests the tokens summary for the current blockchain application.',
 			tags: [
-				"Token",
+				'Token',
 			],
 		},
 	},
