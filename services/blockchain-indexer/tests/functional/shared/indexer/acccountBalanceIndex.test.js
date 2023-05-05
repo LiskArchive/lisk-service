1685b7d804eef85e5a1e6d400da74755cf25a9c3
Priojeet Das Priyom <priojeetpriyom@gmail.com>
2023-05-05 16:43:04 +0200

    Add test coverage for scheduleAccountBalanceUpdateFromEvents

--- services/blockchain-indexer/tests/functional/shared/indexer/acccountBalanceIndex.test.js
+++ services/blockchain-indexer/tests/functional/shared/indexer/acccountBalanceIndex.test.js
@@ -1,3 +1,5 @@
+jest.setTimeout(30000);
+
 /*
  * LiskHQ/lisk-service
  * Copyright © 2023 Lisk Foundation
@@ -20,10 +22,11 @@ const {
 
 const config = require('../../../../config');
 const accountBalancesTableSchema = require('../../../../shared/database/schema/accountBalances');
-const { updateAccountBalances, accountBalanceIndexQueue } = require('../../../../shared/indexer/accountBalanceIndex');
-const { getTokenModuleUserSubStoreInfo, MODULE_SUB_STORE } = require('../../../../shared/indexer/genesisBlock');
+const { updateAccountBalances, accountBalanceIndexQueue, scheduleAccountBalanceUpdateFromEvents } = require('../../../../shared/indexer/accountBalanceIndex');
+const { MODULE_SUB_STORE } = require('../../../../shared/indexer/genesisBlock');
 const request = require('../../../../shared/utils/request');
 const { MODULE } = require('../../../../shared/constants');
+const { eventsIncludingTokenModule } = require('../../../constants/events');
 
 const MYSQL_ENDPOINT = config.endpoints.mysql;
 
@@ -81,5 +84,31 @@ describe('Test updateAccountBalances method', () => {
 		const balanceAfterUpdate = await accountBalancesTable.find({ address: accountInfo.address, tokenID: accountInfo.tokenID }, ['balance']);
 		expect(balanceAfterUpdate.length).toBe(1);
 		expect(balanceAfterUpdate[0].balance).toBeGreaterThanOrEqual(0);
+	});
+});
+
+describe('Test scheduleAccountBalanceUpdateFromEvents method', () => {
+	beforeAll(async () => {
+		await accountBalanceIndexQueue.queue.empty();
+		await accountBalanceIndexQueue.queue.pause();
+	});
+
+	afterAll(async () => accountBalanceIndexQueue.queue.resume());
+
+	it('should schedule account balance update correctly for token module events', async () => {
+		const queueCountBeforeScheduling = await accountBalanceIndexQueue.queue.count();
+		expect(queueCountBeforeScheduling).toBe(0);
+
+		// Delete all balances and check balance before update
+		const accountInfo = usersSubStoreInfos[0];
+		await accountBalancesTable.delete({});
+		const balanceBeforeUpdate = await accountBalancesTable.find({ address: accountInfo.address, tokenID: accountInfo.tokenID }, ['balance']);
+		expect(balanceBeforeUpdate.length).toBe(0);
+
+		await scheduleAccountBalanceUpdateFromEvents(eventsIncludingTokenModule);
+
+		// Check balance after update
+		const queueCountAfterScheduling = await accountBalanceIndexQueue.queue.count();
+		expect(queueCountAfterScheduling).toBeGreaterThanOrEqual(1);
 	});
 });
