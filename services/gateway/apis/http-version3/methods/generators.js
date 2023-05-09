00329cced8d80cebf08536f53c09000b16cde8bd
Vardan Nadkarni <vardan.nadkarni@gmail.com>
2023-05-09 21:10:44 +0530

    removes min from search in swagger

--- services/gateway/apis/http-version3/methods/generators.js
+++ services/gateway/apis/http-version3/methods/generators.js
@@ -25,7 +25,7 @@ module.exports = {
 	params: {
 		limit: { optional: true, type: 'number', min: 1, max: 103, default: 10 },
 		offset: { optional: true, type: 'number', min: 0, default: 0 },
-		search: { optional: true, type: 'string', min: 1, pattern: regex.PARTIAL_SEARCH, altSwaggerKey: 'search' },
+		search: { optional: true, type: 'string', pattern: regex.PARTIAL_SEARCH, altSwaggerKey: 'search' },
 	},
 	tags: ['Generators'],
 	get schema() {
