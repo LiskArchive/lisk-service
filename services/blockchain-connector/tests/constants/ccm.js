4b7214e59e634b98ba86bdf6b83f3991c9472864
nagdahimanshu <himanshu.nagda@lightcurve.io>
2023-05-09 14:40:50 +0200

    Add tests schema for calculate tx fees endpoint

--- services/blockchain-connector/tests/constants/ccm.js
+++ services/blockchain-connector/tests/constants/ccm.js
@@ -14,30 +14,30 @@
  *
  */
 const decodedCCM = Object.freeze({
-    module: 'token',
-    crossChainCommand: 'transferCrossChain',
-    nonce: '1',
-    fee: '10000000',
-    sendingChainID: '02000000',
-    receivingChainID: '02000001',
-    params: '0a0802000000000000001080d0dbc3f4021a14fc18da54f6ce01bf31195548460361dfdb83c2052214888592ae41888c267826d7870206d3016a1863042a00',
-    status: 0,
+	module: 'token',
+	crossChainCommand: 'transferCrossChain',
+	nonce: '1',
+	fee: '10000000',
+	sendingChainID: '02000000',
+	receivingChainID: '02000001',
+	params: '0a0802000000000000001080d0dbc3f4021a14fc18da54f6ce01bf31195548460361dfdb83c2052214888592ae41888c267826d7870206d3016a1863042a00',
+	status: 0,
 });
 
 const encodedCCM = '0a05746f6b656e12127472616e7366657243726f7373436861696e18012080ade2042a04020000003204020000013a3f0a0802000000000000001080d0dbc3f4021a14fc18da54f6ce01bf31195548460361dfdb83c2052214888592ae41888c267826d7870206d3016a1863042a004000';
 
 const invalidCCM = Object.freeze({
-    module: 'token',
-    crossChainCommand: 'transferCrossChain',
-    nonce: '1',
-    fee: '10000000',
-    sendingChainID: '02000000',
-    receivingChainID: '02000001',
-    status: 0,
+	module: 'token',
+	crossChainCommand: 'transferCrossChain',
+	nonce: '1',
+	fee: '10000000',
+	sendingChainID: '02000000',
+	receivingChainID: '02000001',
+	status: 0,
 });
 
 module.exports = {
-    decodedCCM,
-    encodedCCM,
-    invalidCCM,
+	decodedCCM,
+	encodedCCM,
+	invalidCCM,
 };
---
+++ tests/schemas/api_v3/transactionsCalculateFees.schema.js
@@ -0,0 +1,51 @@
+/*
+ * LiskHQ/lisk-service
+ * Copyright © 2023 Lisk Foundation
+ *
+ * See the LICENSE file at the top-level directory of this distribution
+ * for licensing information.
+ *
+ * Unless otherwise agreed in a custom licensing agreement with the Lisk Foundation,
+ * no part of this software, including this file, may be copied, modified,
+ * propagated, or distributed except according to the terms contained in the
+ * LICENSE file.
+ *
+ * Removal or modification of this copyright notice is prohibited.
+ *
+ */
+import Joi from 'joi';
+
+const regex = require('./regex');
+
+const dynamicFeeEstimates = {
+	low: Joi.number().min(0).required(),
+	medium: Joi.number().min(0).required(),
+	high: Joi.number().min(0).required(),
+};
+
+const genericFeeSchema = {
+	tokenID: Joi.string().pattern(regex.tokenID).required(),
+	amount: Joi.string().pattern(regex.DIGITS).required(),
+};
+
+const transactionFeeEstimates = {
+	minFee: Joi.string().required(),
+	accountInitializationFee: Joi.object(genericFeeSchema).optional(),
+	messageFee: Joi.object(genericFeeSchema).optional(),
+};
+
+const dataSchema = {
+	transactionFeeEstimates: Joi.object(transactionFeeEstimates).required(),
+	dynamicFeeEstimates: Joi.object(dynamicFeeEstimates).required(),
+};
+
+const metaSchema = {};
+
+const transactionCalculateFees = {
+	data: Joi.object(dataSchema).required(),
+	meta: Joi.object(metaSchema).required(),
+};
+
+module.exports = {
+	transactionCalculateFees: Joi.object(transactionCalculateFees).required(),
+};
