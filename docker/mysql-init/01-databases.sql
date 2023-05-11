f3a29bd06e0b532da564fa33bd5689079320634d
Priojeet Das Priyom <priojeetpriyom@gmail.com>
2023-05-11 15:05:17 +0200

    WIP: Docker replication works

--- docker/mysql-init/01-databases.sql
+++ docker/mysql-init/01-databases.sql
@@ -3,3 +3,8 @@ CREATE DATABASE IF NOT EXISTS `lisk`;
 
 # create lisk user and grant rights
 GRANT ALL PRIVILEGES ON *.* TO 'lisk'@'%';
+
+-- Create user for read replica and grant replication privilage
+CREATE USER 'read_replica'@'%' IDENTIFIED WITH mysql_native_password BY 'password';
+GRANT REPLICATION SLAVE ON *.* TO 'read_replica'@'%';
+FLUSH PRIVILEGES;
