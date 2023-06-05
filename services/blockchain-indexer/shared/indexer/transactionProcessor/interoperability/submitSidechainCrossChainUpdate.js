d87e2398ba616dd6084ab276df61bae5e99f69da
nagdahimanshu <himanshu.nagda@lightcurve.io>
2023-06-05 10:42:52 +0200

    Applied suggestions

--- services/blockchain-indexer/shared/indexer/transactionProcessor/interoperability/submitSidechainCrossChainUpdate.js
+++ services/blockchain-indexer/shared/indexer/transactionProcessor/interoperability/submitSidechainCrossChainUpdate.js
@@ -18,6 +18,10 @@ const {
 	MySQL: { getTableInstance },
 } = require('lisk-service-framework');
 
+const { MODULE_NAME } = require('./index');
+const { COMMAND_NAME: COMMAND_NAME_REGISTER_MAINCHAIN } = require('./registerMainchain');
+const { COMMAND_NAME: COMMAND_NAME_REGISTER_SIDECHAIN } = require('./registerSidechain');
+
 const config = require('../../../../config');
 const { TRANSACTION_STATUS } = require('../../../constants');
 
@@ -26,13 +30,12 @@ const logger = Logger();
 const MYSQL_ENDPOINT = config.endpoints.mysql;
 const blockchainAppsTableSchema = require('../../../database/schema/blockchainApps');
 const ccuTableSchema = require('../../../database/schema/ccu');
-const transactionsTableSchema = require('../../../database/schema/transactions');
-
+const { getTransactions, isMainchain } = require('../../../dataService');
 const { getChainInfo } = require('./registerMainchain');
+const { APP_STATUS } = require('../../../dataService/business/interoperability/constants');
 
 const getBlockchainAppsTable = () => getTableInstance(blockchainAppsTableSchema, MYSQL_ENDPOINT);
 const getCCUTable = () => getTableInstance(ccuTableSchema, MYSQL_ENDPOINT);
-const getTransactionsTable = () => getTableInstance(transactionsTableSchema, MYSQL_ENDPOINT);
 
 // Command specific constants
 const COMMAND_NAME = 'submitSidechainCrossChainUpdate';
@@ -68,38 +71,66 @@ const revertTransaction = async (blockHeader, tx, events, dbTrx) => {
 	if (tx.executionStatus !== TRANSACTION_STATUS.SUCCESS) return;
 
 	const blockchainAppsTable = await getBlockchainAppsTable();
-	const transactionsTable = await getTransactionsTable();
 	const ccuTable = await getCCUTable();
 
 	logger.trace(`Reverting cross chain update transaction ${tx.id} contained in block at height ${tx.height}.`);
 
-	const searchParams = {
-		sendingChainID: tx.params.sendingChainID,
-		propBetweens: [{
-			property: 'height',
-			lowerThan: blockHeader.height,
-		}],
-		sort: 'height:desc',
-		limit: 1,
-	};
-
-	const [{ height: prevTransactionHeight }] = await ccuTable.find(searchParams, 'height');
-	const [prevTransaction] = await transactionsTable.find(
-		{
-			height: prevTransactionHeight,
-			moduleCommand: tx.moduleCommand,
-			limit: 1,
-		}, 'timestamp');
-
+	let prevTransaction;
 	const chainInfo = await getChainInfo(tx.params.sendingChainID);
+
+	if (chainInfo.status === APP_STATUS.ACTIVE) {
+		const searchParams = {
+			sendingChainID: tx.params.sendingChainID,
+			propBetweens: [{
+				property: 'height',
+				lowerThan: blockHeader.height,
+			}],
+			sort: 'height:desc',
+		};
+
+		const resultSet = await ccuTable.find(searchParams, 'height');
+
+		for (let i = 0; i < resultSet.length; i++) {
+			/* eslint-disable no-await-in-loop */
+			const result = (await getTransactions({
+				height: resultSet[i].height,
+				moduleCommand: tx.moduleCommand,
+				executionStatus: TRANSACTION_STATUS.SUCCESS,
+			})).data;
+
+			if (result.length) {
+				prevTransaction = result.find(e => e.params.sendingChainID === tx.params.sendingChainID);
+				break;
+			}
+		}
+	} else if (chainInfo.status === APP_STATUS.REGISTERED) {
+		const COMMAND = await isMainchain()
+			? COMMAND_NAME_REGISTER_SIDECHAIN
+			: COMMAND_NAME_REGISTER_MAINCHAIN;
+
+		const result = (await getTransactions({
+			moduleCommand: `${MODULE_NAME}:${COMMAND}`,
+			executionStatus: TRANSACTION_STATUS.SUCCESS,
+			propBetweens: [{
+				property: 'height',
+				lowerThan: blockHeader.height,
+			}],
+		})).data;
+
+		if (result.length) {
+			prevTransaction = result.find(e => e.params.sendingChainID === tx.params.sendingChainID);
+		}
+	}
+
 	const appInfo = {
 		chainID: tx.params.sendingChainID,
 		status: chainInfo.status,
-		lastUpdated: prevTransaction.timestamp,
-		lastCertificateHeight: prevTransactionHeight,
+		lastUpdated: prevTransaction.block.timestamp,
+		lastCertificateHeight: prevTransaction.block.height,
 	};
 
 	await blockchainAppsTable.upsert(appInfo, dbTrx);
+	/* eslint-enable no-await-in-loop */
 	logger.debug(`Reverted cross chain update transaction ${tx.id} contained in block at height ${tx.height}.`);
 };
 
