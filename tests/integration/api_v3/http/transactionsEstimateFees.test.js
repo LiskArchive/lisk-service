14c00cc29d03f98f9e3faa2e2b5e9c0019963e9a
Priojeet Das Priyom <priojeetpriyom@gmail.com>
2023-08-03 15:23:52 +0600

    Add integration test coverage for transaction estimate fees

--- tests/integration/api_v3/http/transactionsEstimateFees.test.js
+++ tests/integration/api_v3/http/transactionsEstimateFees.test.js
@@ -13,11 +13,24 @@
  * Removal or modification of this copyright notice is prohibited.
  *
  */
+const BluebirdPromise = require('bluebird');
+
 const config = require('../../../config');
 const { api } = require('../../../helpers/api');
 const {
 	TRANSACTION_OBJECT_VALID,
 	TRANSACTION_ENCODED_VALID,
+	TOKEN_TRANSFER_TRANSACTION_OBJECT,
+	AUTH_REGISTER_MULTI_SIGNATURE_TRANSACTION_OBJECT,
+	POS_REGISTER_VALIDATOR_TRANSACTION_OBJECT,
+	POS_UNLOCK_TRANSACTION_OBJECT,
+	LEGACY_RECLAIM_LSK,
+	LEGACY_REGISTER_KEYS_TRANSACTION_OBJECT,
+	POS_REPORT_MISBEHAVIOR_TRANSACTION_OBJECT,
+	POS_STAKE_TRANSACTION_OBJECT,
+	POS_UPDATE_GENERATOR_KEY_TRANSACTION_OBJECT,
+	POS_CHANGE_COMMISSION_TRANSACTION_OBJECT,
+	POS_CLAIM_REWARDS_TRANSACTION_OBJECT,
 } = require('../constants/transactionsDryRun');
 
 const {
@@ -30,6 +43,21 @@ const { transactionEstimateFees } = require('../../../schemas/api_v3/transaction
 const baseUrl = config.SERVICE_ENDPOINT;
 const baseUrlV3 = `${baseUrl}/api/v3`;
 const endpoint = `${baseUrlV3}/transactions/estimate-fees`;
+
+const getEntries = (o, prefix = '') => Object
+	.entries(o)
+	.flatMap(
+		([k, v]) => Object(v) === v ? getEntries(v, `${prefix}${k}.`) : [[`${prefix}${k}`, v]],
+	);
+
+const getSumOfMetaValues = (meta) => {
+	const flattenedEntries = getEntries(meta.breakdown);
+	const flattenedObject = Object.fromEntries(flattenedEntries);
+	const sum = Object
+		.values(flattenedObject)
+		.reduce((a, b) => BigInt(a) + BigInt(b), BigInt(0));
+	return sum.toString();
+};
 
 describe('Post estimate-fees transactions API', () => {
 	it('should return transaction fees with valid transaction object', async () => {
@@ -53,6 +81,34 @@ describe('Post estimate-fees transactions API', () => {
 		const { fee, ...remTransactionObject } = TRANSACTION_OBJECT_VALID;
 		const response = await api.post(endpoint, { transaction: remTransactionObject });
 		expect(response).toMap(transactionEstimateFees);
+	});
+
+	describe.only('Test estimate-fees transactions for all transaction types', () => {
+		const transactionsMap = {
+			'token:transfer': TOKEN_TRANSFER_TRANSACTION_OBJECT,
+			'auth:registerMultisignature': AUTH_REGISTER_MULTI_SIGNATURE_TRANSACTION_OBJECT,
+			'pos:registerValidator': POS_REGISTER_VALIDATOR_TRANSACTION_OBJECT,
+			'pos:unlock': POS_UNLOCK_TRANSACTION_OBJECT,
+			'pos:updateGeneratorKey': POS_UPDATE_GENERATOR_KEY_TRANSACTION_OBJECT,
+			'pos:reportMisbehavior': POS_REPORT_MISBEHAVIOR_TRANSACTION_OBJECT,
+			'pos:stake': POS_STAKE_TRANSACTION_OBJECT,
+			'pos:changeCommission': POS_CHANGE_COMMISSION_TRANSACTION_OBJECT,
+			'pos:claimRewards': POS_CLAIM_REWARDS_TRANSACTION_OBJECT,
+
+			'legacy:reclaimLSK': LEGACY_RECLAIM_LSK,
+			'legacy:registerKeys': LEGACY_REGISTER_KEYS_TRANSACTION_OBJECT,
+		};
+
+		Object
+			.entries(transactionsMap)
+			.forEach(([transactionType, transactionObject]) => {
+				it(`should return transaction fees when called with ${transactionType} transaction object`, async () => {
+					const { fee, ...remTransactionObject } = transactionObject;
+					const response = await api.post(endpoint, { transaction: remTransactionObject });
+					expect(response).toMap(transactionEstimateFees);
+					expect(getSumOfMetaValues(response.meta)).toEqual(response.data.transaction.fee.minimum);
+				});
+			});
 	});
 
 	it('should return bad request when called with valid transaction string', async () => {
