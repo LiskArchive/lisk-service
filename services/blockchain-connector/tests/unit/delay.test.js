175b36057fbf976bc2182212e72051eff93ba0fd
Priojeet Das Priyom <priojeetpriyom@gmail.com>
2023-10-06 16:31:26 +0200

    Make delay and waitForIt test limit more accurate

--- services/blockchain-connector/tests/unit/delay.test.js
+++ services/blockchain-connector/tests/unit/delay.test.js
@@ -30,6 +30,6 @@ describe('Test delay method', () => {
 		await delay(delayMs);
 		const endTime = Date.now();
 		const millisDifference = endTime - startTime;
-		expect(millisDifference).toBeGreaterThanOrEqual(delayMs * 0.98);
+		expect(millisDifference).toBeGreaterThanOrEqual(delayMs - 1);
 	});
 });
